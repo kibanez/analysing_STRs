c7e819e686b02f5b36e0a451ea985d3984fb180d
kristina ibanez garikano <kibanez@users.noreply.github.com>
2019-10-29 15:22:20 +0000

    first commit

---
+++ plotting_validation_table.R
@@ -0,0 +1,11 @@
+# Objective: Plot the validation table (negative, positive) in a cool/nice way.
+date ()
+Sys.info ()[c("nodename", "user")]
+commandArgs ()
+rm (list = ls ())
+R.version.string ## "R version 3.3.2 (2016-10-31)"
+
+# libraries
+library(dplyr)
+
+
