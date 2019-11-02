50fde4344d6264faa3ac84166a708291d8ca1fcb
kristina ibanez garikano <kibanez@users.noreply.github.com>
2019-11-02 14:11:35 +0000

    loading val data and pilot merged maxCI values data

--- completing_validation_golden_table.R
+++ completing_validation_golden_table.R
@@ -1,5 +1,4 @@
 # Objective: Complete our validation table together with the max CI value estimated by EH for each allele
-
 date ()
 Sys.info ()[c("nodename", "user")]
 commandArgs ()
@@ -10,4 +9,22 @@ R.version.string ## "R version 3.3.2 (2016-10-31)"
 library(dplyr)
 
 # set the working directory
-setwd("~/Documents/STRs/clinical_validation_paper/plots/")
+setwd("~/Documents/STRs/VALIDATION/")
+
+# Load validation golden table data
+val_data = read.csv("./STRVALIDATION_ALLDATA_2019-10-7_ALL_kibanez.tsv",
+                    header = T,
+                    sep = "\t",
+                    stringsAsFactors = F)
+dim(val_data)
+# 639 17
+
+# Load Pilot merged table
+merged_maxCI_table_pilot = read.csv("./raw_data/pilot_validation/merged/merged_validation_pilot.tsv",
+                              sep = "\t",
+                              header = T,
+                              stringsAsFactors = F)
+dim(merged_maxCI_table_pilot)
+# 292 11
+
+# Load 
