e1454062d514f627aa704fe5ddd17e7e51f5a01d
kristina ibanez garikano <kibanez@users.noreply.github.com>
2019-11-03 22:34:39 +0000

    checking mod 2

--- completing_validation_golden_table.R
+++ completing_validation_golden_table.R
@@ -32,7 +32,7 @@ merged_avg_table_pilot = read.csv("./pilot_validation/merged/merged_validation_p
                                   header = T,
                                   stringsAsFactors = F)
 dim(merged_avg_table_pilot)
-# 294  11
+# 271  11
 
 merged_avg_table_pilot = merged_avg_table_pilot %>%
   select(gene, allele, list_samples)
@@ -40,7 +40,7 @@ colnames(merged_avg_table_pilot) = c("gene", "avg_repeat", "list_samples")
 merged_avg_table_pilot = merged_avg_table_pilot %>%
   mutate(unique_id = paste(gene, avg_repeat, sep = "_"))
 dim(merged_avg_table_pilot)
-# 294  4
+# 271  4
 
 merged_maxCI_table_pilot = merged_maxCI_table_pilot %>%
   select(gene, allele, list_samples)
@@ -48,13 +48,13 @@ colnames(merged_maxCI_table_pilot) = c("gene", "maxCI_repeat", "list_samples")
 merged_maxCI_table_pilot = merged_maxCI_table_pilot %>%
   mutate(unique_id = paste(gene, maxCI_repeat, sep = "_"))
 dim(merged_maxCI_table_pilot)
-# 292  4
+# 294  4
 
 merged_all_pilot = full_join(merged_avg_table_pilot,
                          merged_maxCI_table_pilot,
                          by = c("unique_id"))
 dim(merged_all_pilot)
-# 318  7
+# 319  7
 
 colnames(merged_all_pilot) = c("gene_avg", "avg_repeat", "list_samples_avg", "unique_id", "gene_max", "maxCI_repeat", "list_samples_maxCI")
 
@@ -65,7 +65,7 @@ merged_maxCI_table_research = read.csv("../raw_data/research_validation/merged/m
                                        header = T,
                                        stringsAsFactors = F)
 dim(merged_maxCI_table_research)
-# 862  11
+# 869  11
 
 merged_maxCI_table_research = merged_maxCI_table_research %>%
   select(gene, allele, list_samples)
@@ -73,14 +73,14 @@ colnames(merged_maxCI_table_research) = c("gene", "maxCI_repeat", "list_samples"
 merged_maxCI_table_research = merged_maxCI_table_research %>%
   mutate(unique_id = paste(gene, maxCI_repeat, sep = "_"))
 dim(merged_maxCI_table_research)
-# 862  4
+# 869  4
 
 merged_avg_table_research = read.csv("../raw_data/research_validation/merged/merged_validation_research_avg.tsv",
                                      sep = "\t",
                                      header = T,
                                      stringsAsFactors = F)
 dim(merged_avg_table_research)
-# 855  11
+# 772  11
 
 merged_avg_table_research = merged_avg_table_research %>%
   select(gene, allele, list_samples)
@@ -88,23 +88,16 @@ colnames(merged_avg_table_research) = c("gene", "avg_repeat", "list_samples")
 merged_avg_table_research = merged_avg_table_research %>%
   mutate(unique_id = paste(gene, avg_repeat, sep = "_"))
 dim(merged_avg_table_research)
-# 855  4
+# 772  4
 
 merged_all_research = full_join(merged_avg_table_research,
                                 merged_maxCI_table_research,
                                 by = c("unique_id"))
 
 dim(merged_all_research)
-# 1377  7
+# 1329  7
 
 colnames(merged_all_research) = c("gene_avg", "avg_repeat", "list_samples_avg", "unique_id", "gene_max", "maxCI_repeat", "list_samples_maxCI")
-
-merged_all = full_join(merged_all_pilot,
-                       merged_all_research,
-                       by = c("unique_id"))
-dim(merged_all)
-# 1437  13
-
 
 # Here should be WESSEX data (It's not research nor pilot)!!
 # TODO
@@ -125,18 +118,28 @@ for (i in 1:length(val_data$loci)){
   # avg values (to double check)
   row_avg_research = merged_all_research %>%
     filter(gene_avg %in% locus, grepl(platekey, list_samples_avg)) %>%
-    select(avg_repeat) %>% pull() %>% as.character()
+    select(avg_repeat) %>% pull() %>% as.character() 
   
   # maxCI values
   row_maxCI_research = merged_all_research %>%
     filter(gene_max %in% locus, grepl(platekey, list_samples_maxCI)) %>%
     select(maxCI_repeat) %>% pull() %>% as.character()
   
+  
   if (length(row_avg_research) > 0){
     if (length(row_avg_research) < 2){
       val_data2$EH_a1_avg[i] = row_avg_research
       val_data2$EH_a1_maxCI[i] = row_maxCI_research
     }else{
+      # Check whether we do have mod 2, otherwise do `unique`
+      if (length(row_avg_research) %% 2 != 0){
+        row_avg_research = unique(row_avg_research)
+      }
+
+      if (length(row_maxCI_research) %% 2 != 0){
+        row_maxCI_research = unique(row_maxCI_research)
+      }
+      
       val_data2$EH_a1_avg[i] = row_avg_research[1]
       val_data2$EH_a2_avg[i] = row_avg_research[2]
       val_data2$EH_a1_maxCI[i] = row_maxCI_research[1]
