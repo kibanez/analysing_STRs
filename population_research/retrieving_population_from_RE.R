b8c4dc805b42689b3133cb9f46a33d71b60db632
kristina ibanez garikano <kibanez@users.noreply.github.com>
2019-11-06 16:54:22 +0000

    writting down the originated file

--- population_research/retrieving_population_from_RE.R
+++ population_research/retrieving_population_from_RE.R
@@ -17,3 +17,15 @@ popu_table = read.table("aggregate_gvcf_sample_stats_2019-10-03_22-07-31.tsv",
                         stringsAsFactors = F)
 dim(popu_table)
 # 59356  51
+
+# The research group at GEL has estimated with Plink the ancestry of 59,356 unrelated genomes (probands) RD and cancer germlines
+popu_table = popu_table %>% select(participant_id, platekey, type, sample_type, participant_phenotypic_sex, pred_african_ancestries, pred_american_ancestries, pred_european_ancestries, pred_east_asian_ancestries, pred_south_asian_ancestries)
+dim(popu_table)
+# 59356  10
+
+write.table(popu_table,
+            "~/Documents/STRs/ANALYSIS/population_research/population_info_by_031019.tsv",
+            sep = "\t",
+            row.names = F,
+            col.names = T,
+            quote = F)
