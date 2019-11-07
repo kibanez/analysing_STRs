65b160428b867b9f0295ec99a23a9512c55a69af
kristina ibanez garikano <kibanez@users.noreply.github.com>
2019-11-07 09:54:22 +0000

    scripts for enriching with clinical data the STR calls in each genome and locus

---
+++ clinical_enrichment/enrich_merged_with_RE_nov2019_EHv3.1.2.R
@@ -0,0 +1,204 @@
+# R script to enrich with clinical data all STRs we have for all loci
+# This script also generates in 1 row 1 genome or LP
+# We need to split the merge TSV file by loci
+
+# libraries
+library(Rlabkey)
+library(dplyr)
+library(tidyverse); packageDescription ("tidyverse", fields = "Version") # "1.2.1
+
+setwd("/Users/KristinaIbanez/Documents/STRs/GEL_STR/summer_2019/EH_3.1.2_research_October2019/EH_output_v3.1.2_October2019_vcfs/merged/")
+
+# Research 80K analysis - November 2019
+merged_vcf = read.table("./merged_loci_82565_research_genomes_EHv3.1.2_october2019.tsv",
+                        stringsAsFactors = F, 
+                        header = T, 
+                        sep = "\t")
+
+dim(merged_vcf)                        
+# 4495  12
+
+# Change colnames to make everything easier
+colnames(merged_vcf) = c("chr", "start", "end", "repeat-size", "gene", "ref", "alt", "repeat-motif", "num_samples", "AF", "LC", "list_samples")
+
+num_loci= unique(merged_vcf$gene)
+
+num_loci = sort(num_loci)
+
+# the Same thing, we need to stratify and distinguish between assemblies: GRCh37 and GRCh38
+# This research merged TSV file is special because we do have GRCh37 and GRCh38 genomes altogether
+# GRCh37 VCF files have 1,2,3,4...X,Y,MT chromosome nomenclature
+# GRCh38 VCF files have chr1, chr2, chr3, ..., chrX, chrX, chrMT nomenclature
+# And also, the genomic positions for the loci are different, so we cannot combine them easily
+
+# Let's stratify the whole TSV in b37 and b38
+
+merged_b37 = merged_vcf %>% filter(!grepl("chr", chr))
+dim(merged_b37)
+# 1868 12
+
+merged_b38 = merged_vcf %>% filter(grepl("chr", chr))
+dim(merged_b38)
+# 2627  12
+
+
+################################################################################################################################
+# Data from RE rather than from Catalog (this clinical data has been retrieved from RE on Sept 2019)
+clin_data = read.table("/Users/KristinaIbanez/Documents/STRs/GEL_STR/summer_2019/clinical_data/rd_genomes_all_data_250919.tsv",
+                           sep = "\t",
+                           stringsAsFactors = FALSE, 
+                           header = TRUE)
+dim(clin_data)  
+# 1056568  26
+
+# Let´s put all panel names into 1 single string splitted by ','
+list_panels = clin_data %>% group_by(participant_id) %>% summarise(panel_list = toString(panel_name)) %>% ungroup() %>% as.data.frame()
+dim(list_panels)
+# 89163  2
+
+# Let´s put all HPO terms into 1 single string splitted by ','
+list_hpos = clin_data %>% group_by(participant_id) %>% summarise(hpo_list = toString(hpo_term)) %>% ungroup() %>% as.data.frame()
+dim(list_hpos)
+# 89163  2
+
+# Remove the panels and hpo columns, and include the list of panels and hpo respectively
+clin_data = clin_data %>% 
+  select(participant_id, plate_key.x, rare_diseases_family_id, biological_relationship_to_proband, normalised_specific_disease, specific_disease, genome_build, disease_group, disease_sub_group, year_of_birth, participant_phenotypic_sex, programme, family_group_type, affection_status)
+dim(clin_data)
+# 1056568  14
+
+clin_data = left_join(clin_data,
+                      list_panels,
+                      by = "participant_id")
+dim(clin_data)
+# 1056568  15
+
+clin_data = left_join(clin_data,
+                      list_hpos,
+                      by = "participant_id")
+dim(clin_data)
+# 1056568  16
+
+
+for (i in num_loci){
+  # GRCh37 annotation
+  merged_vcf = merged_b37
+  
+  # we go locus by locus
+  df_locus = merged_vcf %>% filter(gene %in% i)
+  df_locus_new = data.frame()
+  
+  if (dim(df_locus)[1] >0){
+    
+  # For each row, we need to split/separate in many rows as samples/LPs
+  for (j in 1:length(df_locus$chr)){
+    
+    # number of samples in which a STR-expansion has been detected
+    number_samp = strsplit(df_locus$list_samples[j],";")[[1]]
+    
+    # Clean the name of the VCF files -- removing the full path from them, and keeping only the VCF name
+    number_samp = sub("^EH_", "", number_samp)
+    number_samp = sub(".vcf", "", number_samp)
+    
+    # Include in df_locus$list_samples the number_samp
+    df_locus$list_samples[j] = paste(number_samp, collapse = ';')
+    
+    for (k in 1:length(number_samp)){
+      # we write for each LP/sample/participant the same line/row, but also enriching with clinical data
+      new_line = df_locus[j,]
+      new_line$list_vcf_affected = number_samp[k]
+      
+      # we add now clinical data for each row -- each participant/sample/genome
+      
+      # sometimes we do have `_x2` for the sample, if 2 alleles are having the same repeat-size (GT = 1/1)
+      number_samp[k] = gsub("`_x2", "", number_samp[k])
+      number_samp[k] = gsub("_x2", "", number_samp[k])
+      to_include = clin_data %>% 
+        filter(plate_key.x %in% number_samp[k]) %>% 
+        select(participant_id, plate_key.x, rare_diseases_family_id, specific_disease, disease_group, disease_sub_group, year_of_birth, participant_phenotypic_sex, biological_relationship_to_proband, affection_status, family_group_type, hpo_list, panel_list, programme, genome_build) %>%
+        unique()
+    
+      if (dim(to_include)[1] <= 0){
+        to_include = rep('.', dim(to_include)[2])
+        to_include = as.data.frame(t(as.data.frame(to_include)))
+        colnames(to_include) = c("participant_id", "plate_key.x", "rare_diseases_family_id", "specific_disease", "disease_group", "disease_sub_group", "year_of_birth", "participant_phenotypic_sex", "biological_relationship_to_proband", "affection_status", "family_group_type", "hpo_list", "panel_list", "programme", "genome_build")
+      }
+      new_line = cbind(new_line, to_include)
+      df_locus_new = rbind(df_locus_new, new_line)
+    }# k
+  }#j
+  
+  # We write for each locus the output dataframe `df_locus_new`
+  output_name = paste("research_genomes_86457_GRCh37_EHv2.5.5", i, sep = "_")
+  output_name = paste(output_name, "tsv", sep = ".")
+  
+  # we filter out the column `list_samples` from the final dataframe, not to have too much data
+  df_locus_new  = df_locus_new[,-11]
+  
+  write.table(df_locus_new, file = output_name, quote = F, row.names = F, col.names = T, sep = "\t")
+  } # if dim > 0
+  ################################################################################################################################################
+  ################################################################################################################################################
+  
+  # GRCh38 annotation
+  
+  merged_vcf = merged_b38
+  
+  # we go locus by locus
+  df_locus = merged_vcf %>% filter(gene %in% i)
+  df_locus_new = data.frame()
+  
+  if (dim(df_locus)[1] > 0){
+  # For each row, we need to split/separate in many rows as samples/LPs
+  for (j in 1:length(df_locus$chr)){
+    
+    # number of samples in which a STR-expansion has been detected
+    #number_samp = strsplit(df_locus$list_vcf_affected[j],";")[[1]]
+    number_samp = strsplit(df_locus$list_samples[j],";")[[1]]
+    
+    # Clean the name of the VCF files -- removing the full path from them, and keeping only the VCF name
+    number_samp = sub("^EH_", "", number_samp)
+    number_samp = sub(".vcf", "", number_samp)
+    
+    # Include in df_locus$list_samples the number_samp
+    df_locus$list_samples[j] = paste(number_samp, collapse = ';')
+    
+    for (k in 1:length(number_samp)){
+      # we write for each LP/sample/participant the same line/row, but also enriching with clinical data
+      
+      new_line = df_locus[j,]
+      new_line$list_vcf_affected = number_samp[k]
+      
+      # we add now clinical data for each row -- each participant/sample/genome
+      
+      # sometimes we do have `_x2` for the sample, if 2 alleles are having the same repeat-size (GT = 1/1)
+      number_samp[k] = gsub("`_x2", "", number_samp[k])
+      number_samp[k] = gsub("`_x2", "", number_samp[k])
+      to_include = clin_data %>% 
+        filter(plate_key.x %in% number_samp[k]) %>% 
+        select(participant_id, plate_key.x, rare_diseases_family_id, specific_disease, disease_group, disease_sub_group, year_of_birth, participant_phenotypic_sex, biological_relationship_to_proband, affection_status, family_group_type, hpo_list, panel_list, programme, genome_build) %>%
+        unique()
+      
+      if (dim(to_include)[1] <= 0){
+        to_include = rep('.', dim(to_include)[2])
+        to_include = as.data.frame(t(as.data.frame(to_include)))
+        colnames(to_include) = c("participant_id", "plate_key.x", "rare_diseases_family_id", "specific_disease", "disease_group", "disease_sub_group", "year_of_birth", "participant_phenotypic_sex", "biological_relationship_to_proband", "affection_status", "family_group_type", "hpo_list", "panel_list", "programme", "genome_build")
+      }
+      new_line = cbind(new_line, to_include)
+      df_locus_new = rbind(df_locus_new, new_line)
+      
+    }# k
+    
+  }#j
+  
+  # We write for each locus the output dataframe `df_locus_new`
+  output_name = paste("research_genomes_82565_GRCh38_EHv3.1.2", i, sep = "_")
+  output_name = paste(output_name, "tsv", sep = ".")
+  
+  # we filter out the column `list_samples` from the final dataframe, not to have too much data
+  df_locus_new  = df_locus_new[,-11]
+  
+  write.table(df_locus_new, file = output_name, quote = F, row.names = F, col.names = T, sep = "\t")
+  
+  }# if dim > 0
+}#i
---
+++ clinical_enrichment/enriching_clinical_data_pilot_cohort.R
@@ -0,0 +1,137 @@
+# The objective here is to enrich with clinical data all platekeys/participantIDs we do have in the PILOT cohort
+date ()
+Sys.info ()[c("nodename", "user")]
+commandArgs ()
+rm (list = ls ())
+R.version.string ## "R version 3.3.2 (2016-10-31)"
+
+# libraries
+library(dplyr)
+
+# setwd
+setwd("/Users/KristinaIbanez/Documents/STRs/GEL_STR/summer_2019/pilot_clinical_data")
+
+# PILOT clinical data retrieved from frozen pilot cohort (as we have done for the research cohort)
+#plate_key.x	participant_id	programme	genome_build	programme_consent_status	rare_diseases_family_id	biological_relationship_to_proband	affection_status	participant_phenotypic_sex	year_of_birth	normalised_specific_disease	disease_sub_group	disease_group	panel_list
+#LP2000950-DNA_H05	111000000	Rare Diseases	GRCh37	Consenting	111000000	Proband	Affected	Female	2005	Intellectual disability	Neurodevelopmental disorders	Neurology and neurodevelopmental disorders	Congenital hearing impairment (profound/severe), Congenital hearing impairment (profound/severe), Congenital hearing impairment (profound/severe), Congenital hearing impairment (profound/severe), Congenital hearing impairment (profound/severe), Congenital hearing impairment (profound/severe), Congenital hearing impairment (profound/severe), Congenital hearing impairment (profound/severe), Intellectual disability, Intellectual disability, Intellectual disability, Intellectual disability, Intellectual disability, Intellectual disability, Intellectual disability, Intellectual disability, Mitochondrial disorders, Mitochondrial disorders, Mitochondrial disorders, Mitochondrial disorders, Mitochondrial disorders, Mitochondrial disorders, Mitochondrial disorders, Mitochondrial disorders, Undiagnosed metabolic disorders, Undiagnosed metabolic disorders, Undiagnosed metabolic disorders, Undiagnosed metabolic disorders, Undiagnosed metabolic disorders, Undiagnosed metabolic disorders, Undiagnosed metabolic disorders, Undiagnosed metabolic disorders
+#LP2000913-DNA_C05	111000001	Rare Diseases	GRCh37	Consenting	111000000	Father	Unaffected	Male	1967	N/A	NA	NA	Congenital hearing impairment (profound/severe), Intellectual disability, Mitochondrial disorders, Undiagnosed metabolic disorders
+
+# Read clinical data
+
+# Platekeys - gelID
+data_platekeys = read.csv("/Users/KristinaIbanez/Documents/STRs/GEL_Pilot/Frozen_RESEARCH/data_freeze_Pilot_LK_RESEARCH/samples.csv",
+                          sep = ",",
+                          stringsAsFactors = FALSE,
+                          header = TRUE)
+dim(data_platekeys)
+# 4833  2
+
+reg_info = read.csv("/Users/KristinaIbanez/Documents/STRs/GEL_Pilot/Frozen_RESEARCH/data_freeze_Pilot_LK_RESEARCH/registration.csv",
+                    sep = ",",
+                    stringsAsFactors = FALSE,
+                    header = TRUE)
+dim(reg_info)
+# 4877  23
+
+reg_info = reg_info %>% select(gelId, sex, sex_at_birth, gender, relation_to_proband, isProband, gelFamilyId, disease_status, yearOfBirth)
+
+diseases_info = read.csv("/Users/KristinaIbanez/Documents/STRs/GEL_Pilot/Frozen_RESEARCH/data_freeze_Pilot_LK_RESEARCH/diseases.csv",
+                         sep = ",",
+                         stringsAsFactors = FALSE,
+                         header = TRUE)
+dim(diseases_info)
+# 2793  3
+
+panel_info = read.csv("/Users/KristinaIbanez/Documents/STRs/GEL_Pilot/Frozen_RESEARCH/data_freeze_Pilot_LK_RESEARCH/panels.csv",
+                      sep = ",",
+                      stringsAsFactors = FALSE,
+                      header = TRUE)
+dim(panel_info)
+# 3315  6
+
+consent_info = read.csv("/Users/KristinaIbanez/Documents/STRs/GEL_Pilot/Frozen_RESEARCH/data_freeze_Pilot_LK_RESEARCH/pedigree.csv",
+                      sep = ",",
+                      stringsAsFactors = FALSE,
+                      header = TRUE)
+dim(consent_info)
+# 17258  34
+
+consent_info = consent_info %>% select(gelId, affectionStatus, review_outcome, qc_state, programmeConsent, consent_opinion)
+
+## OUTPUT FILE
+# First let's put all panel names as a list separated by ','
+data_platekeys$gelID = as.character(data_platekeys$gelID)
+to_write = left_join(data_platekeys,
+                     reg_info %>% select(gelId, gelFamilyId, sex, sex_at_birth, gender, relation_to_proband, isProband, disease_status, yearOfBirth),
+                     by = c("gelID" = "gelId"))
+dim(to_write)
+# 4833  10
+
+# Let´s change all `relation_to_proband` == "" to "Proband"
+to_write = to_write %>% group_by(gelID) %>% 
+  mutate(biological_relation_to_proband = ifelse(isProband, "Proband", relation_to_proband)) %>% 
+  as.data.frame()
+
+to_write = to_write %>% select(gelID, gelFamilyId, plateKey, sex, biological_relation_to_proband, disease_status, yearOfBirth)
+dim(to_write)
+# 4833  7
+
+diseases_info$gelID = as.character(diseases_info$gelID)
+to_write = left_join(to_write,
+                     diseases_info,
+                     by = "gelID")
+dim(to_write)
+# 4974  9
+
+consent_info$gelId = as.character(consent_info$gelId)
+to_write = left_join(to_write,
+                     consent_info %>% select(gelId, qc_state),
+                     by = c("gelID" = "gelId"))
+dim(to_write)
+# 4974  10
+
+panel_info$gelID = as.character(panel_info$gelID)
+to_write = left_join(to_write,
+                     panel_info %>% select(gelID, gelFamilyId, panel),
+                     by = "gelID")
+dim(to_write)
+# 6202  12
+
+# Put all panels separated by commas
+list_panels = to_write %>% group_by(gelID) %>% summarise(panel_list = toString(panel)) %>% ungroup() %>% as.data.frame()
+dim(list_panels)
+# 4833  2
+
+# Remove `panel_name` from to_write
+to_write = to_write[,-12]
+dim(to_write)
+# 6202 11
+
+to_write = left_join(to_write,
+                     list_panels,
+                     by = "gelID")
+
+to_write = unique(to_write)
+
+#plate_key.x	participant_id	programme	genome_build	programme_consent_status	rare_diseases_family_id	biological_relationship_to_proband	affection_status	participant_phenotypic_sex	year_of_birth	normalised_specific_disease	disease_sub_group	disease_group	panel_list
+#LP2000950-DNA_H05	111000000	Rare Diseases	GRCh37	Consenting	111000000	Proband	Affected	Female	2005	Intellectual disability	Neurodevelopmental disorders	Neurology and neurodevelopmental disorders	Congenital hearing impairment (profound/severe), Congenital hearing impairment (profound/severe), Congenital hearing impairment (profound/severe), Congenital hearing impairment (profound/severe), Congenital hearing impairment (profound/severe), Congenital hearing impairment (profound/severe), Congenital hearing impairment (profound/severe), Congenital hearing impairment (profound/severe), Intellectual disability, Intellectual disability, Intellectual disability, Intellectual disability, Intellectual disability, Intellectual disability, Intellectual disability, Intellectual disability, Mitochondrial disorders, Mitochondrial disorders, Mitochondrial disorders, Mitochondrial disorders, Mitochondrial disorders, Mitochondrial disorders, Mitochondrial disorders, Mitochondrial disorders, Undiagnosed metabolic disorders, Undiagnosed metabolic disorders, Undiagnosed metabolic disorders, Undiagnosed metabolic disorders, Undiagnosed metabolic disorders, Undiagnosed metabolic disorders, Undiagnosed metabolic disorders, Undiagnosed metabolic disorders
+to_write = to_write %>% select(gelID, plateKey, gelFamilyId.x, sex, biological_relation_to_proband, disease_status, yearOfBirth, specificDisease, ageOfOnset, qc_state, panel_list)
+
+to_write %>% select(plateKey) %>% unique() %>% pull() %>% length()
+# 4833
+
+write.table(to_write, 
+            file = "pilot_cohort_clinical_data_4833_genomes_withPanels_280919.tsv",
+            quote = F,
+            sep = "\t",
+            row.names = F,
+            col.names = T)
+
+# Without panels
+write.table(to_write[,-11], 
+            file = "pilot_cohort_clinical_data_4833_genomes_removingPanels_280919.tsv",
+            quote = F,
+            sep = "\t",
+            row.names = F,
+            col.names = T)
+
