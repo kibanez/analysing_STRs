48ba4c1710784d32d726e510749a8baa8a4e7f91
kristina ibanez garikano <kibanez@users.noreply.github.com>
2019-11-13 10:07:52 +0000

    include ggpubr

--- population_research/plotting_population_and_STR_distribution.R
+++ population_research/plotting_population_and_STR_distribution.R
@@ -11,6 +11,8 @@ library(dplyr); packageDescription ("dplyr", fields = "Version") #"0.8.3"
 library(ggplot2); packageDescription ("ggplot2", fields = "Version") #"3.2.1"
 library(reshape); packageDescription ("reshape", fields = "Version") #"0.8.8"
 library(scatterplot3d); packageDescription("scatterplot3d", fields = "Version") # 0.3-41
+library(ggpubr); packageDescription("ggpubr", fields = "Version") # 0.2.3
+
 
 # Set environment
 setwd("/Users/kibanez/Documents/STRs/ANALYSIS/population_research/")
