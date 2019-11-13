98cc974ff86cfa1b7fefca7ecd95f276637408e2
kristina ibanez garikano <kibanez@users.noreply.github.com>
2019-11-13 11:50:04 +0000

    postprocessing input data

--- pipeline_performance/plotting_bubble_plot.R
+++ pipeline_performance/plotting_bubble_plot.R
@@ -52,17 +52,67 @@ val_data = val_data %>%
 dim(val_data)
 # 419  19 
 
+# Let's simplify the data we need from `val_data`
+val_data = val_data %>%
+  select(LP_Number, locus_bioinfo, STR_a1, STR_a2, EH_a1_avg, EH_a2_avg)
 
+# in case there is only 1 allele, we should have 1 allele for expValidation and EH
+# in case there are 2 alleles, then the `a1` for expValidation and EH should be the minimum (minor repeat size)
+for (i in 1:length(val_data$LP_Number)){
+  val_validation_a1 = val_data$STR_a1[i]
+  val_validation_a2 = val_data$STR_a2[i]
+  val_eh_a1 = val_data$EH_a1_avg[i]
+  val_eh_a2 = val_data$EH_a2_avg[i]
+  
+  # For alleles that there is no estimation (EH only calls 1 allele) we do have a `0` -- we ignore/avoid this step in these cases
+  if (val_eh_a2 == "0"){
+    min_validation = val_validation_a1
+    min_eh = val_eh_a1
+    max_validation = '.'
+    max_eh = '.'
+  }else{
+    min_validation = min(val_validation_a1, val_validation_a2)
+    max_validation = max(val_validation_a1, val_validation_a2)
+    min_eh = min(val_eh_a1, val_eh_a2)
+    max_eh = max(val_eh_a1, val_eh_a2)
+  }
+  # Post-processing the new values
+  val_data$validation_a1[i] = min_validation
+  val_data$validation_a2[i] = max_validation
+  val_data$eh_a1[i] = min_eh
+  val_data$eh_a2[i] = max_eh
+}
+dim(val_data)
+# 419  10
 
+# Exclude `validation_a2` or `eh_a2` == '.'
+val_data = val_data %>%
+  filter(!validation_a2 %in% '.')
+val_data = val_data %>%
+  filter(!eh_a2 %in% '.')
+dim(val_data)
+# 417  10
 
-exp_alleles = as.integer(df_locus_final_wessex$validation)
-eh_alleles = as.integer(df_locus_final_wessex$eh)
+# Let's take the important meat: experimentally validated data and EH estimations
+exp_alleles_v2 = c(as.integer(val_data$STR_a1), as.integer(val_data$STR_a2))
+eh_alleles_v2 = c(as.integer(val_data$EH_a1_avg), as.integer(val_data$EH_a2_avg))
 
-data_with_freq = xyTable(exp_alleles, eh_alleles)
-data_with_freq2 = data.frame(eh_alleles = data_with_freq$y, exp_alleles = data_with_freq$x, number_of_alleles = data_with_freq$number)
+data_with_freq_v2 = xyTable(exp_alleles_v2, eh_alleles_v2)
+df_data_with_freq_v2 = data.frame(eh_alleles = data_with_freq_v2$y, 
+                                  exp_alleles = data_with_freq_v2$x, 
+                                  number_of_alleles = data_with_freq_v2$number)
 
-max_value = max(data_with_freq2$eh_alleles, data_with_freq2$exp_alleles) + 5
+max_value = max(df_data_with_freq_v2$eh_alleles, 
+                df_data_with_freq_v2$exp_alleles) + 5
+
 png(paste("Correlation_EH_experimental_merging_delivery_versions_WESSEX", paste(l_loci[i],"png", sep = '.'), sep = "_"))
-ggplot(data_with_freq2, aes(x = eh_alleles, y = exp_alleles, size = number_of_alleles)) + geom_point(alpha = 0.7, colour = "blue") + xlim(5,max_value) + ylim(5,max_value) + labs(title = paste("Correlation on repeat sizes: EH vs experimental validation", l_loci[i], sep=' '), x = "Repeat sizes for each allele \n Expansion Hunter", y = "Repeat sizes for each allele \n Experimental validation") + geom_abline(method = "lm", formula = y ~ x, linetype = 2, colour = "gray") +  coord_equal()
+ggplot(df_data_with_freq_v2, 
+       aes(x = eh_alleles, y = exp_alleles, size = number_of_alleles)) + 
+  geom_point(alpha = 0.7, colour = "blue") + 
+  xlim(5,max_value) + 
+  ylim(5,max_value) + 
+  labs(title = paste("Correlation on repeat sizes: EH vs experimental validation", l_loci[i], sep=' '), x = "Repeat sizes for each allele \n Expansion Hunter", y = "Repeat sizes for each allele \n Experimental validation") + 
+  geom_abline(method = "lm", formula = y ~ x, linetype = 2, colour = "gray") +  
+  coord_equal()
 dev.off()  
 
