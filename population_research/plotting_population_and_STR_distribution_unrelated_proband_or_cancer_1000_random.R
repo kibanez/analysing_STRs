9831cb18828402193a55564207197b383717f24d
kristina ibanez garikano <kibanez@users.noreply.github.com>
2019-11-22 11:03:42 +0000

    setting seed for selecting randome genomes

--- population_research/plotting_population_and_STR_distribution_unrelated_proband_or_cancer_1000_random.R
+++ population_research/plotting_population_and_STR_distribution_unrelated_proband_or_cancer_1000_random.R
@@ -1,6 +1,7 @@
 # Objective: take advantage from the work GEL bioinfo research group has done estimating ancestry for ~59,356 genomes
 # and analyse and study the STR distribution across them
 # Only in a subset with unrelated, affected/proband or cancer genomes
+# Only selecting random 1000 samples (except AMR and EAS)
 date ()
 Sys.info ()[c("nodename", "user")]
 commandArgs ()
@@ -40,42 +41,83 @@ source("/Users/kibanez/git/analysing_STRs/functions/plot_violin_ancestry.R")
 source("/Users/kibanez/git/analysing_STRs/functions/plot_gene.R")
 source("/Users/kibanez/git/analysing_STRs/functions/plot_gene_joint_ancestries.R")
 
+# Set seed
+set.seed(2)
+
+# First load the number of genomes unrelated, proband (for RD) and cancer (germline)
+l_asi = read.table("./list_genomes_unrelated_proband_or_cancer_2761_pure_ASI.txt", stringsAsFactors = F)
+l_asi = l_asi$V1
+length(l_asi)
+# 2761
+
+l_afr = read.table("./list_genomes_unrelated_proband_or_cancer_1164_pure_AFR.txt", stringsAsFactors = F)
+l_afr = l_afr$V1
+length(l_afr)
+# 1164
+
+l_eas = read.table("./list_genomes_unrelated_proband_or_cancer_231_pure_EAS.txt", stringsAsFactors = F)
+l_eas = l_eas$V1
+length(l_eas)
+# 231
+
+l_eur = read.table("./list_genomes_unrelated_proband_or_cancer_26784_pure_EUR.txt", stringsAsFactors = F)
+l_eur = l_eur$V1
+length(l_eur)
+# 26784
+
+l_amr = read.table("./list_genomes_unrelated_proband_or_cancer_396_pure_AMR.txt", stringsAsFactors = F)
+l_amr = l_amr$V1
+length(l_amr)
+# 396
+
+# Select random 500 genomes per each super-population
+l_asi_random = sample(l_asi, 400)
+l_afr_random = sample(l_afr, 400)
+l_eur_random = sample(l_eur, 400)
+
+write.table(l_asi_random, "./list_genomes_unrelated_proband_or_cancer_2761_pure_ASI_random_400.txt", quote = F, row.names = F, col.names = F)
+write.table(l_afr_random, "./list_genomes_unrelated_proband_or_cancer_1164_pure_AFR_random_400.txt", quote = F, row.names = F, col.names = F)
+write.table(l_eur_random, "./list_genomes_unrelated_proband_or_cancer_26784_pure_EUR_random_400.txt", quote = F, row.names = F, col.names = F)
+# l_amr stays the same
+# l_eas stays the same
+
+
 # Load EH STR output data - merged TSV files
 # Research ~80K genomes, EH-v3.1.2- November 2019 -- but the ones that are ALSO included in the population info table
-df_asi = read.csv('~/Documents/STRs/ANALYSIS/population_research/EH_3.1.2_research_October2019_55419_genomes/unrelated_probands_and_cancer/ASI/merged/merged_population_genomes_unrelated_probands_and_cancer_2678_avg_EHv3.1.2_ASI.tsv',
+df_asi = read.csv('~/Documents/STRs/ANALYSIS/population_research/EH_3.1.2_research_October2019_55419_genomes/unrelated_probands_and_cancer_RANDOM_400/ASI/merged/merged_population_genomes_unrelated_probands_and_cancer_2678_avg_EHv3.1.2_ASI.tsv',
               sep = '\t',
               stringsAsFactors = F,
               header = T)
 dim(df_asi)
-# 1430  12
+# 935  12
 
-df_eas = read.csv('~/Documents/STRs/ANALYSIS/population_research/EH_3.1.2_research_October2019_55419_genomes/unrelated_probands_and_cancer/EAS/merged/merged_population_genomes_unrelated_probands_and_cancer_227_avg_EHv3.1.2_EAS',
+df_eas = read.csv('~/Documents/STRs/ANALYSIS/population_research/EH_3.1.2_research_October2019_55419_genomes/unrelated_probands_and_cancer_RANDOM_400/EAS/merged/merged_population_genomes_unrelated_probands_and_cancer_227_avg_EHv3.1.2_EAS.tsv',
                   sep = '\t',
                   stringsAsFactors = F,
                   header = T) 
 dim(df_eas)
 # 723  12
 
-df_afr = read.csv('~/Documents/STRs/ANALYSIS/population_research/EH_3.1.2_research_October2019_55419_genomes/unrelated_probands_and_cancer/AFR/merged/merged_population_genomes_unrelated_probands_and_cancer_1136_avg_EHv3.1.2_AFR.tsv',
+df_afr = read.csv('~/Documents/STRs/ANALYSIS/population_research/EH_3.1.2_research_October2019_55419_genomes/unrelated_probands_and_cancer_RANDOM_400/AFR/merged/merged_population_genomes_unrelated_probands_and_cancer_1136_avg_EHv3.1.2_AFR.tsv',
                   sep = '\t',
                   stringsAsFactors = F,
                   header = T) 
 dim(df_afr)
-# 1170  12
+# 934  12
 
-df_amr = read.csv('~/Documents/STRs/ANALYSIS/population_research/EH_3.1.2_research_October2019_55419_genomes/unrelated_probands_and_cancer/AMR/merged/merged_population_genomes_unrelated_probands_and_cancer_384_avg_EHv3.1.2_AMR.tsv',
+df_amr = read.csv('~/Documents/STRs/ANALYSIS/population_research/EH_3.1.2_research_October2019_55419_genomes/unrelated_probands_and_cancer_RANDOM_400/AMR/merged/merged_population_genomes_unrelated_probands_and_cancer_384_avg_EHv3.1.2_AMR.tsv',
                   sep = '\t',
                   stringsAsFactors = F,
                   header = T) 
 dim(df_amr)
 # 965  12
 
-df_eur = read.csv('~/Documents/STRs/ANALYSIS/population_research/EH_3.1.2_research_October2019_55419_genomes/unrelated_probands_and_cancer/EUR/merged/merged_population_genomes_unrelated_probands_and_cancer_26033_avg_EHv3.1.2_EUR.tsv',
+df_eur = read.csv('~/Documents/STRs/ANALYSIS/population_research/EH_3.1.2_research_October2019_55419_genomes/unrelated_probands_and_cancer_RANDOM_400/EUR/merged/merged_population_genomes_unrelated_probands_and_cancer_26033_avg_EHv3.1.2_EUR.tsv',
                   sep = '\t',
                   stringsAsFactors = F,
                   header = T) 
 dim(df_eur)
-# 2282  12
+# 917  12
 
 
 df_asi = df_asi %>% mutate(population = "ASI")
@@ -90,10 +132,10 @@ df_all = rbind(df_afr,
                df_eas,
                df_asi)
 dim(df_all)
-# 6570  13
+# 4474  13
 
 # Population enriched genomes are only GRCh38, we will ignore then GRCh37
-output_folder = "./figures_unrelated_affected_or_cancer/"
+output_folder = "./figures_unrelated_affected_or_cancer_random_400/"
 
 l_loci = sort(unique(df_all$gene))
 for (i in 1:length(l_loci)){
