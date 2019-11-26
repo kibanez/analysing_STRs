d9e13e7773c5b5162191cac5c0750ea52bda403e
kristina ibanez garikano <kibanez@users.noreply.github.com>
2019-11-26 09:52:11 +0000

    plotting GRCh37 and GRCh38 repeats detected by EHdn

--- EHdn/plotting_STR_allele_frequency_EHdn.R
+++ EHdn/plotting_STR_allele_frequency_EHdn.R
@@ -16,17 +16,17 @@ setwd("~/Documents/STRs/ANALYSIS/EHdn/")
 
 # EHdn
 # We want to see the distribution of the number of repeats / WGS estimated by EHdn
-l_ehdn_b37 = list.files("~/Documents/STRs/ANALYSIS/EHdn/output_EHdn_v0.8.0/GRCh37/")
-l_ehdn_b38 = list.files("~/Documents/STRs/ANALYSIS/EHdn/output_EHdn_v0.8.0/GRCh38/")
+l_ehdn_b37 = list.files("~/Documents/STRs/ANALYSIS/EHdn/output_EHdn_v0.8.0/numberRepeats/GRCh37/")
+l_ehdn_b38 = list.files("~/Documents/STRs/ANALYSIS/EHdn/output_EHdn_v0.8.0/numberRepeats/GRCh38/")
 
-l_ehdn_b37 = paste("~/Documents/STRs/ANALYSIS/EHdn/output_EHdn_v0.8.0/GRCh37/", l_ehdn_b37, sep = "")
-l_ehdn_b38 = paste("~/Documents/STRs/ANALYSIS/EHdn/output_EHdn_v0.8.0/GRCh38/", l_ehdn_b38, sep = "")
+l_ehdn_b37 = paste("~/Documents/STRs/ANALYSIS/EHdn/output_EHdn_v0.8.0/numberRepeats/GRCh37/", l_ehdn_b37, sep = "")
+l_ehdn_b38 = paste("~/Documents/STRs/ANALYSIS/EHdn/output_EHdn_v0.8.0/numberRepeats/GRCh38/", l_ehdn_b38, sep = "")
 
 
 ## GRCh37
 df_eh_b37 = data.frame()
 for (i in 1:length(l_ehdn_b37)){
-  file_name = strsplit(l_ehdn_b37[i], "/")[[1]][8]
+  file_name = strsplit(l_ehdn_b37[i], "/")[[1]][9]
   id_name = strsplit(file_name, "\\.")[[1]][1]
   number_repeats = length(readLines(l_ehdn_b37[i]))
   df_eh_b37 = rbind(df_eh_b37, cbind(id_name, number_repeats))
@@ -48,7 +48,7 @@ dev.off()
 ## GRCh38
 df_eh_b38 = data.frame()
 for (i in 1:length(l_ehdn_b38)){
-  file_name = strsplit(l_ehdn_b38[i], "/")[[1]][8]
+  file_name = strsplit(l_ehdn_b38[i], "/")[[1]][9]
   id_name = strsplit(file_name, "\\.")[[1]][1]
   number_repeats = length(readLines(l_ehdn_b38[i]))
   df_eh_b38 = rbind(df_eh_b38, cbind(id_name, number_repeats))
@@ -68,4 +68,18 @@ ggplot(unique(df_eh_b38), aes(x = id_name, y = number_repeats)) +
 dev.off()
 
 
+# Let's plot GRCh37 and GRCh38 together and see whether there is a difference on EHdn performance depending on the genome build
+df_eh_b37$genome = rep("GRCh37", length(df_eh_b37$id_name))
+df_eh_b38$genome = rep("GRCh38", length(df_eh_b38$id_name))
 
+df_ehdn = rbind(df_eh_b37,
+                df_eh_b38)
+dim(df_ehdn)
+# 214  3
+
+png("figures/plot_numberRepeats_EHdn-v0.8.0_WGS_GRCh37_and_GRCh38.png")
+ggplot(df_ehdn, aes(x = id_name, y = number_repeats, color = genome)) +
+  geom_bar(stat = "identity") + 
+  ylab("number of repeats estimated by EHdn-v0.8.0") + 
+  xlab("214 validation WGS samples") 
+dev.off()
