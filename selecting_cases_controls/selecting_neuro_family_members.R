be7e835cd6c4161bcd94824247271dddd71c3ffd
kristina ibanez garikano <kibanez@users.noreply.github.com>
2019-11-26 17:24:39 +0000

    first commit

---
+++ selecting_cases_controls/selecting_neuro_family_members.R
@@ -0,0 +1,3 @@
+# Objective: Select those family members (all within the same family) that have been recruited under `neuro` 
+# Pilot and Main tables
+# `disease_group` = neuro + `disease_subgroup` = Mitochondrial
