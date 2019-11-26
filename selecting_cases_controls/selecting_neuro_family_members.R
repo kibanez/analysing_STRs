ca37427c076aba03d08ae303724a564f6a07c4d8
kristina ibanez garikano <kibanez@users.noreply.github.com>
2019-11-26 18:06:46 +0000

    follow the same strategy in main as in pilot. Only probands have populated disease info

--- selecting_cases_controls/selecting_neuro_family_members.R
+++ selecting_cases_controls/selecting_neuro_family_members.R
@@ -41,15 +41,25 @@ dim(translator_table)
 # 2632  3
 
 # 1 - Select from MAIN data all family members that have been assigned to have Neuro or Mito
-main_data_subset = main_data %>% 
+# Main dataset is special, ONLY proband has enriched or populated the `specificDisease` info. Thus, we first need to have the list of familyIDs to take later all members
+l_main_familyIds_neuro = main_data %>% 
   filter(disease_group %in% "Neurology and neurodevelopmental disorders" | disease_sub_group %in% "Mitochondrial disorders" | disease_sub_group %in% "mitochondrial") %>%
+  select(rare_diseases_family_id) %>%
+  unique() %>%
+  pull()
+length(l_main_familyIds_neuro)
+# 14075
+
+
+main_data_subset = main_data %>% 
+  filter(rare_diseases_family_id %in% l_main_familyIds_neuro) %>%
   select(participant_id, plate_key.x, rare_diseases_family_id, participant_type, affection_status, participant_phenotypic_sex, specific_disease, disease_group, disease_sub_group, genetic_vs_reported_results, genome_build)
 dim(main_data_subset)
-# 559243  11
+# 655006  11
 
 main_data_subset = unique(main_data_subset)
 dim(main_data_subset)
-# 17273  11
+# 35884  11
 
 # 2 - Select from PILOT data all family members that have been assigned to have Neuro or Mito
 # First we need to translate this from translator_table, to take the list of `specific_disease` we need to filter out from the pilot dataset
