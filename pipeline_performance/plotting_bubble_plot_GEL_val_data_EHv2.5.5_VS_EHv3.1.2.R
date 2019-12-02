6898a4083b71ef444231f6e6d66aaa13b7fd0023
kristina ibanez garikano <kibanez@users.noreply.github.com>
2019-12-02 09:53:07 +0000

    merge `val_data_v2` and `val_data_v3`

--- pipeline_performance/plotting_bubble_plot_GEL_val_data_EHv2.5.5_VS_EHv3.1.2.R
+++ pipeline_performance/plotting_bubble_plot_GEL_val_data_EHv2.5.5_VS_EHv3.1.2.R
@@ -30,6 +30,47 @@ val_data_v3 = read.csv("../EHv3_avg_VS_EHv3_maxCI/STRVALIDATION_ALLDATA_2019-10-
                     header = T,
                     stringsAsFactors = F)
 dim(val_data_v3)
-# 638  24
+# 638  19
 
+# Merge V2 and V3, after creating a new column with the EHversion
+val_data_v2$EH_version = rep("EH-v2.5.5", length(val_data_v2$locus_bioinfo))
+val_data_v3$EH_version = rep("EH-v3.1.2", length(val_data_v3$locus_bioinfo))
+
+# Merge
+col_intersected = intersect(colnames(val_data_v2), colnames(val_data_v3))
+val_data_v2 = val_data_v2 %>% select(col_intersected)
+val_data_v3 = val_data_v3 %>% select(col_intersected)
+
+val_data = rbind(val_data_v2,
+                 val_data_v3)
+dim(val_data)
+# 1276  16
+
+# Filter the good ones
+# 1 - Only keep with `Pileup_quality` == good or Good, `MISSING` and `blanks`
+val_data_v2 = val_data %>%
+  filter(Pileup_quality %in% "Good" | Pileup_quality %in% "good" | Pileup_quality %in% "" | Pileup_quality %in% "MISSING")
+dim(val_data)
+# 546  20
+
+# 2 - Only keep experimental val numbers (STR_a1, STR_a2) that are integer
+val_data = val_data %>%
+  filter((!STR_a1 %in% "positive"))
+dim(val_data)
+# 538  20
+
+val_data = val_data %>%
+  filter((!STR_a2 %in% "positive"))
+dim(val_data)
+# 538  20
+
+val_data = val_data %>%
+  filter((!STR_a1 %in% "na"))
+dim(val_data)
+# 538  20
+
+val_data = val_data %>%
+  filter((!STR_a2 %in% "na"))
+dim(val_data)
+# 520  20
 
