b3ebea331ab38c8b5908f7749d18cfe5436afe57
kristina ibanez garikano <kibanez@users.noreply.github.com>
2019-12-04 10:09:31 +0000

    setting working directory writing outputs into files

--- cases_controls/generating_cases_controls_table_per_locus_EHv2.5.5.R
+++ cases_controls/generating_cases_controls_table_per_locus_EHv2.5.5.R
@@ -9,6 +9,9 @@ R.version.string ## "R version 3.6.1 (2019-07-05)"
 # libraries
 library(dplyr)
 
+# Set working directory
+setwd("~/Documents/STRs/ANALYSIS/cases_controls/EHv2.5.5/")
+
 # Load data
 merged_data = read.csv("/Users/kibanez/Documents/STRs/data/research/EH_2.5.5_research_August2019/EH_output_v2.5.5_research_August_2019/merged_genotypeUpdated/merged_loci_86457_research_genomes_new_loci_EHv2.5.5_summer2019_removingListVcfFiles.tsv",
                        sep = '\t',
@@ -59,7 +62,13 @@ popu_table = read.csv("~/Documents/STRs/ANALYSIS/population_research/population_
                       sep = "\t",
                       stringsAsFactors = F)
 dim(popu_table)
-#
+# 59356  21
+
+clin_data = left_join(clin_data,
+                      popu_table %>% select(participant_id, population),
+                      by = "participant_id")
+dim(clin_data)
+# 1056611  17
 
 # As output
 # We want to the following output - NOTE each row is an allele (!!!)
@@ -100,20 +109,17 @@ l_genes = unique(merged_data$gene)
        # we write for each LP/sample/participant the same line/row, but also enriching with clinical data
        new_line = locus_data[j,c(1:10)]
        new_line$list_vcf_affected = number_samp[k]
-       
-       # sometimes we do have `_x2` for the sample, if 2 alleles are having the same repeat-size (GT = 1/1)
-       number_samp[k] = gsub("`_x2", "", number_samp[k])
-       number_samp[k] = gsub("`_2x", "", number_samp[k])
        number_samp[k] = gsub("_x2", "", number_samp[k])
+       
        to_include = clin_data %>% 
          filter(plate_key.x %in% number_samp[k]) %>% 
-         select(participant_id, plate_key.x, rare_diseases_family_id, specific_disease, disease_group, disease_sub_group, year_of_birth, participant_phenotypic_sex, biological_relationship_to_proband, affection_status, family_group_type, hpo_list, panel_list, programme, genome_build) %>%
+         select(participant_id, plate_key.x, rare_diseases_family_id, specific_disease, disease_group, disease_sub_group, year_of_birth, participant_phenotypic_sex, biological_relationship_to_proband, affection_status, family_group_type, hpo_list, panel_list, programme, genome_build, population) %>%
          unique()
        
        if (dim(to_include)[1] <= 0){
          to_include = rep('.', dim(to_include)[2])
          to_include = as.data.frame(t(as.data.frame(to_include)))
-         colnames(to_include) = c("participant_id", "plate_key.x", "rare_diseases_family_id", "specific_disease", "disease_group", "disease_sub_group", "year_of_birth", "participant_phenotypic_sex", "biological_relationship_to_proband", "affection_status", "family_group_type", "hpo_list", "panel_list", "programme", "genome_build")
+         colnames(to_include) = c("participant_id", "plate_key.x", "rare_diseases_family_id", "specific_disease", "disease_group", "disease_sub_group", "year_of_birth", "participant_phenotypic_sex", "biological_relationship_to_proband", "affection_status", "family_group_type", "hpo_list", "panel_list", "programme", "genome_build", "population")
        }
        new_line = cbind(new_line, to_include)
        locus_data_new = rbind(locus_data_new, new_line)
@@ -126,4 +132,22 @@ l_genes = unique(merged_data$gene)
    }#j
  }# dim(locus_data) > 0
  
+ # Write all `locus_data_new` output into a file
+ output_file = paste("table_STR_repeat_size_each_row_allele_EHv2.5.5", i, sep = "_")
+ output_file = paste(output_file, ".tsv" , sep = ".")
+ write.table(locus_data_new, output_file, sep = "\t", quote = F, row.names = F, col.names = T)
+ 
+ # Select interested columns
+ locus_data_new = locus_data_new %>%
+    select(rare_diseases_family_id, participant_id, plate_key.x, gene, Repeat_Motif, allele, specific_disease, disease_group, disease_sub_group, year_of_birth, participant_phenotypic_sex, biological_relationship_to_proband, affection_status, family_group_type, hpo_list, panel_list, programme, genome_build, population)
+    
+ # Adapt column names (for better understanding)
+ colnames(locus_data_new)[6] = "repeat_size"
+ output_file = paste("table_STR_repeat_size_each_row_allele_EHv2.5.5", i, sep = "_")
+ output_file = paste(output_file, "_simplified.tsv" , sep = "")
+ write.table(locus_data_new, output_file, sep = "\t", quote = F, row.names = F, col.names = T)
+ 
+ 
 #}
+ 
+ 
