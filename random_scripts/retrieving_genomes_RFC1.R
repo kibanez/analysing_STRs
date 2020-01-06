47932c1759da3c252cef3399b60861d1b06bfef7
kristina ibanez garikano <kibanez@users.noreply.github.com>
2020-01-06 13:50:08 +0100

    code for retrieving the unique list of genomes for EHv2.5.5

--- random_scripts/retrieving_genomes_RFC1.R
+++ random_scripts/retrieving_genomes_RFC1.R
@@ -10,3 +10,28 @@ library(dplyr)
 
 # Set working directory
 setwd("~/Documents/STRs/ANALYSIS/CANVAS_RFC1/")
+
+# Input merged VCF file for EHv2.5.5
+canvas_v2 = read.csv("~/Documents/STRs/data/research/EH_2.5.5_research_August2019/EH_output_v2.5.5_research_August_2019/merged_genotypeUpdated/merged_loci_86457_research_genomes_new_loci_EHv2.5.5_summer2019_removingListVcfFiles.tsv",
+                     sep = "\t",
+                     stringsAsFactors = F,
+                     header = T)
+dim(canvas_v2)
+# 3983  11
+
+canvas_v2 = canvas_v2 %>% filter(gene %in% "CANVAS_AAGGG")
+dim(canvas_v2)
+# 89  11
+
+# We need to retrieve now the list of platekeys within `list_samples` that are separated by ';'
+l_genomes_canvas_v2 = c()
+for (i in 1:length(canvas_v2$list_samples)){
+  aux_l_genomes = canvas_v2$list_samples[i]
+  l_genomes_canvas_v2 = c(l_genomes_canvas_v2,
+                          strsplit(aux_l_genomes, ';')[[1]])
+  
+}
+
+l_genomes_canvas_v2 = unique(l_genomes_canvas_v2)
+length(l_genomes_canvas_v2)
+# 7159
