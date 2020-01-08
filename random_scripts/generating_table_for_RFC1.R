06008b9ce766813636fd5ada785c1723a316229f
kristina ibanez garikano <kibanez@users.noreply.github.com>
2020-01-08 15:49:06 +0000

    enriching genomes with path, genome build for EHv2.5.5

--- random_scripts/generating_table_for_RFC1.R
+++ random_scripts/generating_table_for_RFC1.R
@@ -11,3 +11,40 @@ library(dplyr)
 # Set working directory
 setwd("~/Documents/STRs/ANALYSIS/CANVAS_RFC1/")
 
+# Upload latest clinical data
+clin_data = read.csv("~/Documents/STRs/clinical_data/clinical_data/rd_genomes_all_data_041219.tsv",
+                     stringsAsFactors = F,
+                     sep = "\t",
+                     header = TRUE)
+dim(clin_data)
+# 1124633  28
+
+clin_data = clin_data %>% select(platekey, file_path, genome_build)
+clin_data = unique(clin_data)
+dim(clin_data)
+# 111148  3
+
+
+# Upload the list of genomes for EHv2.5.5
+list_ehv2 = read.table("list_7159_genomes_RFC1_EHv2.5.5.tsv", stringsAsFactors = F)
+list_ehv2 = list_ehv2$V1
+length(list_ehv2)
+# 7159
+
+list_ehv2 = gsub('.{4}$', '', list_ehv2)
+list_ehv2 = gsub('^.{3}', '', list_ehv2)
+
+clin_data_ehv2 = clin_data %>% 
+  filter(platekey %in% list_ehv2)
+dim(clin_data_ehv2)
+# 7548  2
+
+clin_data_ehv2 = unique(clin_data_ehv2)
+dim(clin_data_ehv2)
+
+write.table(clin_data_ehv2,
+            "table_7548_genomes_for_7159_unique_genomes_RFC1_EHv2.5.5.tsv",
+            quote = F,
+            row.names = F,
+            col.names = T,
+            sep = "\t")
