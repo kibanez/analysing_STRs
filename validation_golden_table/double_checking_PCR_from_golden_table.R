55805f5571396738b3bbe59d48970114d0be19d2
kristina ibanez garikano <kibanez@users.noreply.github.com>
2020-01-09 14:47:31 +0000

    line by line correcting or double checking PCR estimates

--- validation_golden_table/double_checking_PCR_from_golden_table.R
+++ validation_golden_table/double_checking_PCR_from_golden_table.R
@@ -13,12 +13,12 @@ library(dplyr); packageDescription ("dplyr", fields = "Version") #"0.8.3"
 setwd("/Users/kibanez/Documents/STRs/VALIDATION/")
 
 # Load tables
-paper_table = read.csv("EHv255_EHv312_validation_cohort_GEL_and_ILMN.tsv",
+paper_table = read.csv("EHv255_EHv312_validation_cohort_GEL.tsv",
                        stringsAsFactors = F,
                        header = T,
                        sep = "\t")
 dim(paper_table)
-# 803  30
+# 638  28
 
 # Note: this table contains 3 duplicates
 orig_table = read.csv("STRVALIDATION_ALLDATA_2019-10-7_ALL_kibanez_enriched.tsv",
@@ -28,9 +28,33 @@ orig_table = read.csv("STRVALIDATION_ALLDATA_2019-10-7_ALL_kibanez_enriched.tsv"
 dim(orig_table)
 # 638  20
 
-
 new_paper_table = paper_table
-new_paper_table$exp_PCR_a1_checked = c("", length(new_paper_table$locus))
-new_paper_table$exp_PCR_a2_checked = c("", length(new_paper_table$locus))
+new_paper_table$exp_PCR_a1_checked = rep("", length(new_paper_table$locus))
+new_paper_table$exp_PCR_a2_checked = rep("", length(new_paper_table$locus))
 
+for(i in 1:length(new_paper_table$locus)){
+  locus = new_paper_table$locus[i]
+  platekey = new_paper_table$LP_number[i]
+  
+  new_exp_PCR_a1 = orig_table %>% filter(LP_Number %in% platekey, loci %in% locus) %>% select(STR_a1) %>% pull() 
+  new_exp_PCR_a2 = orig_table %>% filter(LP_Number %in% platekey, loci %in% locus) %>% select(STR_a2) %>% pull() 
+  
+  if (length(new_exp_PCR_a1) > 1){
+    new_exp_PCR_a1 = new_exp_PCR_a1[1]
+  }else if (length(new_exp_PCR_a1 )){
+    next
+  }
+  if (length(new_exp_PCR_a2) > 1){
+    new_exp_PCR_a2 = new_exp_PCR_a2[1]
+  }
 
+    new_paper_table$exp_PCR_a1_checked = new_exp_PCR_a1
+  new_paper_table$exp_PCR_a2_checked = new_exp_PCR_a2
+}
+
+write.table(new_paper_table,
+            "EHv255_EHv312_validation_cohort_GEL_PCR_estimates_double_checked.tsv",
+            quote = F,
+            row.names = F,
+            col.names = T,
+            sep = "\t")
