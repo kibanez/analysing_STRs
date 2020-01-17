4688790f5f3e8207142c80d6a9488c838ec50dda
kristina ibanez garikano <kibanez@users.noreply.github.com>
2020-01-17 18:07:04 +0000

    first commit with the script for running dragen CRAM files

---
+++ validation_golden_table/settingUp_dragen_runs.R
@@ -0,0 +1,4 @@
+# Objective: setup the input file with the CRAM files we do have frmo dragen v3.2
+# All have been realigned to GRCh38, in CRAM format
+
+/genomes/scratch/kgarikano/GEL_STR/Validation_golden_table/input/list_FXN_failing_validation.tsv
