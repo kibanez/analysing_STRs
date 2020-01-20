5c28d3401a30148e18ef12145c50da3a4e7ea58b
kristina ibanez garikano <kibanez@users.noreply.github.com>
2020-01-20 17:18:40 +0000

    comment code create directory where cancer_and_RD new tables will be

--- population_research/transforming_tables_by_genomes_to_alleles.R
+++ population_research/transforming_tables_by_genomes_to_alleles.R
@@ -21,7 +21,14 @@ source("~/git/analysing_STRs/functions/transforming_genomeRow_to_alleleRow.R")
 #working_dir="~/Documents/STRs/ANALYSIS/population_research/EH_3.1.2_research_October2019_55419_genomes/unrelated_probands_and_cancer/AMR/"
 #working_dir="~/Documents/STRs/ANALYSIS/population_research/EH_3.1.2_research_October2019_55419_genomes/unrelated_probands_and_cancer/EAS/"
 #working_dir="~/Documents/STRs/ANALYSIS/population_research/EH_3.1.2_research_October2019_55419_genomes/unrelated_probands_and_cancer/ASI/"
-working_dir="~/Documents/STRs/ANALYSIS/population_research/EH_3.1.2_research_October2019_55419_genomes/unrelated_probands_and_cancer/EUR/"
+#working_dir="~/Documents/STRs/ANALYSIS/population_research/EH_3.1.2_research_October2019_55419_genomes/unrelated_probands_and_cancer/EUR/"
+
+# unrelated RANDOM 400
+#working_dir="~/Documents/STRs/ANALYSIS/population_research/EH_3.1.2_research_October2019_55419_genomes/unrelated_probands_and_cancer_RANDOM_400/AFR/"
+#working_dir="~/Documents/STRs/ANALYSIS/population_research/EH_3.1.2_research_October2019_55419_genomes/unrelated_probands_and_cancer_RANDOM_400/AMR/"
+#working_dir="~/Documents/STRs/ANALYSIS/population_research/EH_3.1.2_research_October2019_55419_genomes/unrelated_probands_and_cancer_RANDOM_400/EAS/"
+#working_dir="~/Documents/STRs/ANALYSIS/population_research/EH_3.1.2_research_October2019_55419_genomes/unrelated_probands_and_cancer_RANDOM_400/ASI"
+working_dir="~/Documents/STRs/ANALYSIS/population_research/EH_3.1.2_research_October2019_55419_genomes/unrelated_probands_and_cancer_RANDOM_400/EUR"
 setwd(working_dir)
 
 # Load data
@@ -29,13 +36,22 @@ setwd(working_dir)
 #merged_file = "~/Documents/STRs/ANALYSIS/population_research/EH_3.1.2_research_October2019_55419_genomes/unrelated_probands_and_cancer/AMR/merged/merged_population_genomes_unrelated_probands_and_cancer_384_avg_EHv3.1.2_AMR.tsv"
 #merged_file = "~/Documents/STRs/ANALYSIS/population_research/EH_3.1.2_research_October2019_55419_genomes/unrelated_probands_and_cancer/EAS/merged/merged_population_genomes_unrelated_probands_and_cancer_227_avg_EHv3.1.2_EAS.tsv"
 #merged_file = "~/Documents/STRs/ANALYSIS/population_research/EH_3.1.2_research_October2019_55419_genomes/unrelated_probands_and_cancer/ASI/merged/merged_population_genomes_unrelated_probands_and_cancer_2678_avg_EHv3.1.2_ASI.tsv"
-merged_file = "~/Documents/STRs/ANALYSIS/population_research/EH_3.1.2_research_October2019_55419_genomes/unrelated_probands_and_cancer/EUR/merged/merged_population_genomes_unrelated_probands_and_cancer_26033_avg_EHv3.1.2_EUR.tsv"
+#merged_file = "~/Documents/STRs/ANALYSIS/population_research/EH_3.1.2_research_October2019_55419_genomes/unrelated_probands_and_cancer/EUR/merged/merged_population_genomes_unrelated_probands_and_cancer_26033_avg_EHv3.1.2_EUR.tsv"
+
+
+# unrelated RANDOM 400
+#merged_file = "~/Documents/STRs/ANALYSIS/population_research/EH_3.1.2_research_October2019_55419_genomes/unrelated_probands_and_cancer_RANDOM_400/AFR/merged/merged_population_genomes_unrelated_probands_and_cancer_1136_avg_EHv3.1.2_AFR.tsv"
+#merged_file = "~/Documents/STRs/ANALYSIS/population_research/EH_3.1.2_research_October2019_55419_genomes/unrelated_probands_and_cancer_RANDOM_400/AMR/merged/merged_population_genomes_unrelated_probands_and_cancer_384_avg_EHv3.1.2_AMR.tsv"
+#merged_file = "~/Documents/STRs/ANALYSIS/population_research/EH_3.1.2_research_October2019_55419_genomes/unrelated_probands_and_cancer_RANDOM_400/EAS/merged/merged_population_genomes_unrelated_probands_and_cancer_227_avg_EHv3.1.2_EAS.tsv"
+#merged_file = "~/Documents/STRs/ANALYSIS/population_research/EH_3.1.2_research_October2019_55419_genomes/unrelated_probands_and_cancer_RANDOM_400/ASI/merged/merged_population_genomes_unrelated_probands_and_cancer_2678_avg_EHv3.1.2_ASI.tsv"
+merged_file = "~/Documents/STRs/ANALYSIS/population_research/EH_3.1.2_research_October2019_55419_genomes/unrelated_probands_and_cancer_RANDOM_400/EUR/merged/merged_population_genomes_unrelated_probands_and_cancer_26033_avg_EHv3.1.2_EUR.tsv"
 
 # Transform genome-row tables to allele-row tables
 transform_genome_to_allele_table(working_dir,merged_file)
 
 list_files_tables = sort(list.files(pattern = "\\_simplified.tsv$"))
 
+dir.create("cancer_and_RD")
 # Once the above function creates for each locus a table having an allele info per row, we will proceed to check for duplicates 
 for (i in 1:length(list_files_tables)){
   merged_data = read.csv(list_files_tables[i],
@@ -47,7 +63,9 @@ for (i in 1:length(list_files_tables)){
   # Keep with Cancer and RD programmes, filter out `.`
   merged_data = merged_data %>% filter(!programme %in% ".")
  
+  
   output_file = paste(strsplit(list_files_tables[i], ".tsv")[[1]], "cancer_and_RD.tsv", sep = "_")
+  output_file = paste("cancer_and_RD", output_file, sep = "/")
   write.table(merged_data,
               output_file,
               quote = F,
