28ca51d9cca99d3ed4dce7be3cbddb2c646155d4
kristina ibanez garikano <kibanez@users.noreply.github.com>
2020-01-21 14:22:11 +0000

    adapt the script for forencics markers

--- population_research/computing_stat_signif_across_ancestries_forensics.R
+++ population_research/computing_stat_signif_across_ancestries_forensics.R
@@ -27,66 +27,68 @@ dir.create("analysis/")
 # We need to do this by locus
 # Plot violing-plots as well
 for (i in 1:length(l_loci)){
+  
+  locus_name = l_loci[i]
 
   # AFR table
-  afr_file = paste("AFR/cancer_and_RD/table_STR_repeat_size_each_row_allele_", l_loci[i], sep = "")
-  afr_file = paste(afr_file, "_simplified_cancer_and_RD.tsv", sep = "")
-  
-  afr_table = read.csv(afr_file,
+  afr_table = read.csv("AFR/merged/merged_forensics_loci_1777_AFR_HipSTRv0.6.2.tsv",
                        sep = "\t",
                        stringsAsFactors = F,
                        header = T)
   
   afr_table = afr_table %>%
-    select(population, repeat_size)
+    filter(gene %in% locus_name) %>%
+    select(repeat.size)
+  
+  afr_table$population = rep("AFR", length(afr_table$repeat.size))
   
   # AMR table
-  amr_file = paste("AMR/cancer_and_RD/table_STR_repeat_size_each_row_allele_", l_loci[i], sep = "")
-  amr_file = paste(amr_file, "_simplified_cancer_and_RD.tsv", sep = "")
-  
-  amr_table = read.csv(amr_file,
+  amr_table = read.csv("AMR/merged/merged_forensics_loci_797_AMR_HipSTRv0.6.2.tsv",
                        sep = "\t",
                        stringsAsFactors = F,
                        header = T)
   
   amr_table = amr_table %>%
-    select(population, repeat_size)
+    filter(gene %in% locus_name) %>%
+    select(repeat.size)
+  
+  amr_table$population = rep("AMR", length(amr_table$repeat.size))
   
   # ASI table
-  asi_file = paste("ASI/cancer_and_RD/table_STR_repeat_size_each_row_allele_", l_loci[i], sep = "")
-  asi_file = paste(asi_file, "_simplified_cancer_and_RD.tsv", sep = "")
-  
-  asi_table = read.csv(asi_file,
+  asi_table = read.csv("ASI/merged/merged_forensics_loci_5947_ASI_HipSTRv0.6.2.tsv",
                        sep = "\t",
                        stringsAsFactors = F,
                        header = T)
   
   asi_table = asi_table %>%
-    select(population, repeat_size)
+    filter(gene %in% locus_name) %>%
+    select(repeat.size)
+  
+  asi_table$population = rep("ASI", length(asi_table$repeat.size))
   
   # EAS table
-  eas_file = paste("EAS/cancer_and_RD/table_STR_repeat_size_each_row_allele_", l_loci[i], sep = "")
-  eas_file = paste(eas_file, "_simplified_cancer_and_RD.tsv", sep = "")
-  
-  eas_table = read.csv(eas_file,
+  eas_table = read.csv("EAS/merged/merged_forensics_loci_400_EAS_HipSTRv0.6.2.tsv",
                        sep = "\t",
                        stringsAsFactors = F,
                        header = T)
   
   eas_table = eas_table %>%
-    select(population, repeat_size)
+    filter(gene %in% locus_name) %>%
+    select(repeat.size)
+  
+  eas_table$population = rep("EAS", length(eas_table$repeat.size))
   
   # EUR table
-  eur_file = paste("EUR/cancer_and_RD/table_STR_repeat_size_each_row_allele_", l_loci[i], sep = "")
-  eur_file = paste(eur_file, "_simplified_cancer_and_RD.tsv", sep = "")
-  
-  eur_table = read.csv(eur_file,
+  eur_table = read.csv("EUR/merged/merged_forensics_loci_46883_EUR_HipSTRv0.6.2.tsv",
                        sep = "\t",
                        stringsAsFactors = F,
                        header = T)
   
   eur_table = eur_table %>%
-    select(population, repeat_size)
+    filter(gene %in% locus_name) %>%
+    select(repeat.size)
+  
+  eur_table$population = rep("EUR", length(eur_table$repeat.size))
   
   # Merged table
   merged_table = rbind(afr_table,
@@ -115,33 +117,33 @@ for (i in 1:length(l_loci)){
 #  - Some fields of science tend to prefer the KS test over the MW test. It makes sense to follow the traditions of your field.
 
 
-  violin_plot = ggplot(merged_table, aes(x = population, y=repeat_size, fill = population)) +
+  violin_plot = ggplot(merged_table, aes(x = population, y=repeat.size, fill = population)) +
     geom_violin() +
     xlab("Population") + 
     ylab("Repeat sizes (repeat units)") + 
-    ggtitle(l_loci[i]) 
+    ggtitle(locus_name) 
   
   
   my_comparisons <- list( c("AFR", "AMR"), c("AFR", "ASI"), c("AFR", "EAS"), c("AFR", "EUR"),
                           c("AMR", "ASI"), c("AMR", "EAS"), c("AMR", "EUR"))
   
   # Pairwise t-test between groups
-  stat.test <- compare_means(repeat_size ~ population, 
+  stat.test <- compare_means(repeat.size ~ population, 
                              data = merged_table,
                              paired = FALSE,
                              method = "wilcox.test",
-                             p.adjust.method = "bonferroni",
-                             alternative = "great") %>%
+                             p.adjust.method = "bonferroni") %>%
+                             #alternative = "great") %>%
     mutate(y.position = c(51, 53, 55, 57, 59, 61, 63, 65, 67, 69))
   
-  violin_plot_with_stat = ggviolin(merged_table, x = "population", y = "repeat_size", fill = "population") +
+  violin_plot_with_stat = ggviolin(merged_table, x = "population", y = "repeat.size", fill = "population") +
     stat_pvalue_manual(
       #data = stat.test, label = "p.adj",
       data = stat.test, label = "p.signif",
       xmin = "group1", xmax = "group2",
       y.position = "y.position") 
   
-  violin_plot_with_stat_with_boxplot = ggviolin(merged_table, x = "population", y = "repeat_size", fill = "population",
+  violin_plot_with_stat_with_boxplot = ggviolin(merged_table, x = "population", y = "repeat.size", fill = "population",
                                                 add = "boxplot", add.params = list(fill = "black")) +
     stat_pvalue_manual(
       #data = stat.test, label = "p.adj",
