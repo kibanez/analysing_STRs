cbb158e480b561cedb8ce91fb6d3dacfeb4cd745
kristina ibanez garikano <kibanez@users.noreply.github.com>
2020-01-22 10:44:19 +0000

    loading clinical data, pilot and main

--- EHdn/case-control/defining_cases_controls_PAT.R
+++ EHdn/case-control/defining_cases_controls_PAT.R
@@ -1,1 +1,26 @@
 # Objective: define and track here the modus operandi when selecting samples for cases and controls for PILOT adult ataxia
+date ()
+Sys.info ()[c("nodename", "user")]
+commandArgs ()
+rm (list = ls ())
+R.version.string ## "R version 3.6.1 (2019-07-05)"
+
+# libraries
+library(dplyr)
+
+# defining working directory
+setwd("~/Documents/STRs/ANALYSIS/EHdn/EHdn-v0.8.6/case-control/")
+
+# in this case, the first analysis we will perform is called PAT: pilot adult ataxia 
+pilot_clin_data = read.csv("~/Documents/STRs/clinical_data/pilot_clinical_data/pilot_cohort_clinical_data_4833_genomes_removingPanels_280919.tsv",
+                           sep = "\t",
+                           stringsAsFactors = F,
+                           header = T)
+dim(pilot_clin_data)
+
+main_clin_data = read.csv("~/Documents/STRs/clinical_data/clinical_data/rd_genomes_all_data_041219.tsv",
+                          sep = "\t",
+                          stringsAsFactors = F,
+                          header = T)
+dim(main_clin_data)
+# 
