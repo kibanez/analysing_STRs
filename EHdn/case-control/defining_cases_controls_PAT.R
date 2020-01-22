06908ca50f54c02d7df0c4960bbbdc3d5730d17d
kristina ibanez garikano <kibanez@users.noreply.github.com>
2020-01-22 11:02:51 +0000

    defining cases samples in pilot and GRCh37 main datasets

--- EHdn/case-control/defining_cases_controls_PAT.R
+++ EHdn/case-control/defining_cases_controls_PAT.R
@@ -23,4 +23,34 @@ main_clin_data = read.csv("~/Documents/STRs/clinical_data/clinical_data/rd_genom
                           stringsAsFactors = F,
                           header = T)
 dim(main_clin_data)
-# 
+# 1124633  28
+
+# Cases
+# - ONLY probands
+# - YOB < 2000 (i.e. adults)
+# - specific disease in ataxia OR neuropathies
+# - for `main` take GRCh37
+
+pilot_cases = pilot_clin_data %>%
+  filter(yearOfBirth < 2000, 
+         (grepl("neuropathies", specificDisease) | grepl("ataxia", specificDisease)), 
+         biological_relation_to_proband %in% "Proband")
+dim(pilot_cases)
+# 154  10
+
+length(unique(pilot_cases$plateKey))
+# 153
+
+
+main_cases = main_clin_data %>%
+  filter(year_of_birth < 2000,
+         participant_type %in% "Proband",
+         !grepl("Neurology", main_clin_data$disease_group),
+         genome_build %in% "GRCh37")
+dim(main_cases)
+# 16711  28
+
+length(unique(main_cases$platekey))
+# 1410
+
+
