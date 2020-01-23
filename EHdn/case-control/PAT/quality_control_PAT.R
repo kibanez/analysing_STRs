429ebdb9c727066872661eda184146a2a8bb576d
kristina ibanez garikano <kibanez@users.noreply.github.com>
2020-01-23 10:52:58 +0000

    loading pilot and main clinical data

--- EHdn/case-control/PAT/quality_control_PAT.R
+++ EHdn/case-control/PAT/quality_control_PAT.R
@@ -11,3 +11,19 @@ library(ggplot2)
 
 # defining working directory
 setwd("~/Documents/STRs/ANALYSIS/EHdn/EHdn-v0.8.6/case-control/PAT/")
+
+# pilot clinical data
+pilot_clin_data = read.csv("~/Documents/STRs/clinical_data/pilot_clinical_data/pilot_cohort_clinical_data_4833_genomes_removingPanels_280919.tsv",
+                           sep = "\t",
+                           stringsAsFactors = F,
+                           header = T)
+dim(pilot_clin_data)
+# 4974  10
+
+# main clinical data
+main_clin_data = read.csv("~/Documents/STRs/clinical_data/clinical_data/rd_genomes_all_data_041219.tsv",
+                          sep = "\t",
+                          stringsAsFactors = F,
+                          header = T)
+dim(main_clin_data)
+# 1124633  28
