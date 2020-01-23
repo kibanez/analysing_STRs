6f2acfcb34d5bbf42bb0871333e13b94213c20f0
kristina ibanez garikano <kibanez@users.noreply.github.com>
2020-01-23 11:27:38 +0000

    fixing selected cases/controls from pilot/main oNLY selecting those that have been selected from `defining_cases_controls_PAT.R`

--- EHdn/case-control/PAT/quality_control_PAT.R
+++ EHdn/case-control/PAT/quality_control_PAT.R
@@ -17,10 +17,20 @@ load("PAT_case_control_environment.Rdata")
 
 # Pilot and Main cases - taking platekey, sex and age
 selected_pilot_cases = pilot_cases %>% 
+  filter(plateKey %in% l_pilot_cases) %>%
   select(plateKey, sex, yearOfBirth)
+dim(selected_pilot_cases)
+# 194  3
 
 selected_main_cases = main_cases %>%
+  filter(platekey %in% l_main_cases) %>%
   select(platekey, participant_phenotypic_sex, year_of_birth)
+dim(selected_main_cases)
+# 59432  3
+
+selected_main_cases = unique(selected_main_cases)
+dim(selected_main_cases)
+# 1427  3
 
 colnames(selected_pilot_cases) = c("platekey", "sex", "age")
 colnames(selected_main_cases) = c("platekey", "sex", "age")
@@ -29,17 +39,52 @@ selected_cases = rbind(selected_pilot_cases,
                        selected_main_cases)
 
 dim(selected_cases)
-# 59626  3
+# 1621 3
 
 # Pilot and Main controls
 selected_pilot_controls = pilot_controls %>%
+  filter(plateKey %in% l_pilot_controls) %>%
   select(plateKey, sex, yearOfBirth)
+dim(selected_pilot_controls)
+# 1145  3
+
+selected_pilot_controls = unique(selected_pilot_controls)
+dim(selected_pilot_controls)
+# 1113  3
 
 selected_main_controls = main_controls %>%
+  filter(platekey %in% l_main_controls) %>%
   select(platekey, participant_phenotypic_sex, year_of_birth)
+dim(selected_main_controls)
+# 48648  3
+
+selected_main_controls = unique(selected_main_controls)
+dim(selected_main_controls)
+# 2371  3
 
 colnames(selected_pilot_controls) = c("platekey", "sex", "age")
 colnames(selected_main_controls) = c("platekey", "sex", "age")
 
 selected_controls = rbind(selected_pilot_controls,
                           selected_main_controls)
+dim(selected_controls)
+# 3483  3
+
+# Define each `selected_cases` and `selected_controls` with the group name
+selected_cases$group = rep("case", length(selected_cases$platekey))
+selected_controls$group = rep("control", length(selected_controls$platekey))
+
+merged_pat = rbind(selected_cases,
+                   selected_controls)
+
+dim(merged_pat)
+# 109419 4
+
+
+# Plot age distribution
+ggplot()
+
+
+ggplot(data,aes(x=value, fill=variable)) + geom_density(alpha=0.25)
+ggplot(data,aes(x=value, fill=variable)) + geom_histogram(alpha=0.25)
+ggplot(data,aes(x=variable, y=value, fill=variable)) + geom_boxplot()
