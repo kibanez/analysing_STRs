f9b6009dfbeed5e1dba6b571153c5548ae0ec0c2
kristina ibanez garikano <kibanez@users.noreply.github.com>
2020-01-23 11:03:42 +0000

    define selected_cases and selected_controls from main and pilot datasets

--- EHdn/case-control/PAT/quality_control_PAT.R
+++ EHdn/case-control/PAT/quality_control_PAT.R
@@ -15,4 +15,31 @@ setwd("~/Documents/STRs/ANALYSIS/EHdn/EHdn-v0.8.6/case-control/PAT/")
 # Load PAT case-control environment
 load("PAT_case_control_environment.Rdata")
 
-#
+# Pilot and Main cases - taking platekey, sex and age
+selected_pilot_cases = pilot_cases %>% 
+  select(plateKey, sex, yearOfBirth)
+
+selected_main_cases = main_cases %>%
+  select(platekey, participant_phenotypic_sex, year_of_birth)
+
+colnames(selected_pilot_cases) = c("platekey", "sex", "age")
+colnames(selected_main_cases) = c("platekey", "sex", "age")
+
+selected_cases = rbind(selected_pilot_cases,
+                       selected_main_cases)
+
+dim(selected_cases)
+# 59626  3
+
+# Pilot and Main controls
+selected_pilot_controls = pilot_controls %>%
+  select(plateKey, sex, yearOfBirth)
+
+selected_main_controls = main_controls %>%
+  select(platekey, participant_phenotypic_sex, year_of_birth)
+
+colnames(selected_pilot_controls) = c("platekey", "sex", "age")
+colnames(selected_main_controls) = c("platekey", "sex", "age")
+
+selected_controls = rbind(selected_pilot_controls,
+                          selected_main_controls)
