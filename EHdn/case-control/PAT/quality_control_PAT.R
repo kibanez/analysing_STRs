f6c2b5693d004bf482392077b6f718999450cd2c
kristina ibanez garikano <kibanez@users.noreply.github.com>
2020-01-23 10:50:34 +0000

    new script for quality control checks

---
+++ EHdn/case-control/PAT/quality_control_PAT.R
@@ -0,0 +1,13 @@
+# Objective: to see age distribution across case and control datasets
+date ()
+Sys.info ()[c("nodename", "user")]
+commandArgs ()
+rm (list = ls ())
+R.version.string ## "R version 3.6.1 (2019-07-05)"
+
+# libraries
+library(dplyr)
+library(ggplot2)
+
+# defining working directory
+setwd("~/Documents/STRs/ANALYSIS/EHdn/EHdn-v0.8.6/case-control/PAT/")
