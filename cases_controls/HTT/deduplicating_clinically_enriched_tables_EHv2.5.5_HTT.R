3633216a7710eaf0522e000aa737f6a3184ec986
kristina ibanez garikano <kibanez@users.noreply.github.com>
2020-02-04 17:09:32 +0000

    update numbers and double check the code for EH2.5.5

--- cases_controls/HTT/deduplicating_clinically_enriched_tables_EHv2.5.5_HTT.R
+++ cases_controls/HTT/deduplicating_clinically_enriched_tables_EHv2.5.5_HTT.R
@@ -28,50 +28,50 @@ list_spec_disease = merged_data %>%
   group_by(participant_id) %>% 
   summarise(spec_disease_list = toString(specific_disease)) %>% ungroup() %>% as.data.frame()
 dim(list_spec_disease)
-# 76148  2
+# 75095  2
 
 list_disease_group = merged_data %>% 
   group_by(participant_id) %>% 
   summarise(disease_group_list = toString(disease_group)) %>% ungroup() %>% as.data.frame()
 dim(list_disease_group)
-# 76148  2
+# 75095  2
 
 list_disease_subgroup = merged_data %>% 
   group_by(participant_id) %>% 
   summarise(disease_subgroup_list = toString(disease_sub_group)) %>% ungroup() %>% as.data.frame()
 dim(list_disease_subgroup)
-# 76148  2
+# 75095  2
 
 merged_data = left_join(merged_data,
                       list_spec_disease,
                       by = "participant_id")
 dim(merged_data)
-# 169402  20
+# 177316  20
 
 merged_data = left_join(merged_data,
                         list_disease_group,
                         by = "participant_id")
 dim(merged_data)
-# 169402     21
+# 177316     21
 
 merged_data = left_join(merged_data,
                         list_disease_subgroup,
                         by = "participant_id")
 dim(merged_data)
-# 169402     22
+# 177316     22
 
 # let's remove the other columns now
 merged_data = merged_data[,-c(7:9)]
 dim(merged_data)
-# 169402  19
+# 177316  19
 
 
 # There are some participants for which there are several genomes/platekeys 
 # we will take/select the latest one
 merged_data %>% select(participant_id) %>% unique() %>% pull() %>% length()
-# 72725
+# 75095
 merged_data %>% select(platekey) %>% unique() %>% pull() %>% length()
-# 81095 
+# 84917
 
 # First, select duplicated genomes
 duplicated_genomes = merged_data %>% 
@@ -79,10 +79,10 @@ duplicated_genomes = merged_data %>%
   filter(n()>2)
 
 duplicated_genomes %>% select(participant_id) %>% unique() %>% pull() %>% length()
-# 9802 genomes that have >1 genome
+# 11246 genomes that have >1 genome
 l_duplicated_genomes = unique(duplicated_genomes$participant_id)
 length(l_duplicated_genomes)
-# 9802
+# 11246
 
 df_pid_platekey = merged_data %>% 
   filter(participant_id %in% l_duplicated_genomes) %>% 
@@ -98,18 +98,18 @@ df_pid_platekey = df_pid_platekey %>%
 # select latest genomes
 l_latest_dedup_platekeys = unique(df_pid_platekey$latest_platekey)
 length(l_latest_dedup_platekeys)
-# 9802 (== dedup participant ids)
+# 11246 (== dedup participant ids)
 
 # PART 1 - select pid not duplicated (to merge with the rest afterwards)
 merged_data_dedup = merged_data %>% filter(!participant_id %in% l_duplicated_genomes)
 dim(merged_data_dedup)
-# 125846  19
+# 127698  19
 
 # PART 2 - include the genomes recovered from the duplicated genomes
 merged_data_dedup = rbind(merged_data_dedup,
                           merged_data %>% filter(platekey %in% l_latest_dedup_platekeys, genome_build %in% "GRCh38"))
 dim(merged_data_dedup)
-# 131104  19
+# 133110  19
 
 # so far, we have removed duplicates having more than 1 genome
 # QUALITY CHECK - check whether there are pids with more than 3 rows
@@ -117,7 +117,7 @@ merged_data_dedup %>%
   group_by(participant_id) %>% 
   filter(n()>2) %>%
   dim()
-# 1900  19
+# 1960  19
 
 l_duplicated_genomes2 = merged_data_dedup %>% 
   group_by(participant_id) %>% 
@@ -126,7 +126,7 @@ l_duplicated_genomes2 = merged_data_dedup %>%
   pull() %>%
   unique()
 length(l_duplicated_genomes2)
-# 461
+# 476
 
 # Take the largets repeat-size
 df_pid_platekey_repeatsize = merged_data_dedup %>% 
@@ -147,19 +147,19 @@ df_aux_recover = merged_data_dedup %>% filter(participant_id %in% l_duplicated_g
 # First, remove all genomes in l_genomes_duplciated2
 merged_data_dedup = merged_data_dedup %>% filter(!participant_id %in% l_duplicated_genomes2)
 dim(merged_data_dedup)
-# 129204  19
+# 131150  19
 
 
 df_aux_recover = df_aux_recover %>% group_by(participant_id) %>% mutate(large_repeat = max(repeat_size)) %>% ungroup() %>% as.data.frame()
 index_to_keep = which(df_aux_recover$large_repeat == df_aux_recover$repeat_size)
 df_aux_recover = df_aux_recover[index_to_keep,]
 dim(df_aux_recover)
-# 1133  20
+# 1140  20
 
 # remove last column
 df_aux_recover = df_aux_recover[,-20]
 dim(df_aux_recover)
-# 1133  19
+# 1140  19
 
 # There are some genomes in GRCh37 and GRCh38, let's take GRCh38 info
 l_genomes_both_builds = df_aux_recover %>%
@@ -170,25 +170,28 @@ l_genomes_both_builds = df_aux_recover %>%
   unique() %>%
   pull()
 
+length(l_genomes_both_builds)
+# 100
+
 df_aux_recover = rbind(df_aux_recover %>% 
                          filter(!participant_id %in% l_genomes_both_builds),
                        df_aux_recover %>%
                          filter(participant_id %in% l_genomes_both_builds, genome_build %in% "GRCh38"))
 
 dim(df_aux_recover)
-# 1133  19
+# 1140  19
 
 merged_data_dedup = rbind(merged_data_dedup,
                           df_aux_recover)
 dim(merged_data_dedup)
-#  130337  19
+#  132290  19
 
 # QUALITY CHECK
 merged_data_dedup %>% 
   group_by(participant_id) %>% 
   filter(n()>2) %>%
   dim()
-# 437  19
+# 388  19
 
 aver = merged_data_dedup %>% 
   group_by(participant_id) %>% 
@@ -196,8 +199,9 @@ aver = merged_data_dedup %>%
   ungroup() %>%
   as.data.frame()
 length(unique(aver$participant_id))
-# 113
-# there are 113 participant-genome rows with too many rows...let's simplify them
+# 100
+
+# there are 100 participant-genome rows with too many rows...let's simplify them
 
 l_parti_too_many_rows = unique(aver$participant_id)
 merged_data_dedup_final = merged_data_dedup %>% 
@@ -205,32 +209,33 @@ merged_data_dedup_final = merged_data_dedup %>%
 
 aver = unique(aver)
 dim(aver)
-# 113  19
+# 100  19
+
 # repeat x2 each row
 aver = aver[rep(seq_len(nrow(aver)), each = 2), ]
 dim(aver)
-# 226  19
+# 200  19
 
 merged_data_dedup_final = rbind(merged_data_dedup_final,
                                 aver)
 
 dim(merged_data_dedup_final)
-# 130126  19
+# 132102  19
 
 # How many genomes?
 length(unique(merged_data_dedup_final$platekey))
-# 65063
+# 66051
 
 # how many participant ids?
 length(unique(merged_data_dedup_final$participant_id))
-# 65063
+# 66051
 
 # how many alleles? not unique
 length(merged_data_dedup_final$repeat_size)
-# 130126
+# 132102
 
 write.table(merged_data_dedup_final, 
-            "./table_STR_repeat_size_each_row_allele_EHv3.1.2_HTT_CAG_simplified_dedup_270120.tsv",
+            "./table_STR_repeat_size_each_row_allele_EHv2.5.5_HTT_CAG_simplified_dedup_040220.tsv",
             quote = F,
             row.names = F,
             col.names = T,
