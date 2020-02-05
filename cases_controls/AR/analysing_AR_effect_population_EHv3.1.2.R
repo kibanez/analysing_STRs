9435a0386e816cf07ee871349bf90baca85643ca
kristina ibanez garikano <kibanez@users.noreply.github.com>
2020-02-05 16:52:06 +0000

    update numbers of AR having 37 or 38 repeat-sizes across different datasets

--- cases_controls/AR/analysing_AR_effect_population_EHv3.1.2.R
+++ cases_controls/AR/analysing_AR_effect_population_EHv3.1.2.R
@@ -15,29 +15,29 @@ library(ggplot2)
 setwd("~/Documents/STRs/ANALYSIS/cases_controls/EHv3.1.2/")
 
 # Load data
-dedup_data = read.csv("",
+dedup_data = read.csv("table_STR_repeat_size_each_row_allele_EHv3.1.2_AR_CAG_simplified_dedup_050220.tsv",
                       sep = '\t',
                       header = T,
                       stringsAsFactors = F)
 dim(dedup_data)
-# 115772 19
+# 112108 19
 
 # dataset 1
 dedup_data_all = dedup_data
 dim(dedup_data_all)
-# 115772  19
+# 112108  19
 
 # dataset 2
 dedup_data_not_neuro_not_mito = dedup_data %>% 
   filter(!grepl("[Nn][Ee][Uu][Rr][Oo]", disease_group_list), !grepl("[Mm][Ii][Tt][Oo]", panel_list)) 
 dim(dedup_data_not_neuro_not_mito)
-# 81790  19
+# 79345  19
 
 # dataset 3
 dedup_data_not_neuro_not_mito_not_cancer = dedup_data %>% 
   filter(!grepl("[Nn][Ee][Uu][Rr][Oo]", disease_group_list), !grepl("[Mm][Ii][Tt][Oo]", panel_list), (programme %in% "Rare Diseases")) 
 dim(dedup_data_not_neuro_not_mito_not_cancer)
-# 66003  19
+# 63670  19
 
 dedup_only_cancer = dedup_data %>%
   filter(programme %in% "Cancer")
@@ -51,19 +51,67 @@ dedup_data_pietro = dedup_data %>%
            (biological_relationship_to_proband %in% "N/A" & participant_phenotypic_sex %in% "Male" & programme %in% "Rare Diseases") |
            (is.na(biological_relationship_to_proband) & participant_phenotypic_sex %in% "Male" & programme %in% "Rare Diseases"))
 dim(dedup_data_pietro)
-# 56951  19
+# 55376  19
+
+# dataset1
+# Check how many participants present an allele >=37
+dedup_data_all %>% filter(repeat_size >= 37) %>% select(participant_id) %>% unique() %>% pull() %>% length()
+# 132
+
+dedup_data_all %>% filter(repeat_size >= 37) %>% select(participant_id) %>% pull() %>% length()
+# 138
+
+# Check how many participants present an allele >=38
+dedup_data_all %>% filter(repeat_size >= 38) %>% select(participant_id) %>% unique() %>% pull() %>% length()
+# 95
+
+dedup_data_all %>% filter(repeat_size >= 38) %>% select(participant_id) %>% pull() %>% length()
+# 98
 
 
+# dataset2
+# Check how many participants present an allele >=37
+dedup_data_not_neuro_not_mito %>% filter(repeat_size >= 37) %>% select(participant_id) %>% unique() %>% pull() %>% length()
+# 78
+
+dedup_data_not_neuro_not_mito %>% filter(repeat_size >= 37) %>% select(participant_id) %>% pull() %>% length()
+# 80
+
+# Check how many participants present an allele >=38
+dedup_data_not_neuro_not_mito %>% filter(repeat_size >= 38) %>% select(participant_id) %>% unique() %>% pull() %>% length()
+# 52
+
+dedup_data_not_neuro_not_mito %>% filter(repeat_size >= 38) %>% select(participant_id) %>% pull() %>% length()
+# 54
+
+
+# dataset3
+# Check how many participants present an allele >=37
+dedup_data_not_neuro_not_mito_not_cancer %>% filter(repeat_size >= 37) %>% select(participant_id) %>% unique() %>% pull() %>% length()
+# 69
+
+dedup_data_not_neuro_not_mito_not_cancer %>% filter(repeat_size >= 37) %>% select(participant_id) %>% pull() %>% length()
+# 71
+
+# Check how many participants present an allele >=38
+dedup_data_not_neuro_not_mito_not_cancer %>% filter(repeat_size >= 38) %>% select(participant_id) %>% unique() %>% pull() %>% length()
+# 45
+
+dedup_data_not_neuro_not_mito_not_cancer %>% filter(repeat_size >= 38) %>% select(participant_id) %>% pull() %>% length()
+# 47
+
+
+# dataset 4
 # Check how many participants present an allele >=37
 dedup_data_pietro %>% filter(repeat_size >= 37) %>% select(participant_id) %>% unique() %>% pull() %>% length()
-# 44
+# 58
 
 dedup_data_pietro %>% filter(repeat_size >= 37) %>% select(participant_id) %>% pull() %>% length()
-# 46
+# 62
 
 # Check how many participants present an allele >=38
 dedup_data_pietro %>% filter(repeat_size >= 38) %>% select(participant_id) %>% unique() %>% pull() %>% length()
-# 32
+# 44
 
 dedup_data_pietro %>% filter(repeat_size >= 38) %>% select(participant_id) %>% pull() %>% length()
-# 33
+# 45
