e7196ca4c32f1eb9ebe34b62cdbca277bcee01b7
kristina ibanez garikano <kibanez@users.noreply.github.com>
2020-02-05 13:06:54 +0000

    checking duplicates selecting the lastest platekey

--- cases_controls/AR/deduplicating_clinically_enriched_tables_EHv2.5.5_AR.R
+++ cases_controls/AR/deduplicating_clinically_enriched_tables_EHv2.5.5_AR.R
@@ -21,7 +21,6 @@ merged_data = read.csv("table_STR_repeat_size_each_row_allele_EHv2.5.5_AR_CAG_si
 dim(merged_data)
 # 135908  19
 
-
 # What it happens now, is that `specific disease` are in `specific_disease`, `disease_group`, and `disease_subgroup`
 list_spec_disease = merged_data %>% 
   group_by(participant_id) %>% 
@@ -45,21 +44,55 @@ merged_data = left_join(merged_data,
                         list_spec_disease,
                         by = "participant_id")
 dim(merged_data)
-# 177316  20
+# 135908  20
 
 merged_data = left_join(merged_data,
                         list_disease_group,
                         by = "participant_id")
 dim(merged_data)
-# 177316     21
+# 135908     21
 
 merged_data = left_join(merged_data,
                         list_disease_subgroup,
                         by = "participant_id")
 dim(merged_data)
-# 177316     22
+# 135908     22
 
 # let's remove the other columns now
 merged_data = merged_data[,-c(7:9)]
 dim(merged_data)
-# 177316  19
+# 135908  19
+
+# There are some participants for which there are several genomes/platekeys 
+# we will take/select the latest one
+merged_data %>% select(participant_id) %>% unique() %>% pull() %>% length()
+# 75095
+merged_data %>% select(platekey) %>% unique() %>% pull() %>% length()
+# 84917
+
+# First, select duplicated genomes
+duplicated_genomes = merged_data %>% 
+  group_by(participant_id) %>% 
+  filter(n()>2)
+
+duplicated_genomes %>% select(participant_id) %>% unique() %>% pull() %>% length()
+# 6483 genomes that have >1 genome
+l_duplicated_genomes = unique(duplicated_genomes$participant_id)
+length(l_duplicated_genomes)
+# 6483
+
+df_pid_platekey = merged_data %>% 
+  filter(participant_id %in% l_duplicated_genomes) %>% 
+  select(participant_id, platekey)
+
+# From here, select the latest genome
+df_pid_platekey = df_pid_platekey %>% 
+  group_by(participant_id) %>%
+  mutate(latest_platekey = max(platekey)) %>%
+  ungroup() %>% 
+  as.data.frame()
+
+# select latest genomes
+l_latest_dedup_platekeys = unique(df_pid_platekey$latest_platekey)
+length(l_latest_dedup_platekeys)
+# 6483 (== dedup participant ids)
