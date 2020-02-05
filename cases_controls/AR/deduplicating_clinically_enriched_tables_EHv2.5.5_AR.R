6db7cbcedf1a27003e9e4747ff4e64e9d891fa7a
kristina ibanez garikano <kibanez@users.noreply.github.com>
2020-02-05 13:03:04 +0000

    enrich and merge clinical data

--- cases_controls/AR/deduplicating_clinically_enriched_tables_EHv2.5.5_AR.R
+++ cases_controls/AR/deduplicating_clinically_enriched_tables_EHv2.5.5_AR.R
@@ -21,3 +21,45 @@ merged_data = read.csv("table_STR_repeat_size_each_row_allele_EHv2.5.5_AR_CAG_si
 dim(merged_data)
 # 135908  19
 
+
+# What it happens now, is that `specific disease` are in `specific_disease`, `disease_group`, and `disease_subgroup`
+list_spec_disease = merged_data %>% 
+  group_by(participant_id) %>% 
+  summarise(spec_disease_list = toString(specific_disease)) %>% ungroup() %>% as.data.frame()
+dim(list_spec_disease)
+# 75095  2
+
+list_disease_group = merged_data %>% 
+  group_by(participant_id) %>% 
+  summarise(disease_group_list = toString(disease_group)) %>% ungroup() %>% as.data.frame()
+dim(list_disease_group)
+# 75095  2
+
+list_disease_subgroup = merged_data %>% 
+  group_by(participant_id) %>% 
+  summarise(disease_subgroup_list = toString(disease_sub_group)) %>% ungroup() %>% as.data.frame()
+dim(list_disease_subgroup)
+# 75095  2
+
+merged_data = left_join(merged_data,
+                        list_spec_disease,
+                        by = "participant_id")
+dim(merged_data)
+# 177316  20
+
+merged_data = left_join(merged_data,
+                        list_disease_group,
+                        by = "participant_id")
+dim(merged_data)
+# 177316     21
+
+merged_data = left_join(merged_data,
+                        list_disease_subgroup,
+                        by = "participant_id")
+dim(merged_data)
+# 177316     22
+
+# let's remove the other columns now
+merged_data = merged_data[,-c(7:9)]
+dim(merged_data)
+# 177316  19
