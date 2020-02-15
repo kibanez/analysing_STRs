8535658504db1ce7aa8e8da22a54dcf0d1551c90
kristina ibanez garikano <kibanez@users.noreply.github.com>
2020-02-15 11:01:56 +0000

    set environment load popu data

--- population_research/plotting_PC_vs_disease_STR_size_across_populations.R
+++ population_research/plotting_PC_vs_disease_STR_size_across_populations.R
@@ -1,1 +1,23 @@
 # Objective: for each locus/gene, plot altogether PCs vs repeat-sizes between different ancestries
+date ()
+Sys.info ()[c("nodename", "user")]
+commandArgs ()
+rm (list = ls ())
+R.version.string ## "R version 3.6.2 (2019-12-12)"
+
+# libraries
+library(dplyr); packageDescription ("dplyr", fields = "Version") #"0.8.3"
+library(ggplot2); packageDescription ("ggplot2", fields = "Version") #"3.2.1"
+
+
+# Set environment
+setwd("/Users/kibanez/Documents/STRs/ANALYSIS/population_research/")
+
+# Load population data
+popu_table_enriched = read.csv("./population_info_enriched_59356_by_031019.tsv",
+                               header = T,
+                               sep = "\t",
+                               stringsAsFactors = F)
+dim(popu_table_enriched)
+# 59356  20
+
