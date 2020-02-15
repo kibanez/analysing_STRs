ae14a3947c33df6798060571201704a883404e8b
kristina ibanez garikano <kibanez@users.noreply.github.com>
2020-02-15 11:03:55 +0000

    load merged repeat-sizes per population and merged all them

--- population_research/plotting_PC_vs_disease_STR_size_across_populations.R
+++ population_research/plotting_PC_vs_disease_STR_size_across_populations.R
@@ -21,3 +21,53 @@ popu_table_enriched = read.csv("./population_info_enriched_59356_by_031019.tsv",
 dim(popu_table_enriched)
 # 59356  20
 
+# Load merged repeat-sizes for each population
+df_asi = read.csv('~/Documents/STRs/ANALYSIS/population_research/EH_3.1.2_research_October2019_55419_genomes/unrelated_probands_and_cancer/ASI/merged/merged_population_genomes_unrelated_probands_and_cancer_2678_avg_EHv3.1.2_ASI.tsv',
+                  sep = '\t',
+                  stringsAsFactors = F,
+                  header = T)
+dim(df_asi)
+# 1430  12
+
+df_eas = read.csv('~/Documents/STRs/ANALYSIS/population_research/EH_3.1.2_research_October2019_55419_genomes/unrelated_probands_and_cancer/EAS/merged/merged_population_genomes_unrelated_probands_and_cancer_227_avg_EHv3.1.2_EAS',
+                  sep = '\t',
+                  stringsAsFactors = F,
+                  header = T) 
+dim(df_eas)
+# 723  12
+
+df_afr = read.csv('~/Documents/STRs/ANALYSIS/population_research/EH_3.1.2_research_October2019_55419_genomes/unrelated_probands_and_cancer/AFR/merged/merged_population_genomes_unrelated_probands_and_cancer_1136_avg_EHv3.1.2_AFR.tsv',
+                  sep = '\t',
+                  stringsAsFactors = F,
+                  header = T) 
+dim(df_afr)
+# 1170  12
+
+df_amr = read.csv('~/Documents/STRs/ANALYSIS/population_research/EH_3.1.2_research_October2019_55419_genomes/unrelated_probands_and_cancer/AMR/merged/merged_population_genomes_unrelated_probands_and_cancer_384_avg_EHv3.1.2_AMR.tsv',
+                  sep = '\t',
+                  stringsAsFactors = F,
+                  header = T) 
+dim(df_amr)
+# 965  12
+
+df_eur = read.csv('~/Documents/STRs/ANALYSIS/population_research/EH_3.1.2_research_October2019_55419_genomes/unrelated_probands_and_cancer/EUR/merged/merged_population_genomes_unrelated_probands_and_cancer_26033_avg_EHv3.1.2_EUR.tsv',
+                  sep = '\t',
+                  stringsAsFactors = F,
+                  header = T) 
+dim(df_eur)
+# 2282  12
+
+
+df_asi = df_asi %>% mutate(population = "ASI")
+df_eas = df_eas %>% mutate(population = "EAS")
+df_afr = df_afr %>% mutate(population = "AFR")
+df_amr = df_amr %>% mutate(population = "AMR")
+df_eur = df_eur %>% mutate(population = "EUR")
+
+df_all = rbind(df_afr,
+               df_amr,
+               df_eur,
+               df_eas,
+               df_asi)
+dim(df_all)
+# 6570  13
