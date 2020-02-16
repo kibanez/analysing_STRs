4b68db37db26d3a5b64b92b1468eab24b17a1a4e
kristina ibanez garikano <kibanez@users.noreply.github.com>
2020-02-16 20:42:40 +0000

    new script for plotting nOTCH2 figure for paper

---
+++ random_scripts/plotting_NOTCH2_paper.R
@@ -0,0 +1,6 @@
+# Objective: plot percentage (not frequency) of each repeat-size in 
+# - individuals recruited under `neurology`
+# - rest of individuals
+# - NIID
+# - inclusions
+# last both sent by Zhongbo
