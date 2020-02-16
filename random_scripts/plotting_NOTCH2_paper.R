c904b7ac1e7d69ef93a6660ad495c2c866bdeee5
kristina ibanez garikano <kibanez@users.noreply.github.com>
2020-02-16 20:46:07 +0000

    setting working dir loading data

--- random_scripts/plotting_NOTCH2_paper.R
+++ random_scripts/plotting_NOTCH2_paper.R
@@ -4,3 +4,22 @@
 # - NIID
 # - inclusions
 # last both sent by Zhongbo
+date ()
+Sys.info ()[c("nodename", "user")]
+commandArgs ()
+rm (list = ls ())
+R.version.string ## "R version 3.6.2 (2019-12-12)"
+
+# libraries
+library(dplyr)
+library(ggplot2)
+
+# Set working directory
+working_dir="~/Documents/STRs/PAPERS/NIID/"
+setwd(working_dir)
+
+# Load data
+df_zhongbo = read.csv("data/repeat_sizes_NIID_and_inclusions_for_Kristina.csv",
+                      sep = ",",
+                      header = T,
+                      stringsAsFactors = F)
