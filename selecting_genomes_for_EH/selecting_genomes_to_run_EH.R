7636099e2e5c22510e24d9c18ebe8e31242b2045
kristina ibanez garikano <kibanez@users.noreply.github.com>
2020-02-25 17:03:44 +0000

    loading file with the paths of all genomes sequenced by GEL so far

--- selecting_genomes_for_EH/selecting_genomes_to_run_EH.R
+++ selecting_genomes_for_EH/selecting_genomes_to_run_EH.R
@@ -1,4 +1,5 @@
 # Objective: select genomes (better deduplicated) in order to run afterwards EH through all them
+# The idea is to merge or join all data available to us: RE batch + population aggregated gVCF (in case we have more genomes to fish) + earlier EH batches
 date()
 Sys.info()[c("nodename", "user")]
 commandArgs()
@@ -10,6 +11,24 @@ library(dplyr)
 
 # Working directory
 setwd("~/Documents/STRs/data/research/input")
+
+# Loading all genomes sequenced by GEL so far (25th Feb 2020)
+all_genomes = read.table("~/Documents/STRs/data/research/input/list_all_genomes_25022020.tsv", stringsAsFactors = F)
+all_genomes = all_genomes$V1
+length(all_genomes)
+# 121506
+
+# we do this once, and after this, we will load the whole table with genomes and path to the genomes
+df_all_genomes = c()
+for(i in 1:length(all_genomes)){
+  path_to_genome = all_genomes[i]
+  bam_name = strsplit(path_to_genome, "/")[[1]][length(strsplit(path_to_genome, "/")[[1]])-2]
+  df_all_genomes = rbind(df_all_genomes,
+                         cbind(bam_name, path_to_genome))
+  
+}
+
+df_all_genomes = as.data.frame(df_all_genomes)
 
 # Loading last RE clinical data batch (already enriched)
 clin_data = read.csv("~/Documents/STRs/clinical_data/clinical_data/rd_genomes_all_data_041219.tsv",
@@ -45,3 +64,19 @@ l_germline_pid = all_germlines %>%
   pull()
 length(l_germline_pid)
 # 87168
+
+# Load popu table
+popu_table = read.csv("~/Documents/STRs/clinical_data/clinical_data/aggregate_gvcf_sample_stats_2019-10-03_22-07-31.tsv",
+                      sep = "\t",
+                      stringsAsFactors = F,
+                      header = T)
+dim(popu_table)
+# 59356  51
+
+popu_platekeys = unique(popu_table$platekey)
+length(popu_platekeys)
+# 59356
+length(intersect(popu_platekeys, all_germlines$platekey))
+# 58441
+
+setdiff(popu_platekeys, all_germlines$platekey)
