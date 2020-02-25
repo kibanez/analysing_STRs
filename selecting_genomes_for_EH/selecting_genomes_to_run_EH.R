5a72b81427837d9c96da33c1b20b966f2cc1c5a8
kristina ibanez garikano <kibanez@users.noreply.github.com>
2020-02-25 14:31:56 +0000

    setting up the script

---
+++ selecting_genomes_for_EH/selecting_genomes_to_run_EH.R
@@ -0,0 +1,14 @@
+# Objective: select genomes (better deduplicated) in order to run afterwards EH through all them
+date()
+Sys.info()[c("nodename", "user")]
+commandArgs()
+rm(list = ls())
+R.version.string # "R version 3.6.2 (2019-12-12)"
+
+# Libraries
+library(dplyr)
+
+# Working directory
+setwd("~/Documents/STRs/data/research/input")
+
+
