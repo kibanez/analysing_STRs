cf9a73291baf9875b53fdcefa7e6dc95d08763c1
kristina ibanez garikano <kibanez@users.noreply.github.com>
2020-02-25 14:37:37 +0000

    loading ALL genomes' path from V8

--- selecting_genomes_for_EH/selecting_genomes_to_run_EH.R
+++ selecting_genomes_for_EH/selecting_genomes_to_run_EH.R
@@ -18,3 +18,21 @@ clin_data = read.csv("~/Documents/STRs/clinical_data/clinical_data/rd_genomes_al
                      header = T)
 dim(clin_data)
 # 1124633  28
+
+# There are duplicates here, more genomes than participantIDs
+length(unique(clin_data$platekey))
+# 104843
+
+length(unique(clin_data$participant_id))
+# 87395
+
+all_germlines = read.csv("~/Documents/STRs/clinical_data/clinical_data/raw/RE_clinical_data_V8/genome_file_paths_and_types_2019-12-04_15-13-29.tsv",
+                         sep = "\t",
+                         stringsAsFactors = F,
+                         header = T)
+dim(all_germlines)
+# 500443  11
+
+table(all_germlines$type)
+#cancer germline        cancer somatic experimental germline  experimental somatic rare disease germline 
+#73813                 52249                   268                   159                373954 
