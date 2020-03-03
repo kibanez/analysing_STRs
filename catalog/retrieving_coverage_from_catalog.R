99bfacb8a22b043b7b3bc432fd57ee09a83cd687
kristina ibanez garikano <kibanez@users.noreply.github.com>
2020-03-03 17:27:49 +0000

    fixing total number of genomes loop in order to retrieve info from catalog

--- catalog/retrieving_coverage_from_catalog.R
+++ catalog/retrieving_coverage_from_catalog.R
@@ -23,15 +23,25 @@ con <- opencgaLogin(opencga = con, userid = "interpretationpipeline", passwd = "
                     autoRenew = TRUE, showToken = TRUE)
 
 # load GEL validation platekeys
-l_val_data = read.csv("./list_all_val_genomes_GEL.txt", stringsAsFactors = F, header = F)
+l_val_data = read.csv("./list_all_unique_254_genomes_GEL.txt", stringsAsFactors = F, header = F)
 l_val_data = unique(l_val_data$V1)
 length(l_val_data)
-# 255
+# 254
 
 #write.table(l_val_data, "./list_all_unique_255_genomes_GEL.txt", quote = F, row.names = F, col.names = F)
 
-# study '32 == RD GRCh38 dataset
-test <- fileClient(OpencgaR = con, action = "search", batch_size=5000,
-                   params = list(study = "1000000032", name="LP3001783-DNA_A01.bam"))
-df_coverage = test$stats$WHOLE_GENOME_COVERAGE$coverageSummary[[1]]
-df_coverage %>% filter(scope %in% "autosomes") %>% select(avg) %>% pull()
+df_coverage = c()
+for (i in 1:length(l_val_data)){
+  platekey = paste(l_val_data[i], ".bam", sep = "")
+  
+  # study '32 == RD GRCh38 dataset
+  test <- fileClient(OpencgaR = con, action = "search", batch_size=5000,
+                     params = list(study = "1000000032", name=platekey))
+  if (!is.null(test)){
+    df_coverage = test$stats$WHOLE_GENOME_COVERAGE$coverageSummary[[1]]
+    autosome_cov = df_coverage %>% filter(scope %in% "autosomes") %>% select(avg) %>% pull()
+    df_coverage = rbind(df_coverage,
+                        cbind(platekey, autosome_cov))
+    
+  }
+}
