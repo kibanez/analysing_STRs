33d0a71ea50cf3f6e86ea736e6619e82bb131aae
kristina ibanez garikano <kibanez@users.noreply.github.com>
2020-03-04 10:29:33 +0000

    new script to see the delivery version, of the platekeys included in the diagnosis table

---
+++ paper_validation/retrieving_platekey_from_diagnosis.R
@@ -0,0 +1,10 @@
+# Objective: retrieve the corresponding platekeys for the pids in the diagnosis table
+date ()
+Sys.info ()[c("nodename", "user")]
+commandArgs ()
+rm (list = ls ())
+R.version.string ## "R version 3.6.1 (2019-07-05)"
+
+# libraries
+library(dplyr)
+
