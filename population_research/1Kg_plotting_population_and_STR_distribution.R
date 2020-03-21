4a506724fb365c7264c4b9e4fffc00e8ab106707
kibanez <kristina.ibanez85@gmail.com>
2020-03-21 04:35:55 -0500

    loading normal and patho thresholds

--- population_research/1Kg_plotting_population_and_STR_distribution.R
+++ population_research/1Kg_plotting_population_and_STR_distribution.R
@@ -24,6 +24,15 @@ popu_info = read.csv("./integrated_call_samples_v2.20130502.ALL.ped",
 dim(popu_info)
 # 3691  17
 
+# Load thresholds
+# STR annotation, threshold including the largest normal and the smallest pathogenic sizes reported
+gene_annotation_normal = '/Users/kibanez/git/analysing_STRs/threshold_largest_normal_reported_research.txt'
+gene_data_normal = read.table(gene_annotation_normal, stringsAsFactors=F, header = T)
+
+gene_annotation_pathogenic = '/Users/kibanez/git/analysing_STRs/threshold_smallest_pathogenic_reported_research.txt'
+gene_data_pathogenic = read.table(gene_annotation_pathogenic, stringsAsFactors=F, header = T)
+
+
 # Functions
 source("/Users/kibanez/git/analysing_STRs/functions/plot_violin_ancestry.R")
 source("/Users/kibanez/git/analysing_STRs/functions/plot_gene.R")
@@ -54,169 +63,13 @@ for (i in 1:length(l_popus)){
   
 }
 
-
-df_acb = read.csv('~/Documents/STRs/ANALYSIS/population_research/1Kg/data/ACB/merged_ACB_1Kg_samples.tsv',
-              sep = '\t',
-              stringsAsFactors = F,
-              header = T)
-dim(df_acb)
-# 
-
-df_asw = read.csv('~/Documents/STRs/ANALYSIS/population_research/1Kg/data/',
-                  sep = '\t',
-                  stringsAsFactors = F,
-                  header = T)
-dim(df_asw)
-#
-
-df_beb = read.csv('~/Documents/STRs/ANALYSIS/population_research/1Kg/data/',
-                  sep = '\t',
-                  stringsAsFactors = F,
-                  header = T)
-dim(df_beb)
-
-df_ceu = read.csv('~/Documents/STRs/ANALYSIS/population_research/1Kg/data/',
-               sep = '\t',
-               stringsAsFactors = F,
-               header = T)
-dim(df_ceu)
-#
-
-df_clm = read.csv('~/Documents/STRs/ANALYSIS/population_research/1Kg/data/',
-               sep = '\t',
-               stringsAsFactors = F,
-               header = T)
-dim(df_clm)
-#
-
-
-df_esn = read.csv('~/Documents/STRs/ANALYSIS/population_research/1Kg/data/',
-               sep = '\t',
-               stringsAsFactors = F,
-               header = T)
-dim(df_esn)
-#
-
-df_fin = read.csv('~/Documents/STRs/ANALYSIS/population_research/1Kg/data/',
-               sep = '\t',
-               stringsAsFactors = F,
-               header = T)
-dim(df_fin)
-#
-
-df_gbr = read.csv('~/Documents/STRs/ANALYSIS/population_research/1Kg/data/',
-               sep = '\t',
-               stringsAsFactors = F,
-               header = T)
-dim(df_gbr)
-#
-
-df_gih = read.csv('~/Documents/STRs/ANALYSIS/population_research/1Kg/data/',
-               sep = '\t',
-               stringsAsFactors = F,
-               header = T)
-dim(df_gih)
-#
-
-df_gwd = read.csv('~/Documents/STRs/ANALYSIS/population_research/1Kg/data/',
-               sep = '\t',
-               stringsAsFactors = F,
-               header = T)
-dim(df_gwd)
-#
-
-df_ibs = read.csv('~/Documents/STRs/ANALYSIS/population_research/1Kg/data/',
-               sep = '\t',
-               stringsAsFactors = F,
-               header = T)
-dim(df_ibs)
-#
-
-df_itu = read.csv('~/Documents/STRs/ANALYSIS/population_research/1Kg/data/',
-               sep = '\t',
-               stringsAsFactors = F,
-               header = T)
-dim(df_itu)
-#
-
-df_lwk = read.csv('~/Documents/STRs/ANALYSIS/population_research/1Kg/data/',
-               sep = '\t',
-               stringsAsFactors = F,
-               header = T)
-dim(df_lwk)
-#
-
-df_msl = read.csv('~/Documents/STRs/ANALYSIS/population_research/1Kg/data/',
-               sep = '\t',
-               stringsAsFactors = F,
-               header = T)
-dim(df_msl)
-
-df_mxl = read.csv('~/Documents/STRs/ANALYSIS/population_research/1Kg/data/',
-               sep = '\t',
-               stringsAsFactors = F,
-               header = T)
-dim(df_mxl)
-
-df_pel = read.csv('~/Documents/STRs/ANALYSIS/population_research/1Kg/data/',
-               sep = '\t',
-               stringsAsFactors = F,
-               header = T)
-dim(df_pel)
-
-df_pjl = read.csv('~/Documents/STRs/ANALYSIS/population_research/1Kg/data/',
-               sep = '\t',
-               stringsAsFactors = F,
-               header = T)
-dim(df_pjl)
-#
-
-df_pur = read.csv('~/Documents/STRs/ANALYSIS/population_research/1Kg/data/',
-               sep = '\t',
-               stringsAsFactors = F,
-               header = T)
-dim(df_pur)
-
-df_stu = read.csv('~/Documents/STRs/ANALYSIS/population_research/1Kg/data/',
-               sep = '\t',
-               stringsAsFactors = F,
-               header = T)
-dim(df_stu)
-#
-
-df_tsi = read.csv('~/Documents/STRs/ANALYSIS/population_research/1Kg/data/',
-               sep = '\t',
-               stringsAsFactors = F,
-               header = T)
-dim(df_tsi)
-#
-
-df_yri = read.csv('~/Documents/STRs/ANALYSIS/population_research/1Kg/data/',
-               sep = '\t',
-               stringsAsFactors = F,
-               header = T)
-dim(df_yri)
-#
-
-
-df_asi = df_asi %>% mutate(population = "ASI")
-df_eas = df_eas %>% mutate(population = "EAS")
-df_afr = df_afr %>% mutate(population = "AFR")
-df_amr = df_amr %>% mutate(population = "AMR")
-df_eur = df_eur %>% mutate(population = "EUR")
-
-df_all = rbind(df_afr,
-               df_amr,
-               df_eur,
-               df_eas,
-               df_asi)
-dim(df_all)
-# 7290  13
+dim(df_merged)
+# 23148  13
 
 # Population enriched genomes are only GRCh38, we will ignore then GRCh37
 output_folder = "./figures/"
 
-l_loci = sort(unique(df_all$gene))
+l_loci = sort(unique(df_merged$gene))
 for (i in 1:length(l_loci)){
   # Each locus - Individually
   plot_gene(df_afr, l_loci[i], gene_data_normal, gene_data_pathogenic, output_folder, "GRCh38", "AFR")
@@ -226,8 +79,8 @@ for (i in 1:length(l_loci)){
   plot_gene(df_asi, l_loci[i], gene_data_normal, gene_data_pathogenic, output_folder, "GRCh38", "ASI")
   
   # Jointly - distribution
-  plot_gene_joint_ancestries(df_all, l_loci[i], gene_data_normal, gene_data_pathogenic, output_folder)
+  plot_gene_joint_ancestries(df_merged, l_loci[i], gene_data_normal, gene_data_pathogenic, output_folder)
   
   # Jointly - Violin plots
-  plot_violin_ancestry(df_all, l_loci[i], gene_data_normal, gene_data_pathogenic, output_folder)
+  plot_violin_ancestry(df_merged, l_loci[i], gene_data_normal, gene_data_pathogenic, output_folder)
 }
