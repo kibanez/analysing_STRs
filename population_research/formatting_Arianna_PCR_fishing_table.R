9f714938c65af42293c0af0dc17fd88f94d51bb5
kristina ibanez garikano <kibanez@users.noreply.github.com>
2020-03-30 13:10:20 +0200

    new script in order to format table created by Arianna with PCR estimations from NHNN

---
+++ population_research/formatting_Arianna_PCR_fishing_table.R
@@ -0,0 +1,16 @@
+# Objective: Arianna worked out all PCR info retrieved from NHNN database
+# I need to format the table
+date ()
+Sys.info ()[c("nodename", "user")]
+commandArgs ()
+rm (list = ls ())
+R.version.string ## "R version 3.6.1 (2019-07-05)"
+
+library(ggplot2); packageDescription ("ggplot2", fields = "Version") #"2.2.1"
+library(grid); packageDescription ("grid", fields = "Version") #"3.3.2"
+library(gridExtra); packageDescription ("gridExtra", fields = "Version") #"2.3"
+library(reshape2); packageDescription ("reshape2", fields = "Version") #"1.4.3"
+require(dplyr); packageDescription ("dplyr", fields = "Version") #"0.7.4"
+
+# Set working directory
+setwd("~/Documents/STRs/VALIDATION/PCR_EH_estimations/")
