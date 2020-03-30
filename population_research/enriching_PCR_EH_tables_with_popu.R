8b00474a467a080c66f2e0674c698f3ff9a2ef58
kristina ibanez garikano <kibanez@users.noreply.github.com>
2020-03-30 12:39:11 +0200

    setting up working dir loading pilot programme popu

--- population_research/enriching_PCR_EH_tables_with_popu.R
+++ population_research/enriching_PCR_EH_tables_with_popu.R
@@ -10,3 +10,15 @@ library(grid); packageDescription ("grid", fields = "Version") #"3.3.2"
 library(gridExtra); packageDescription ("gridExtra", fields = "Version") #"2.3"
 library(reshape2); packageDescription ("reshape2", fields = "Version") #"1.4.3"
 require(dplyr); packageDescription ("dplyr", fields = "Version") #"0.7.4"
+
+# Set working directory
+setwd("~/Documents/STRs/VALIDATION/PCR_EH_estimations/")
+
+# Load Pilot programme with population info
+pilot_popu = read.csv("~/Documents/STRs/ANALYSIS/population_research/PILOT_ANCESTRY/FINE_GRAINED_RF_classifications_incl_superPOP_prediction_final20191216.csv",
+                      sep = ",",
+                      stringsAsFactors = F,
+                      header = T)
+dim(pilot_popu)
+# 4821  44
+
