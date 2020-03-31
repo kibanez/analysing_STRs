f971c16ffacbd3c2980e15ed7a69326e3af459cb
kristina ibanez garikano <kibanez@users.noreply.github.com>
2020-03-31 13:27:42 +0200

    load merged table data

--- population_research/plotting_bubble_plot_per_superpopu.R
+++ population_research/plotting_bubble_plot_per_superpopu.R
@@ -11,3 +11,16 @@ library(grid); packageDescription ("grid", fields = "Version") #"3.3.2"
 library(gridExtra); packageDescription ("gridExtra", fields = "Version") #"2.3"
 library(reshape2); packageDescription ("reshape2", fields = "Version") #"1.4.3"
 require(dplyr); packageDescription ("dplyr", fields = "Version") #"0.7.4"
+
+
+# Set working directory
+setwd("~/Documents/STRs/VALIDATION/PCR_EH_estimations/")
+
+# Load data
+merged_table = read.csv("./googleDrive_all_merged_dedup_table.tsv",
+                        sep = "\t",
+                        stringsAsFactors = F, 
+                        header = T)
+dim(merged_table)
+# 792  11
+
