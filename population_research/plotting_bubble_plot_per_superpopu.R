e7c6e94875dea5729c32fc989b8236d969e81f42
kristina ibanez garikano <kibanez@users.noreply.github.com>
2020-03-31 13:26:16 +0200

    new script for plotting bubble plots coluring by super popus from PCR and EHv2/EHv3 estimations

---
+++ population_research/plotting_bubble_plot_per_superpopu.R
@@ -0,0 +1,13 @@
+# Objective: create bubble plots representing repeat sizes EH vs PCR, colouring by super-population
+# PER LOCUS
+date ()
+Sys.info ()[c("nodename", "user")]
+commandArgs ()
+rm (list = ls ())
+R.version.string ## "R version 3.6.1 (2019-07-05)"
+
+library(ggplot2); packageDescription ("ggplot2", fields = "Version") #"2.2.1"
+library(grid); packageDescription ("grid", fields = "Version") #"3.3.2"
+library(gridExtra); packageDescription ("gridExtra", fields = "Version") #"2.3"
+library(reshape2); packageDescription ("reshape2", fields = "Version") #"1.4.3"
+require(dplyr); packageDescription ("dplyr", fields = "Version") #"0.7.4"
