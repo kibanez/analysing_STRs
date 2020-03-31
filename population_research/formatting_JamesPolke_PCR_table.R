0d5ca738690ffb0fd01875c0e39cc96520b17619
kristina ibanez garikano <kibanez@users.noreply.github.com>
2020-03-31 11:00:38 +0200

    new script formatting James Polke's group work on estimating PCR alleles

---
+++ population_research/formatting_JamesPolke_PCR_table.R
@@ -0,0 +1,16 @@
+# Objective: James' team worked through some extra genomes, estimating PCR repeat-size alleles
+# I need to format the table and enrich with super and sub-population and gender information
+date ()
+Sys.info ()[c("nodename", "user")]
+commandArgs ()
+rm (list = ls ())
+R.version.string ## "R version 3.6.1 (2019-07-05)"
+
+library(ggplot2); packageDescription ("ggplot2", fields = "Version") #"2.2.1"
+library(grid); packageDescription ("grid", fields = "Version") #"3.3.2"
+library(gridExtra); packageDescription ("gridExtra", fields = "Version") #"2.3"
+library(reshape2); packageDescription ("reshape2", fields = "Version") #"1.4.3"
+require(dplyr); packageDescription ("dplyr", fields = "Version") #"0.7.4"
+
+# Set working directory
+setwd("~/Documents/STRs/VALIDATION/PCR_EH_estimations/")
