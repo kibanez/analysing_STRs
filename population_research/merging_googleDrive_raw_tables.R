3529cdf66a7fdaa21572f1b85c658b2de21bf666
kristina ibanez garikano <kibanez@users.noreply.github.com>
2020-03-31 11:47:14 +0200

    new script merging and deduplicating all 3 PCR and EH tables

--- population_research/merging_googleDrive_raw_tables.R
+++ population_research/merging_googleDrive_raw_tables.R
@@ -1,2 +1,17 @@
 # Objective: from the recoded and formatted tables from google drive (https://docs.google.com/spreadsheets/d/1cuh2rsDkQP3YEHjX6ogLWlgxO3sd0Jfs4zCVdizBQEc/edit#gid=1383787997)
 # The aim here is to dedup all info we do have from 3 independent tables and merge them all
+date ()
+Sys.info ()[c("nodename", "user")]
+commandArgs ()
+rm (list = ls ())
+R.version.string ## "R version 3.6.1 (2019-07-05)"
+
+library(ggplot2); packageDescription ("ggplot2", fields = "Version") #"2.2.1"
+library(grid); packageDescription ("grid", fields = "Version") #"3.3.2"
+library(gridExtra); packageDescription ("gridExtra", fields = "Version") #"2.3"
+library(reshape2); packageDescription ("reshape2", fields = "Version") #"1.4.3"
+require(dplyr); packageDescription ("dplyr", fields = "Version") #"0.7.4"
+
+# Set working directory
+setwd("~/Documents/STRs/VALIDATION/PCR_EH_estimations/")
+
