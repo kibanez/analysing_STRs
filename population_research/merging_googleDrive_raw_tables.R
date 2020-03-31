c5d11e50aa191d1930fed801eb69898eae0ca1d6
kristina ibanez garikano <kibanez@users.noreply.github.com>
2020-03-31 11:47:00 +0200

    new script merging and deduplicating all 3 independent PCR_EH tables

---
+++ population_research/merging_googleDrive_raw_tables.R
@@ -0,0 +1,2 @@
+# Objective: from the recoded and formatted tables from google drive (https://docs.google.com/spreadsheets/d/1cuh2rsDkQP3YEHjX6ogLWlgxO3sd0Jfs4zCVdizBQEc/edit#gid=1383787997)
+# The aim here is to dedup all info we do have from 3 independent tables and merge them all
