98e3b5e278685c6e2615738c9e72820b8e5ea7d7
kristina ibanez garikano <kibanez@users.noreply.github.com>
2020-04-02 14:23:38 +0200

    recode merged table take VCF files in the pathogenic tail

--- population_research/analyse_population_pathogenic_tail_loci.R
+++ population_research/analyse_population_pathogenic_tail_loci.R
@@ -33,3 +33,50 @@ popu_table = read.csv("~/Documents/STRs/ANALYSIS/population_research/MAIN_ANCEST
 dim(popu_table)
 # 59464  36
 
+# Merged GRCh37 and GRCh38 tables, recoding chr names
+merged_table$chr = recode(merged_table$chr,
+                "1" = "chr1",
+                "2" = "chr2",
+                "3" = "chr3",
+                "4" = "chr4",
+                "5" = "chr5",
+                "6" = "chr6",
+                "7" = "chr7",
+                "8" = "chr8",
+                "9" = "chr9",
+                "10" = "chr10",
+                "11" = "chr11",
+                "12" = "chr12",
+                "13" = "chr13",
+                "14" = "chr14",
+                "15" = "chr15",
+                "16" = "chr16",
+                "17" = "chr17",
+                "18" = "chr18",
+                "19" = "chr19",
+                "20" = "chr20",
+                "21" = "chr21",
+                "22" = "chr22",
+                "X" = "chrX")
+merged_table = merged_table %>%
+  group_by(chr, gene, allele) %>%
+  mutate(total_num_samples = sum(num_samples)) %>%
+  ungroup() %>%
+  as.data.frame() 
+
+
+# HTT
+merged_table_htt = merged_table %>%
+  filter(gene %in% "HTT", allele >= 40)
+list_vcf_patho_htt = c()
+for (i in 1:length(merged_table_htt$list_samples)){
+  list_vcf_patho_htt = c(list_vcf_patho_htt,
+                         strsplit(merged_table_htt$list_samples[i], ';')[[1]][1])
+  
+}
+
+list_vcf_patho_htt = gsub('.vcf', '', list_vcf_patho_htt)
+list_vcf_patho_htt = gsub('^EH_', '', list_vcf_patho_htt)
+
+# Enrich platekeys now with ancestry info
+
