ec167f0b5d4a41b7e4ea22e5358d504e07dbdf58
kristina ibanez garikano <kibanez@users.noreply.github.com>
2020-04-03 12:21:53 +0200

    new script selecting cases with potential pathogenic expansions on FMR1 from summer batch EHv255

---
+++ paper_validation/selecting_potential_pathogenic_cases_FMR1_EHv2.5.5.R
@@ -0,0 +1,22 @@
+# Objective: from EHv2.5.5 summer batch select genomes with the following selection criteria (and compare with Arianna's numbers):
+# both from GRCH37 and GRCH38:
+# proband only
+# specific disease == intellectual disability
+# Arianna's numbers are the following:
+# 5783 probands recruited under ID
+# repeat size > 55
+# 131 probands
+date ()
+Sys.info ()[c("nodename", "user")]
+commandArgs ()
+rm (list = ls ())
+R.version.string ## "R version 3.6.1 (2019-07-05)"
+
+library(ggplot2); packageDescription ("ggplot2", fields = "Version") #"3.2.1"
+library(grid); packageDescription ("grid", fields = "Version") #"3.6.1"
+library(gridExtra); packageDescription ("gridExtra", fields = "Version") #"2.3"
+library(reshape2); packageDescription ("reshape2", fields = "Version") #"1.4.3"
+require(dplyr); packageDescription ("dplyr", fields = "Version") #"0.8.3"
+
+# Set working dir
+setwd("~/Documents/STRs/data/research/EH_2.5.5_research_August2019/EH_output_v2.5.5_research_August_2019/merged_genotypeUpdated/")
