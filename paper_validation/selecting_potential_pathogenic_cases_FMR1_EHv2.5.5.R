be8dd29bce2ddc87cb256a256e0efaf2f4470fdb
kristina ibanez garikano <kibanez@users.noreply.github.com>
2020-04-03 12:24:05 +0200

    load merged EHv255 table

--- paper_validation/selecting_potential_pathogenic_cases_FMR1_EHv2.5.5.R
+++ paper_validation/selecting_potential_pathogenic_cases_FMR1_EHv2.5.5.R
@@ -13,10 +13,21 @@ rm (list = ls ())
 R.version.string ## "R version 3.6.1 (2019-07-05)"
 
 library(ggplot2); packageDescription ("ggplot2", fields = "Version") #"3.2.1"
-library(grid); packageDescription ("grid", fields = "Version") #"3.6.1"
+library(grid); packageDescription ("grid", fields = "Version") #"3.6.2"
 library(gridExtra); packageDescription ("gridExtra", fields = "Version") #"2.3"
 library(reshape2); packageDescription ("reshape2", fields = "Version") #"1.4.3"
 require(dplyr); packageDescription ("dplyr", fields = "Version") #"0.8.3"
 
 # Set working dir
 setwd("~/Documents/STRs/data/research/EH_2.5.5_research_August2019/EH_output_v2.5.5_research_August_2019/merged_genotypeUpdated/")
+
+# Load data
+merged_table = read.csv("merged_loci_86457_research_genomes_new_loci_EHv2.5.5_summer2019_removingListVcfFiles.tsv",
+                        sep = "\t",
+                        stringsAsFactors = F,
+                        header = T)
+dim(merged_table)
+# 3983  11
+
+# Recode b37 and b38 chr names
+
