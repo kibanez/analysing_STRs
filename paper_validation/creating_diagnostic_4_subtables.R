b864f81c855c6d1a228dd6af0e5c7bcd0663a983
kristina ibanez garikano <kibanez@users.noreply.github.com>
2020-04-10 12:56:02 +0200

    load EHv255 estimations for the main programme launnched on summer 2019

--- paper_validation/creating_diagnostic_4_subtables.R
+++ paper_validation/creating_diagnostic_4_subtables.R
@@ -46,3 +46,19 @@ table_diseases_pilot = table_diseases_pilot %>%
 table_diseases = mutate(table_diseases, adult.paediatric = ifelse(age < 18, "Paediatric", "Adult"))
 table_diseases_pilot = mutate(table_diseases_pilot, adult.paediatric = ifelse(age < 18, "Paediatric", "Adult"))
 
+# We need to load here the repeat-size estimations for all them - EHv255
+repeats_table_main = read.csv("~/Documents/STRs/data/research/EH_2.5.5_research_August2019/EH_output_v2.5.5_research_August_2019/merged_genotypeUpdated/merged_loci_86457_research_genomes_new_loci_EHv2.5.5_summer2019_removingListVcfFiles.tsv",
+                              stringsAsFactors = F,
+                              header = T,
+                              sep = "\t")
+dim(repeats_table_main)
+# 3983  11
+
+
+
+# Let's define now the 4 subtables for the purpose of the paper
+# TABLE A. ONLY INCLUDING ADULTS (I.E. >= 18 IN 2020, EXCEPT FXN WHERE WE INCLUDE CHILDREN), USING FULL-MUTATION CUTOFF THRESHOLD  
+# (OR YOU CAN PRODUCE AA TABLE USING THE PREMUTATION CUTOFF, BUT I SUSPOECT IT WILL BE VERY NOISY AND WOULD NOT REFELCT THE THRESHOLDS THAT PANELAPP IS CURRENTLY USING)
+
+
+
