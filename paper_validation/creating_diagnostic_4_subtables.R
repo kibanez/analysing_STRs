8bf5669b34a78b1a698967cd0970805519196c8b
kristina ibanez garikano <kibanez@users.noreply.github.com>
2020-04-10 11:48:14 +0200

    load pilot disease table

--- paper_validation/creating_diagnostic_4_subtables.R
+++ paper_validation/creating_diagnostic_4_subtables.R
@@ -11,3 +11,21 @@ library(ggplot2); packageDescription ("ggplot2", fields = "Version") #"3.2.1"
 
 # set the working directory
 setwd("~/Documents/STRs/PAPERS/VALIDATION_PAPER/")
+
+# Load table with the diagnostics 
+# Main table
+table_diseases = read.csv("table_diseases_enriched_popu_includingSkeletalMuscleChan.tsv",
+                          stringsAsFactors = F, 
+                          header = T,
+                          sep = "\t")
+dim(table_diseases)
+# 11842  20
+
+# Pilot table
+table_diseases_pilot = read.csv("table_diseases_enriched_PILOT_13diseases_enriched_popu.tsv",
+                                stringsAsFactors = F,
+                                header = T,
+                                sep = "\t")
+dim(table_diseases_pilot)
+# 659  13
+
