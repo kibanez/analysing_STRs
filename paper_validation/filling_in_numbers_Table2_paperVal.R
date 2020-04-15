30ff7f6a965ba1c785a5294482400179d6a9191d
kristina ibanez garikano <kibanez.ingemm@gmail.com>
2020-04-15 10:13:53 +0200

    I'll automate this by using list of diseases in main programme

--- paper_validation/filling_in_numbers_Table2_paperVal.R
+++ paper_validation/filling_in_numbers_Table2_paperVal.R
@@ -23,3 +23,28 @@ table_diseases_pilot = read.csv("table_diseases_enriched_PILOT_13diseases_enrich
 dim(table_diseases_pilot)
 # 660 13
 
+# Counting number of participants, age (mean and distribution)
+# Create the list of diseases
+l_diseases_main = c("Intellectual disability",
+                    "Amyotrophic lateral sclerosis or motor neuron disease", 
+                    "Charcot-Marie-Tooth disease", 
+                    "Congenital muscular dystrophy",
+                    "Congenital myopathy", 
+                    "Early onset dementia", 
+                    "Early onset dystonia", 
+                    "Distal myopathies", 
+                    "Complex Parkinsonism", 
+                    "Hereditary ataxia", 
+                    "Hereditary spastic paraplegia", 
+                    "Skeletal Muscle Channelopathies",
+                    "'Early onset and familial Parkinson''s Disease'",
+                    "Mitochondrial disorders",
+                    "Kabuki syndrome")
+
+table_diseases %>% filter(normalised_specific_disease %in% "Hereditary ataxia") %>% select(normalised_specific_disease) %>% unique() 
+#   normalised_specific_disease
+#           Hereditary ataxia
+table_diseases %>% filter(normalised_specific_disease %in% "Hereditary ataxia") %>% select(participant_id) %>% unique() %>% pull() %>% length()
+# 1038  
+
+table_diseases_pilot %>% filter()
