f7b841fc9de0e539bfe07f5ae5646cabc9b28fd8
kristina ibanez garikano <kibanez.ingemm@gmail.com>
2020-04-15 10:10:03 +0200

    loading table diseases for main cohort

--- paper_validation/filling_in_numbers_Table2_paperVal.R
+++ paper_validation/filling_in_numbers_Table2_paperVal.R
@@ -7,4 +7,12 @@ library(tidyverse); packageDescription ("tidyverse", fields = "Version") # "1.2.
 # Set directory
 setwd("~/Documents/STRs/PAPERS/VALIDATION_PAPER/")
 
-#
+# Load main table
+table_diseases = read.csv("table_diseases_enriched_popu_includingSkeletalMuscleChan.tsv",
+                          stringsAsFactors = F, 
+                          header = T,
+                          sep = "\t")
+dim(table_diseases)
+# 11801  16
+
+# Load pilot table
