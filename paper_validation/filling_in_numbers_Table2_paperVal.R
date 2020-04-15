c0f8d802507efa953b2fb5dd05ec48b7cf15252f
kristina ibanez garikano <kibanez.ingemm@gmail.com>
2020-04-15 10:08:59 +0200

    new script to compute number of participants for each subsection in the breakdown of Table2

---
+++ paper_validation/filling_in_numbers_Table2_paperVal.R
@@ -0,0 +1,10 @@
+# Objective: R script to fill out Table2 of the paper
+
+# libraries
+library(dplyr)
+library(tidyverse); packageDescription ("tidyverse", fields = "Version") # "1.2.1
+
+# Set directory
+setwd("~/Documents/STRs/PAPERS/VALIDATION_PAPER/")
+
+#
