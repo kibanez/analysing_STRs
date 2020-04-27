7ecdd17d3c138e91bf4b31ff222d636183df05fb
kristina ibanez garikano <kibanez.ingemm@gmail.com>
2020-04-27 16:20:47 +0200

    new script, simple, just to compute the total number of unique participants included in 4 subtables

---
+++ paper_validation/counting_unique_PID_across_subtables.R
@@ -0,0 +1,9 @@
+# Objective; calculate the total number of unique participants across A,B,C, and D subtables
+date ()
+Sys.info ()[c("nodename", "user")]
+commandArgs ()
+rm (list = ls ())
+R.version.string ## "R version 3.3.2 (2016-10-31)"
+
+# libraries
+library(dplyr); packageDescription ("dplyr", fields = "Version") #"0.8.5"
