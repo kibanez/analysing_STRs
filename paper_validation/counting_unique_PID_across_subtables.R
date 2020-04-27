496b67e0a2f3c66340ae4647c0ec42fa98de7d54
kristina ibanez garikano <kibanez.ingemm@gmail.com>
2020-04-27 16:23:33 +0200

    setting up the working environment loading table a main, table pilot, table B main

--- paper_validation/counting_unique_PID_across_subtables.R
+++ paper_validation/counting_unique_PID_across_subtables.R
@@ -7,3 +7,21 @@ R.version.string ## "R version 3.3.2 (2016-10-31)"
 
 # libraries
 library(dplyr); packageDescription ("dplyr", fields = "Version") #"0.8.5"
+
+# set environemnt
+setwd("~/Documents/STRs/PAPERS/VALIDATION_PAPER/subtables/")
+
+# Load final complete subtables
+table_a_main = read.csv("./TableA_main_including_ultrarare_same_cutoff_for_tableA.tsv",
+                        stringsAsFactors = F,
+                        sep = "\t",
+                        header = T)
+table_a_pilot = read.csv("./TableA_pilot_new_cutoffs.tsv",
+                         stringsAsFactors = F,
+                         header = T,
+                         sep = "\t")
+
+table_b_main = read.csv("./TableB_main.tsv",
+                        stringsAsFactors = F,
+                        header = T,
+                        sep = "\t")
