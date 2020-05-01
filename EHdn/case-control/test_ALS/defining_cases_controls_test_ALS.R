721d9523c5e17083a7497ede9c2d286e77d0f461
kristina ibanez garikano <kibanez.ingemm@gmail.com>
2020-05-01 16:18:03 +0200

    new folder and script for testing ALS case-control analysis with EHdn

---
+++ EHdn/case-control/test_ALS/defining_cases_controls_test_ALS.R
@@ -0,0 +1,14 @@
+# Objective: define and track here the modus operandi when selecting samples for cases and controls for PILOT adult ataxia
+date ()
+Sys.info ()[c("nodename", "user")]
+commandArgs ()
+rm (list = ls ())
+R.version.string ## "R version 3.6.3 (2020-02-29)"
+
+# libraries
+library(dplyr)
+
+# defining working directory
+setwd("~/Documents/STRs/ANALYSIS/EHdn/EHdn-v0.9.0/case-control/analysis/test_ALS/")
+
+#
