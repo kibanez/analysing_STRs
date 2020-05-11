edee776f88d582c290bfd7a8850f1d2f57968264
kristina ibanez garikano <kibanez.ingemm@gmail.com>
2020-05-11 17:47:46 +0200

    new folder for new analysis

---
+++ EHdn/case-control/UCHL1+C9orf72/defining_cases_controls_UCHL1_C9orf72.R
@@ -0,0 +1,114 @@
+# Objective: define and track here the modus operandi when selecting samples for cases and controls for PILOT adult ataxia
+date ()
+Sys.info ()[c("nodename", "user")]
+commandArgs ()
+rm (list = ls ())
+R.version.string ## "R version 3.6.3 (2020-02-29)"
+
+# libraries
+library(dplyr)
+
+# defining working directory
+setwd("~/Documents/STRs/ANALYSIS/EHdn/EHdn-v0.9.0/case-control/analysis/test_ALS/")
+
+# Load list of cases
+l_cases = read.table("./list_ALS_GRCh38.txt", stringsAsFactors = F)
+l_cases = l_cases$V1
+
+
+# Load Main data
+main_clin_data = read.csv("~/Documents/STRs/clinical_data/clinical_data/rd_genomes_all_data_300320.tsv",
+                          sep = "\t",
+                          stringsAsFactors = F,
+                          header = T)
+dim(main_clin_data)
+# 1124633  31
+
+# Cases - `l_cases`
+l_main_cases = l_cases
+
+# Controls
+# - ONLY probands
+# - rare disease germline genomes (exclude for now cancer genomes)
+# - `disease_group` NOT in neurology
+# - for `main` take GRCh38
+main_controls = main_clin_data %>%
+  filter(participant_type %in% "Proband",
+         programme %in% "Rare Diseases",
+         genome_build %in% "GRCh38",
+         year_of_birth < 2000,
+         !plate_key %in% l_main_cases,
+         !grepl("[Nn][Ee][Uu][Rr][Oo]", main_clin_data$disease_group))
+dim(main_controls)
+# 124282  31
+
+length(unique(main_controls$plate_key))
+# 11355
+
+# Writing into files
+write.table(l_main_cases, "input/main_6_cases.txt", quote = F, row.names = F, col.names = F)
+
+l_main_controls = unique(main_controls$plate_key)
+write.table(l_main_controls, "input/main_11355_controls.txt", quote = F, row.names = F, col.names = F)
+
+# Let's create now the `manifest` file
+cases_df = data.frame(platekey = l_main_cases,
+                      group = rep("case", length(l_cases)))
+
+cases_df = cases_df %>%
+  group_by(platekey) %>%
+  mutate(path = paste(paste("/home/kgarikano/GEL_STR/EHdn/case-control/EHdn_v0.9.0/analysis/ALS_test/str-profiles", platekey, sep = "/"), "_EHdeNovo.str_profile.json", sep = "")) %>%
+  ungroup() %>%
+  as.data.frame()
+
+controls_df = data.frame(platekey = l_main_controls,
+                         group = rep("control", length(l_main_controls)))
+
+controls_df = controls_df %>%
+  group_by(platekey) %>%
+  mutate(path = paste(paste("/home/kgarikano/GEL_STR/EHdn/case-control/EHdn_v0.9.0/analysis/ALS_test/str-profiles", platekey, sep = "/"), "_EHdeNovo.str_profile.json", sep = "")) %>%
+  ungroup() %>%
+  as.data.frame()
+
+merged_df = rbind(cases_df,
+                  controls_df)
+
+dim(merged_df)
+# 11361  3
+
+# QC check - there should not be duplicated platekeys
+length(merged_df$platekey)
+# 11361
+
+write.table(merged_df,
+            "input/manifest_test_ALS.tsv",
+            sep = "\t",
+            quote = F,
+            row.names = F,
+            col.names = F)
+
+
+# There are some genomes that have not been included in EHdn calculation
+real_list = read.table("./list_11226_cases.txt", stringsAsFactors = F)
+real_list = real_list$V1
+# 11226
+
+merged_df = merged_df %>%
+  filter(platekey %in% real_list, group %in% "control" | group %in% "case")
+dim(merged_df)
+# 11226  3
+
+write.table(merged_df,
+            "input/manifest_test_ALS_real.tsv",
+            sep = "\t",
+            quote = F,
+            row.names = F,
+            col.names = F)
+
+
+save.image("test_ALS_case_control_environment.Rdata")
+
+# run quality control checks
+source("~/git/analysing_STRs/EHdn/case-control/functions/quality_control.R")
+plotting_age_distribution(environment_file = "~/Documents/STRs/ANALYSIS/EHdn/EHdn-v0.9.0/case-control/analysis/test_ALS/test_ALS_case_control_environment.Rdata", 
+                          working_directory = "~/Documents/STRs/ANALYSIS/EHdn/EHdn-v0.9.0/case-control/analysis/test_ALS/")
