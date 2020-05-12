47192074625596ee9c7264a91c0e1b6523134db9
kristina ibanez garikano <kibanez.ingemm@gmail.com>
2020-05-12 11:07:56 +0200

    load list of controls

--- EHdn/case-control/analysing_annotated_casecontrol.R
+++ EHdn/case-control/analysing_annotated_casecontrol.R
@@ -26,6 +26,12 @@ l_cases = l_cases$V1
 length(l_cases)
 # 6
 
+# List of controls
+l_controls = read.table("./test_ALS/input/main_11355_controls.txt", stringsAsFactors = F)
+l_controls = l_controls$V1
+length(l_controls)
+# 11355
+
 # Filtering criteria: pvalue <= 0.05 
 cc_data_filtered = cc_data %>%
   filter(pvalue <= 0.05)
@@ -33,6 +39,7 @@ dim(cc_data_filtered)
 # 186  9
 
 # Filtering those genes or repeat-motifs that are significantly enriched in cases
-
+# We need to analyse `counts` field
+# Create cases and controls columns, and put there the % of how many are there??
 
 
