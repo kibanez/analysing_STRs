5793ee02cae731950fe65e1428735b38688f8bfe
kristina ibanez garikano <kibanez.ingemm@gmail.com>
2020-05-12 12:32:45 +0200

    going across all pvalue interesting cases

--- EHdn/case-control/analysing_annotated_casecontrol.R
+++ EHdn/case-control/analysing_annotated_casecontrol.R
@@ -42,4 +42,40 @@ dim(cc_data_filtered)
 # We need to analyse `counts` field
 # Create cases and controls columns, and put there the % of how many are there??
 
+for (i in 1:length(cc_data_filtered$counts)){
+  aver = strsplit(cc_data_filtered$counts[i], ",")[[1]]
+  total_counts = length(aver)
+  cases_l = c()
+  controls_l = c()
+  
+  for (j in 1:total_counts){
+    item = strsplit(aver[j], ":")[[1]][1]
+    if (item %in% l_cases){
+      cases_l = c(cases_l, item)
+    }else if (item %in% l_controls){
+      controls_l = c(controls_l, item)
+    }
+  }
+  
+  # Raw list of cases and controls
+  cc_data_filtered$list_cases[i] = paste(unlist(cases_l), collapse = ';')
+  cc_data_filtered$list_controls[i] = paste(unlist(controls_l), collapse = ';')
+  
+  # % of cases and controls
+  perc_cases = length(cases_l) / length(l_cases)
+  perc_controls = length(controls_l) / length(l_controls)
+  
+  cc_data_filtered$perc_cases[i] = perc_cases
+  cc_data_filtered$perc_controls[i] = perc_controls
+  
+}
 
+dim(cc_data_filtered)
+# 186  13
+
+write.table(cc_data_filtered,
+            "aver.tsv",
+            sep = "\t",
+            quote = F,
+            row.names = F,
+            col.names = T)
