95da37f0a2d590d6580c70a6e8e21c2dbe8ef184
kristina ibanez garikano <kibanez.ingemm@gmail.com>
2020-05-29 15:36:46 +0100

    loading annovar and cellbase tables

--- operations_across_genes/merging_annovar_cellbase_csv.R
+++ operations_across_genes/merging_annovar_cellbase_csv.R
@@ -10,3 +10,23 @@ library(dplyr)
 
 # defining working directory
 setwd("~/Documents/STRs/ANALYSIS/FXN_monoallelic/")
+
+# naming annovar and cellbase output files
+cellbase_output = "merged_38_mono_expanded_FXN_genome_cellbase_table.csv"
+annovar_output = "merged_38_mono_expanded_FXN_genome_annovar.csv.hg38_multianno.txt"
+
+# loading annotated data 
+cellbase_table = read.csv(cellbase_output,
+                          header = T,
+                          stringsAsFactors = F,
+                          sep = ",")
+dim(cellbase_table)
+# 304  36
+
+annovar_table = read.csv(annovar_output,
+                         header = T,
+                         stringsAsFactors = F,
+                         sep = "\t")
+dim(annovar_table)
+# 18636  104
+
