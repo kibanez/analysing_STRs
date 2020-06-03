607b496918997c61ff981d481b1170c878fd5701
kristina ibanez garikano <kibanez.ingemm@gmail.com>
2020-06-03 16:53:50 +0200

    loading original main and pilot ancestry tables

--- population_research/detecting_expanded_genomes_from_popu_annotated_genomes.R
+++ population_research/detecting_expanded_genomes_from_popu_annotated_genomes.R
@@ -10,3 +10,19 @@ library(dplyr)
 
 # defining working directory
 setwd("~/Documents/STRs/ANALYSIS/population_research/PAPER/expanded_genomes_main_pilot/")
+
+# Load data
+main_table = read.csv("../../MAIN_ANCESTRY/GEL_60k_germline_dataset_fine_grained_population_assignment20200224.csv",
+                      stringsAsFactors = F,
+                      header = T,
+                      sep = ",")
+dim(main_table)
+# 59464  36
+
+pilot_table = read.csv("../../PILOT_ANCESTRY/FINE_GRAINED_RF_classifications_incl_superPOP_prediction_final20191216.csv",
+                       stringsAsFactors = F,
+                       header = T,
+                       sep = ",")
+dim(pilot_table)
+# 4821  44
+
