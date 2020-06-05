a185d5a2d05bd7e3f85dcc255d9a0bf6ec682cd7
kristina ibanez garikano <kibanez.ingemm@gmail.com>
2020-06-05 18:20:41 +0200

    changing names diff in main and pilot

--- paper_validation/numbers_for_table2.R
+++ paper_validation/numbers_for_table2.R
@@ -119,3 +119,69 @@ table(l_gender_all)
 # male = 7839
 
 # total = 13958
+
+# Let's calculate across independent diseases
+l_independent_diseases = c("Hereditary ataxia",
+                           "Hereditary spastic paraplegia",
+                           "'Early onset and familial Parkinson''s Disease'",
+                           "Complex Parkinsonism (includes pallido-pyramidal syndromes)",
+                           "Early onset dystonia",
+                           "Early onset dementia",
+                           "Amyotrophic lateral sclerosis or motor neuron disease",
+                           "Charcot-Marie-Tooth disease",
+                           "Ultra-rare undescribed monogenic disorders",
+                           "Intellectual disability",
+                           "Kabuki syndrome",
+                           "Congenital myopathy",
+                           "Distal myopathies",
+                           "Congenital muscular dystrophy",
+                           "Skeletal Muscle Channelopathies")
+
+
+l_independent_diseases_pilot = c("Hereditary ataxia",
+                     "Hereditary spastic paraplegia",
+                     "Early onset and familial Parkinson's Disease",
+                     "Complex Parkinsonism (includes pallido-pyramidal syndromes)",
+                     "Early onset dystonia",
+                     "Early onset dementia (encompassing fronto-temporal dementia and prion disease)",
+                     "Amyotrophic lateral sclerosis/motor neuron disease",
+                     "Charcot-Marie-Tooth disease",
+                     "",
+                     "Intellectual disability",
+                     "Kabuki syndrome",
+                     "Congenital myopathy",
+                     "Distal myopathies",
+                     "Congenital muscular dystrophy",
+                     "Skeletal Muscle Channelopathies")
+                     
+            
+for (i in 1:length(l_independent_diseases)){
+  print(l_independent_diseases[i])
+  # Unique PIDs
+  l_main_pid = table_diseases %>% filter(normalised_specific_disease %in% l_independent_diseases[i]) %>% select(participant_id) %>% unique() %>% pull() 
+  l_pilot_pid = table_diseases_pilot %>% filter(specificDisease %in% l_independent_diseases_pilot[i]) %>% select(gelID) %>% unique() %>% pull() 
+  
+  l_pid_all = unique(c(l_main_pid,
+                       l_pilot_pid))
+  print(length(l_pid_all))
+  
+  # Age of all
+  l_main_age = table_diseases %>% filter(normalised_specific_disease %in% l_independent_diseases[i]) %>% select(age) %>% pull() 
+  l_pilot_age = table_diseases_pilot %>% filter(specificDisease %in% l_independent_diseases_pilot[i]) %>% select(age) %>% pull() 
+  
+  l_age_all = c(l_main_age,
+                l_pilot_age)
+  print(summary(l_age_all))
+
+  # Gender
+  l_main_gender = table_diseases %>% filter(normalised_specific_disease %in% l_independent_diseases[i]) %>% select(participant_phenotypic_sex)  %>% pull() 
+  l_pilot_gender = table_diseases_pilot %>% filter(specificDisease %in% l_independent_diseases_pilot[i]) %>% select(sex) %>% pull() 
+  
+  l_gender_all = c(l_main_gender,
+                   l_pilot_gender)
+  
+  print(table(l_gender_all))
+  
+  
+}
+
