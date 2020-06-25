7410e4939b35f524969b27093e83851674e8011c
kristina ibanez garikano <kibanez.ingemm@gmail.com>
2020-06-25 15:13:09 +0200

    code for renaming the genomes and loci

--- paper_validation/renaming_genomes_per_pileup.R
+++ paper_validation/renaming_genomes_per_pileup.R
@@ -15,14 +15,41 @@ setwd("~/Documents/STRs/VALIDATION/QC_visual_inspection/pileups_june/")
 
 # Load original or raw data
 # Visual_QC_EHv2.5.5_ME.xlsx as tsv
-l_pos_gel = read.csv("./list_GEL_EHv2_as_positive.txt", stringsAsFactors = F, header = F)
-l_pos_gel = l_pos_gel$V1
-length(l_pos_gel)
-# 66
+df_pos_gel = read.csv("./list_GEL_EHv2_as_positive.txt", stringsAsFactors = F, header = F, sep = "\t")
+dim(df_pos_gel)
+# 66  2
 
 # Load correspondence between GEL Platekey and Paper ID
 # From Table S7
 df_ids = read.csv("./correspondence_gelID_paperID.tsv", stringsAsFactors = F, sep = "\t")
 dim(df_ids)
 # 635  3
+
+for (i in 1:length(df_pos_gel$V1)){
+  prefix = paste("output_EHv255_positive_GEL/EH_", df_pos_gel$V1[i], sep = "")
+  orig_vcf = paste(prefix, ".vcf", sep = "")
+  orig_json = paste(prefix, ".json", sep = "")
+  orig_log = paste(prefix, "_alignments_relevant_reads.log", sep = "")
+  
+  paperID = df_ids %>% 
+    filter(gel_ID %in% df_pos_gel$V1[i], locus %in% df_pos_gel$V2[i]) %>%
+    select(paper_ID) %>%
+    unique() %>%
+    pull()
+  if (length(paperID) > 0){
+    prefix_new = paste("output_EHv255_positive_GEL/", paperID, sep = "")
+    new_vcf = paste(prefix_new, ".vcf", sep = "")
+    new_json = paste(prefix_new, ".json", sep = "")
+    new_log = paste(prefix_new, "_alignments_relevant_reads.log", sep = "")
+    
+    file.rename(from = orig_vcf, to = new_vcf)
+    file.rename(from = orig_json, to = new_json)
+    file.rename(from = orig_log, to = new_log)
+    
+  }else{
+    print("error")
+  }
+}
+
+# Prepare
 
