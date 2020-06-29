2c3267812afecf10998178e550547a84d9f095ac
kristina ibanez garikano <kibanez.ingemm@gmail.com>
2020-06-29 12:10:48 +0200

    new script to count number of genomes

---
+++ population_research/number_genomes_per_project.R
@@ -0,0 +1,15 @@
+# Objective: combine 100K, 1K, and gnomAD projects and see how many genomes we are analysing
+date ()
+Sys.info ()[c("nodename", "user")]
+commandArgs ()
+rm (list = ls ())
+R.version.string ## "R version 3.6.1 (2019-07-05)"
+
+library(ggplot2); packageDescription ("ggplot2", fields = "Version") #"3.2.1"
+library(grid); packageDescription ("grid", fields = "Version") #"3.6.1"
+library(gridExtra); packageDescription ("gridExtra", fields = "Version") #"2.3"
+library(reshape2); packageDescription ("reshape2", fields = "Version") #"1.4.3"
+require(dplyr); packageDescription ("dplyr", fields = "Version") #"0.8.3"
+
+# Set working dir
+setwd("~/Documents/STRs/ANALYSIS/population_research/PAPER/")
