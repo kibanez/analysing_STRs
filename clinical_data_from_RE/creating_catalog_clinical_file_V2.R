68b5b0f4df3734313bfe242a66499402f971e373
kristina ibanez garikano <kibanez.ingemm@gmail.com>
2020-07-13 12:44:39 +0200

    adapting to the fields as the others writing down into a file

--- clinical_data_from_RE/creating_catalog_clinical_file_V2.R
+++ clinical_data_from_RE/creating_catalog_clinical_file_V2.R
@@ -52,48 +52,49 @@ path = read.csv("./sequencing_report_2020-07-07_11-23-59.tsv",
 dim(path)
 # 32436  11
 
-path_subset = path %>% select(participant_id, platekey, type, path)
+# Create genome_build
+path = path %>%
+  group_by(platekey) %>%
+  mutate(genome_build = case_when(
+    delivery_version == "V2" ~ "GRCh37",
+    delivery_version == "V4" ~ "GRCh38",
+    delivery_version == "unknown" ~ "unknown")) %>%
+ ungroup() %>%
+  as.data.frame()
+
+path_subset = path %>% select(participant_id, platekey, type, path, genome_build)
 
 # Let's focus on the REAL genomes we do have
-all_data = path_subset %>% select(participant_id, platekey, path, type)
+all_data = path_subset %>% select(participant_id, platekey, path, type, genome_build)
 dim(all_data)
-# 32436  4
+# 32436  5
 
 all_data = left_join(all_data,
-                     rd_analysis %>% select(participant_id, rare_diseases_family_id, platekey, biological_relationship_to_proband, participant_type, normalised_specific_disease, genome_build, participant_ethnic_category),
+                     disease_info %>% select(participant_id, disease_group, disease_sub_group, specific_disease, normalised_specific_disease),
                      by = "participant_id")
 dim(all_data)
-# 80547  11
-
-all_data = left_join(all_data,
-                     disease_info %>% select(participant_id, disease_group, disease_sub_group, specific_disease),
-                     by = "participant_id")
-dim(all_data)
-# 81554  14
+# 32727  9
 
 all_data = left_join(all_data, 
-                     participant_info %>% select(participant_id, participant_medical_review_qc_state_code, year_of_birth, participant_phenotypic_sex, participant_karyotypic_sex, participant_stated_gender, programme_consent_status, programme, participant_medical_review_qc_state_code),
+                     participant_info %>% select(participant_id, rare_diseases_family_id, biological_relationship_to_proband, participant_type,
+                                                 participant_medical_review_qc_state_code, year_of_birth, participant_phenotypic_sex, participant_karyotypic_sex, 
+                                                 participant_stated_gender, programme_consent_status, programme, participant_medical_review_qc_state_code, 
+                                                 participant_ethnic_category),
                      by = "participant_id")
 dim(all_data)                 
-# 57040  22
+# 32727  20
 
 all_data = left_join(all_data,
                      rd_family %>% select(rare_diseases_family_id, family_group_type, family_medical_review_qc_state_code),
                      by = "rare_diseases_family_id")
 dim(all_data)
-# 57040  24
-
-all_data = left_join(all_data,
-                     panels_info %>% select(participant_id, panel_name, panel_version),
-                     by = "participant_id")
-dim(all_data)
-# 95981  26
+# 32727  22
 
 all_data = left_join(all_data,
                      rd_participant %>% filter(hpo_present %in% "Yes") %>% select(participant_id, hpo_term, hpo_id),
                      by = "participant_id")
 dim(all_data)
-# 370296  28
+# 100933  24
 
 # population data
 popu_table = read.csv("~/Documents/STRs/ANALYSIS/population_research/MAIN_ANCESTRY/GEL_60k_germline_dataset_fine_grained_population_assignment20200224.csv",
@@ -105,8 +106,12 @@ dim(popu_table)
 
 all_data = left_join(all_data,
                       popu_table %>% select(ID, best_guess_predicted_ancstry, self_reported),
-                      by = c("platekey.x"="ID"))
+                      by = c("platekey"="ID"))
 dim(all_data)
-# 370296  30
+# 100933  26
 
-write.table(all_data, "../../rd_genomes_all_data_100720_V4.tsv", sep = "\t", quote = F, row.names = F, col.names = T)
+all_data = unique(all_data)
+dim(all_data)
+# 100933  26
+
+write.table(all_data, "../../rd_genomes_all_data_130720_V2.tsv", sep = "\t", quote = F, row.names = F, col.names = T)
