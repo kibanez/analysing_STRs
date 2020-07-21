f43aa117066d40f8c9a630fbc39dbcef5fb62bfc
kristina ibanez garikano <kibanez.ingemm@gmail.com>
2020-07-21 11:31:24 +0200

    new script to count and compute total number of probands

---
+++ population_research/computing_probands_not_neuro_from_case-controls.R
@@ -0,0 +1,14 @@
+# Objective: count how many probands we do have
+# Count how many probands MINUS neuro we do have
+# For EHv3.2.2
+date ()
+Sys.info ()[c("nodename", "user")]
+commandArgs ()
+rm (list = ls ())
+R.version.string ## "R version 3.6.1 (2019-07-05)"
+
+# libraries
+library(dplyr)
+
+# Set working directory
+setwd("~/Documents/STRs/ANALYSIS/cases_controls/batch_march/EHv322/")
