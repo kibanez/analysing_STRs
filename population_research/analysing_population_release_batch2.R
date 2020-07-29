20f443b9abd48c3ac788e4dd86af709e819dd764
kristina ibanez garikano <kibanez.ingemm@gmail.com>
2020-07-29 14:41:59 +0200

    analysing total number of genomes and unrelated genomes writing into a file list of unrelated genomes

--- population_research/analysing_population_release_batch2.R
+++ population_research/analysing_population_release_batch2.R
@@ -16,3 +16,25 @@ library(ggpubr); packageDescription("ggpubr", fields = "Version") # 0.2.3
 # Set environment
 setwd("/Users/kibanez/Documents/STRs/ANALYSIS/population_research/")
 
+# Load popu data with related/no related info
+popu_batch2 = read.csv("./MAIN_ANCESTRY/batch2/aggV2_bedmerge_30KSNPs_labkeyV9_08062020_update_PCsancestryrelated.tsv",
+                       sep = " ",
+                       stringsAsFactors = F,
+                       header = T)
+dim(popu_batch2)
+# 78388  17
+
+unique(length(popu_batch2$plate_key))
+# 78388
+
+# List of unrelated genomes
+l_unrelated = popu_batch2 %>% filter(unrelated_set == 1) %>% select(plate_key) %>% unique() %>% pull()
+length(l_unrelated)
+# 55847
+
+write.table(l_unrelated,
+            "./MAIN_ANCESTRY/batch2/l_unrelated_55847_genomes_batch2.txt",
+            quote = F,
+            row.names = F,
+            col.names = F)
+
