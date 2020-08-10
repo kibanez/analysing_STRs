4a856f8c0da1b06b00281afbd25fc9c308d9b74a
kristina ibanez garikano <kibanez.ingemm@gmail.com>
2020-08-10 13:44:25 +0200

    unrel and cc tables for ATXN2

--- population_research/computing_probands_not_neuro_from_case-controls.R
+++ population_research/computing_probands_not_neuro_from_case-controls.R
@@ -410,3 +410,43 @@ length(which(l_yes_unrel %in% probands_unrelated))
 # How many are PROBANDS not NEURO?
 length(which(l_yes_unrel %in% probands_not_neuro_unrelated))
 # 3
+
+## ATXN2
+# cc tables
+l_yes_cc = read.csv("../carriers/cc_pileup_100Kg/list_yes_ATXN2.txt",
+                    stringsAsFactors = F,
+                    header = F)
+l_yes_cc = l_yes_cc$V1
+length(l_yes_cc)
+# 38
+
+# How many are PROBANDS?
+length(which(l_yes_cc %in% l_cc_probands))
+# 20
+
+# How many are PROBANDS not NEURO?
+length(which(l_yes_cc %in% l_cc_probands_not_neuro))
+# 10
+
+# unrel tables
+l_yes_unrel = read.csv("../carriers/pileup_100Kg/list_to_compute_yes_or_no_ATXN2.txt",
+                       stringsAsFactors = F,
+                       header = F)
+l_yes_unrel = l_yes_unrel$V1
+length(l_yes_unrel)
+# 40
+
+l_yes_unrel = intersect(l_yes_unrel,
+                        l_yes_cc)
+length(l_yes_unrel)
+# 20
+
+# How many are PROBANDS?
+length(which(l_yes_unrel %in% probands_unrelated))
+# 7
+
+# How many are PROBANDS not NEURO?
+length(which(l_yes_unrel %in% probands_not_neuro_unrelated))
+# 2
+
+
