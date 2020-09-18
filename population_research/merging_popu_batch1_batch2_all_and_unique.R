5b678e9afd89f85116183a415edc74e8278069d5
kristina ibanez garikano <kibanez.ingemm@gmail.com>
2020-09-18 15:19:56 +0200

    updating library version

--- population_research/merging_popu_batch1_batch2_all_and_unique.R
+++ population_research/merging_popu_batch1_batch2_all_and_unique.R
@@ -1,17 +1,18 @@
-# Objetive: analyse population release - batch2
+# Objetive: merge population batch1 and batch2, in order to have a unique space
 date ()
 Sys.info ()[c("nodename", "user")]
 commandArgs ()
 rm (list = ls ())
-R.version.string ## "R version 3.6.1 (2019-07-05)"
+R.version.string ## "R version 3.6.3 (2020-02-29)"
 
 # libraries
-library(dplyr); packageDescription ("dplyr", fields = "Version") #"0.8.3"
-library(ggplot2); packageDescription ("ggplot2", fields = "Version") #"3.2.1"
+library(dplyr); packageDescription ("dplyr", fields = "Version") #"0.8.5"
+library(ggplot2); packageDescription ("ggplot2", fields = "Version") #"3.3.0"
 library(reshape); packageDescription ("reshape", fields = "Version") #"0.8.8"
 library(scatterplot3d); packageDescription("scatterplot3d", fields = "Version") # 0.3-41
-library(ggpubr); packageDescription("ggpubr", fields = "Version") # 0.2.3
-
+library(ggpubr); packageDescription("ggpubr", fields = "Version") # 0.3.0
 
 # Set environment
 setwd("/Users/kibanez/Documents/STRs/ANALYSIS/population_research/MAIN_ANCESTRY/")
+
+# 
