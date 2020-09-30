f0855cc161637ebb54333052caadabc38c6beabd
kristina ibanez garikano <kibanez.ingemm@gmail.com>
2020-09-30 16:17:09 +0200

    load clinical data across 13 loci and 90863 pids

--- population_research/computing_carrier_ratio_across_13loci.R
+++ population_research/computing_carrier_ratio_across_13loci.R
@@ -11,3 +11,12 @@ library(dplyr)
 
 # Set working directory
 setwd("~/Documents/STRs/ANALYSIS/population_research/PAPER/carriers/cc_pileup_100Kg/")
+
+# Load clinical data for 90K pids across 13 loci
+clin_data = read.csv("~/Documents/STRs/ANALYSIS/cases_controls/batch_march/EHv322/table_13_loci_across_90863_genomes_each_row_allele_EHv3.2.2_90863platekeys_88827pids.tsv",
+                     stringsAsFactors = F,
+                     header = T,
+                     sep = "\t")
+dim(clin_data)
+# 91857  46
+
