0f3368361c606f6000134bca6226f5cf9a0c9b85
kristina ibanez garikano <kibanez.ingemm@gmail.com>
2020-09-30 16:51:35 +0200

    load visual QC data

--- population_research/computing_carrier_ratio_across_13loci.R
+++ population_research/computing_carrier_ratio_across_13loci.R
@@ -12,11 +12,24 @@ library(dplyr)
 # Set working directory
 setwd("~/Documents/STRs/ANALYSIS/population_research/PAPER/carriers/cc_pileup_100Kg/")
 
-# Load clinical data for 90K pids across 13 loci
+# Load clinical data for 90,863 genomes for which we do have EHv3 estimations across 13 loci
 clin_data = read.csv("~/Documents/STRs/ANALYSIS/cases_controls/batch_march/EHv322/table_13_loci_across_90863_genomes_each_row_allele_EHv3.2.2_90863platekeys_88827pids.tsv",
                      stringsAsFactors = F,
                      header = T,
                      sep = "\t")
 dim(clin_data)
 # 91857  46
+
+# remove platekeys that have no PIDs
+clin_data  = clin_data %>% filter(participant_id != ".")
+dim(clin_data)
+# 89821  46
+
+# Load the whole table for 100kGP - case-controls 
+table_100cc_QC = read.csv("./table_platekey_locus_QC_inspection.tsv",
+                          stringsAsFactors = F,
+                          header = T,
+                          sep = "\t")
+dim(table_100cc_QC)
+# 1878  13
 
