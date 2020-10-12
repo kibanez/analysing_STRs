1fdbb6ce577c8b658f4308fac316a4592382eada
kristina ibanez garikano <kibanez.ingemm@gmail.com>
2020-10-12 12:44:49 +0200

    new function to compute percentiles and quaritles for a given dataframe across all genes

---
+++ functions/computing_percentiles.R
@@ -0,0 +1,5 @@
+# Function that given a dataframe with different genes, it computes the percentiles for each gene across given population
+computing_percentiles <- function(df_input){
+  
+  return(df_percentiles)
+}
