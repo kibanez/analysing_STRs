00190fdc2b6c8af4d0bc79c77f3a736018170279
kristina ibanez garikano <kibanez.ingemm@gmail.com>
2020-10-23 11:30:30 +0200

    removing very old code

--- paper_validation/plotting_bubble_plot_PCR_vs_visual_inspection.R
+++ paper_validation/plotting_bubble_plot_PCR_vs_visual_inspection.R
@@ -26,95 +26,11 @@ val_data = read.csv("./GEL_accuracy_final_not_NCL.tsv",
 dim(val_data)
 # 418  11 - no NCL
 
-# 1 - Define min and max alleles for both PCR and EH 
-val_data = val_data %>% 
-  group_by(validation_id, locus) %>%
-  mutate(min_PCR = min(exp_PCR_a1, exp_PCR_a2),
-         max_PCR = max(exp_PCR_a1, exp_PCR_a2),
-         min_EH = min(Truth.Short.Allele, Truth.Long.Allele),
-         max_EH = max(Truth.Short.Allele, Truth.Long.Allele)) %>%
-  ungroup() %>%
-  as.data.frame()
-dim(val_data)
-# 616  15
-# 418 16
-
-# Let's take the important meat: experimentally validated data and EH estimations
-exp_alleles_v2 = c(as.integer(val_data$min_PCR), as.integer(val_data$max_PCR))
-eh_alleles_v2 = c(as.integer(val_data$min_EH), as.integer(val_data$max_EH))
-locus_v2 = c(val_data$locus, val_data$locus)
-
-# Remove NAs
-index_NA = which(is.na(exp_alleles_v2))
-exp_alleles_v2 = exp_alleles_v2[-index_NA]
-eh_alleles_v2 = eh_alleles_v2[-index_NA]
-locus_v2 = locus_v2[-index_NA]
-
-# Create dataframe with exp, eh, freq for each locus
-df_data_with_freq_v2 = data.frame()
-l_locus = unique(locus_v2)
-for(i in 1:length(l_locus)){
-  aux_validation_a1 = val_data %>% filter(locus %in% l_locus[i]) %>% select(min_PCR) %>% pull() %>% as.integer() 
-  aux_validation_a2 = val_data %>% filter(locus %in% l_locus[i]) %>% select(max_PCR) %>% pull() %>% as.integer() 
-  aux_exp_alleles_v2 = c(aux_validation_a1, aux_validation_a2)
-  
-  aux_eh_a1 = val_data %>% filter(locus %in% l_locus[i]) %>% select(min_EH) %>% pull() %>% as.integer() 
-  aux_eh_a2 = val_data %>% filter(locus %in% l_locus[i]) %>% select(max_EH) %>% pull() %>% as.integer() 
-  aux_eh_alleles_v2 = c(aux_eh_a1, aux_eh_a2)
-  
-  data_aux = xyTable(aux_exp_alleles_v2, aux_eh_alleles_v2)
-  
-  df_data_aux = data.frame(eh_alleles = data_aux$y,
-                           exp_alleles = data_aux$x,
-                           number_of_alleles = data_aux$number,
-                           locus = rep(l_locus[i], length(data_aux$x)))
-  # Concat info per locus
-  df_data_with_freq_v2 = rbind(df_data_with_freq_v2,
-                               df_data_aux)
-                      
-}
-
 output_folder = "./figures/"
-
-#df_data_with_freq_v2$exp_alleles[133] = 100
-
-max_value = max(df_data_with_freq_v2$eh_alleles, 
-                df_data_with_freq_v2$exp_alleles,
-                na.rm = TRUE) + 5
-
-# Filling manually colours (locus)
-brewer.pal(n = 8, name = "Dark2")
-# "#1B9E77" "#D95F02" "#7570B3" "#E7298A" "#66A61E" "#E6AB02" "#A6761D" "#666666"
-brewer.pal(n = 8, name = "Set1")
-# "#E41A1C" "#377EB8" "#4DAF4A" "#984EA3" "#FF7F00" "#FFFF33" "#A65628" "#F781BF"
 
 group.colors = c("AR" = "#1B9E77", "ATN1" = "#D95F02", "ATXN1" ="#7570B3", "ATXN2" = "#E7298A", "ATXN3" = "#66A61E", 
                  "ATXN7" = "#E6AB02", "CACNA1A" = "#A6761D", "FXN" = "#666666", "HTT" ="#E41A1C", "TBP" = "#FF7F00", 
                  "C9orf72" = "#FFFF33", "FMR1" = "#F781BF", "PPP2R2B" = "black")
-
-joint_plot = ggplot(df_data_with_freq_v2, 
-                    aes(x = exp_alleles, y = eh_alleles, colour = factor(locus))) + 
-  geom_point(aes(fill = factor(locus), size = number_of_alleles)) + 
-  xlim(5,max_value) + 
-  ylim(5,max_value) + 
-  labs(title = "", 
-       y = "Repeat sizes for each allele \n Expansion Hunter after visual inspection", 
-       x = "Repeat sizes for each allele \n Experimental PCR validation") + 
-  geom_abline(method = "lm", formula = x ~ y, linetype = 2, colour = "gray") +  
-  coord_equal() +
-  scale_fill_manual(values=group.colors) +  
-  theme(legend.title = element_blank(),
-        axis.text.x.top = element_text()) + 
-  guides(size = FALSE) 
-
-
-png("figures/joint_bubble_plot_EH_visualInspection_vs_PCR_generalView.png", units="in", width=5, height=5, res=300)
-print(joint_plot)
-dev.off()
-
-pdf("figures/joint_bubble_plot_EH_visualInspection_vs_PCR_generalView.pdf")
-print(joint_plot)
-dev.off()
 
 # Mike's suggestion - 1
 # Truth short and truth long alleles in X axis
