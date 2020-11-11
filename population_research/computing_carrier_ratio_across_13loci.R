c085b0f1be6ef4b0eae9b9e46100059f7e5097ec
kristina ibanez garikano <kibanez.ingemm@gmail.com>
2020-11-11 12:57:19 +0100

    piece of code when computing mutation freq only considering AFR genomes (RD and Cancer germlines)

--- population_research/computing_carrier_ratio_across_13loci.R
+++ population_research/computing_carrier_ratio_across_13loci.R
@@ -465,7 +465,7 @@ length(l_unrelated_genomes)
 clin_data_unrel = clin_data %>%
   filter(platekey %in% l_unrelated_genomes)
 dim(clin_data_unrel)
-# 55014  47
+# 55014  70
 
 total_genomes_unrelated = length(unique(clin_data_unrel$participant_id))
 # 54492
@@ -538,6 +538,47 @@ write.table(df_unrel_eur,
             row.names = F,
             col.names = T)
 
+# Now the same, but focusing on AFR genomes
+total_genomes_unrelated_AFR = clin_data_unrel %>% 
+  filter(superpopu %in% "AFR") %>% 
+  select(participant_id) %>% 
+  unique() %>% 
+  pull() %>% 
+  length()
+
+# 1766
+# Compute carrier ratio for each locus
+# select ONLY genomes that have an expansion that passed visual QC in the RD and cancer probands
+# Create a df for UNRELATED AFR
+df_unrel_afr = data.frame()
+for (i in 1:length(l_locus)){
+  locus_after_VI = paste(l_locus[i], "after_VI", sep = "_")
+  total_RD_probands_and_cancer_expanded_after_QC_locus = clin_data_unrel %>% 
+    filter(eval(parse(text=locus_after_VI)) == TRUE,
+           superpopu %in% "AFR") %>%
+    select(participant_id) %>%
+    unique() %>%
+    pull() %>%
+    length()
+  
+  freq_carrier = round(total_genomes_unrelated_AFR / total_RD_probands_and_cancer_expanded_after_QC_locus,digits = 2)
+  ratio_freq_carrier = paste("1 in", as.character(freq_carrier), sep = " ")
+  
+  ci_max = round(total_genomes_unrelated_AFR/(total_genomes_unrelated_AFR*((total_RD_probands_and_cancer_expanded_after_QC_locus/total_genomes_unrelated_AFR)-1.96*sqrt((total_RD_probands_and_cancer_expanded_after_QC_locus/total_genomes_unrelated_AFR)*(1-total_RD_probands_and_cancer_expanded_after_QC_locus/total_genomes_unrelated_AFR)/total_genomes_unrelated_AFR))), digits = 2)
+  ci_min = round(total_genomes_unrelated_AFR/(total_genomes_unrelated_AFR*((total_RD_probands_and_cancer_expanded_after_QC_locus/total_genomes_unrelated_AFR)+1.96*sqrt((total_RD_probands_and_cancer_expanded_after_QC_locus/total_genomes_unrelated_AFR)*(1-total_RD_probands_and_cancer_expanded_after_QC_locus/total_genomes_unrelated_AFR)/total_genomes_unrelated_AFR))), digits = 2)
+  
+  ci_ratio= as.character(paste(as.character(ci_min), as.character(ci_max), sep = "-"))
+  
+  df_unrel_afr = rbind(df_unrel_afr,
+                       cbind(l_locus[i], total_RD_probands_and_cancer_expanded_after_QC_locus, total_genomes_unrelated_AFR, ratio_freq_carrier, ci_ratio))
+}
+# write into a table
+write.table(df_unrel_afr,
+            "total_numbers_and_ratio_UNRELATED_AFR.tsv",
+            sep = "\t",
+            quote = F,
+            row.names = F,
+            col.names = T)
 
 
 
