8d5975e51073df5abb2fc7c2f195e26dc65ec696
kristina ibanez garikano <kibanez.ingemm@gmail.com>
2021-01-05 13:42:20 +0100

    new script to haplotype region within HTT

---
+++ haplotyping/haplotyping_HTT.R
@@ -0,0 +1,20 @@
+,# Objective: analyse haplotyping blocks within HTT
+date ()
+Sys.info ()[c("nodename", "user")]
+commandArgs ()
+rm (list = ls ())
+R.version.string ## "R version 3.6.1 (2019-07-05)"
+
+# libraries
+library(dplyr)
+
+# Set working directory
+setwd("~/Documents/STRs/ANALYSIS/haplotyping/HTT/")
+
+# Load clinical data
+clin_data = read.csv("~/Documents/STRs/clinical_data/clinical_data/rd_genomes_all_data_251120_V10.tsv",
+                     sep = "\t",
+                     stringsAsFactors = F,
+                     header = T)
+dim(clin_data)
+# 2096500  36
