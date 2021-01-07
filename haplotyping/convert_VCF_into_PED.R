3ab6a1aa11d62af44597b0817f2786255c121c28
kristina ibanez garikano <kibanez.ingemm@gmail.com>
2021-01-07 11:33:48 +0100

    writing into final file

--- haplotyping/convert_VCF_into_PED.R
+++ haplotyping/convert_VCF_into_PED.R
@@ -17,8 +17,9 @@ library(tidyverse)
 options <- commandArgs(trailingOnly = T)
 
 input_ped <- as.character(options[1])
-phenotype_file <- as.character(options[2])
-output_ped <- as.character(options[3])
+genotype_ped <- as.character(options[2])
+phenotype_file <- as.character(options[3])
+output_ped <- as.character(options[4])
 
 #Check whether `input_ped` and `phenotype_file` do exist
 if (!file.exists(input_ped)){
@@ -29,4 +30,36 @@ if (!file.exists(phenotype_file)){
   write("convert_VCF_into_PED R function: Phenotypes (gender and affection status) for all genomes are missing", stderr())
 }
 
+# Load data
+ped_data = read.csv(input_ped,
+                    stringsAsFactors = F,
+                    header = F,
+                    sep = "\t")
+
+pheno_data = read.csv(phenotype_file,
+                      stringsAsFactors = F,
+                      header = T,
+                      sep = "\t")
+
+genotype_data = read.csv(genotype_ped,
+                         stringsAsFactors = F,
+                         header = F,
+                         sep = "\t")
+
+# Enrich with `gender` and `affection status` each genome
+ped_data = left_join(ped_data[,c(1:4)],
+                     pheno_data %>% select(IID, sex, CaseControl),
+                     by = c("V2" = "IID"))
+
+# Merge `ped_data` with `genotypes`
+ped_complete = cbind(ped_data,
+                     genotype_data)
+
+# Write into file
+write.table(ped_complete,
+            output_ped,
+            quote = F, 
+            row.names = F,
+            col.names = F,
+            sep = "\t")
 
