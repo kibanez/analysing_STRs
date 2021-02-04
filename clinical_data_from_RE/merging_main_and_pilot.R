dd97ff9293ed9bfb64f5bdc2bc866cc464255322
kristina ibanez garikano <kibanez.ingemm@gmail.com>
2021-02-04 11:10:31 +0000

    loading and adapting pilot clinical data

--- clinical_data_from_RE/merging_main_and_pilot.R
+++ clinical_data_from_RE/merging_main_and_pilot.R
@@ -11,6 +11,57 @@ library(dplyr)
 setwd("~/Documents/STRs/clinical_data/clinical_data/")
 
 # Load latest Pilot data (frozen)
+pilot_clin_data = read.csv("~/Documents/STRs/clinical_data/pilot_clinical_data/pilot_cohort_clinical_data_4833_genomes_removingPanels_280919.tsv",
+                           sep = "\t",
+                           stringsAsFactors = FALSE,
+                           header = TRUE)
+dim(pilot_clin_data)
+# 4974  10
 
+# Let´s put all panel names into 1 single string splitted by ','
+list_panels_pilot = pilot_clin_data %>% group_by(gelID) %>% summarise(panel_list = toString(unique(specificDisease))) %>% ungroup() %>% as.data.frame()
+dim(list_panels_pilot)
+# 4833  2
+
+pilot_clin_data = left_join(pilot_clin_data,
+                            list_panels_pilot,
+                            by = "gelID")
+# Remove specificDisease
+pilot_clin_data = pilot_clin_data[,-8]
+pilot_clin_data = unique(pilot_clin_data)
+dim(pilot_clin_data)
+# 4833  10
+
+# Let's enrich with popu data
+pilot_popu_table = read.csv("~/Documents/STRs/ANALYSIS/population_research/PILOT_ANCESTRY/FINE_GRAINED_RF_classifications_incl_superPOP_prediction_final20191216.csv",
+                            stringsAsFactors = F,
+                            header = T,
+                            sep = ",")
+dim(pilot_popu_table)
+# 4821  44
+
+pilot_clin_data = left_join(pilot_clin_data,
+                            pilot_popu_table %>% select(ID, bestGUESS_sub_pop, bestGUESS_super_pop, self_reported),
+                            by = c("plateKey"="ID"))
+
+pilot_clin_data = unique(pilot_clin_data)
+dim(pilot_clin_data)
+# 4834  13
+
+# Enrich pilot clinical data with disease group and disease subgroup
+pheno_table = read.csv("~/Documents/STRs/clinical_data/pilot_clinical_data/phenotyping_v140_2019-09-13_15-26-02.tsv",
+                       stringsAsFactors = F,
+                       header = T,
+                       sep = "\t")
+pheno_table = unique(pheno_table)
+dim(pheno_table)
+# 106  3
+
+pilot_clin_data = left_join(pilot_clin_data,
+                            pheno_table,
+                            by = c("panel_list" = "specific_disease"))
+pilot_clin_data = unique(pilot_clin_data)
+dim(pilot_clin_data)
+# 4834  15
 
 # Load latests Main clinical data release (created from our R scripts)
