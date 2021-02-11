6286414cd896d34abf6e65a35144921d6b0cc691
kristina ibanez garikano <kibanez.ingemm@gmail.com>
2021-02-11 10:06:29 +0000

    selecting control genomes frmo pilot and main programmes

--- EHdn/selecting_genomes_b37_main_pilot.R
+++ EHdn/selecting_genomes_b37_main_pilot.R
@@ -44,4 +44,31 @@ cases_main = unique(rbind(cases_main_ha,
 dim(cases_main)
 # 405  24
 
+merged_cases = unique(rbind(cases_pilot,
+                            cases_main))
+dim(merged_cases)
+# 648  24
+
+l_platekeys_cases = unique(merged_cases$platekey)
+length(l_platekeys_cases)
+# 471
+
 # CONTROL selection - Main GRCh37 + Pilot
+#normalised_specific_disease as N/A (no disease)
+#disease_group not in Neurology/NEUROLOGY/Neurodevelopmental diseases
+#check that the genomes considered as cases are not within this group
+#for main dataset - select only GRCh37 genomes
+controls_pilot = clin_data %>%
+  filter(programme %in% "RD Pilot", !grepl("[Nn][Ee][Uu][Rr][Oo]", diseasegroup_list))
+
+controls_main = clin_data %>%
+  filter(programme %in% "Rare Diseases", !grepl("[Nn][Ee][Uu][Rr][Oo]", diseasegroup_list), !platekey %in% l_platekeys_cases)
+
+merged_controls = unique(rbind(controls_pilot,
+                               controls_main))
+dim(merged_controls)
+# 104881  24
+
+l_platekeys_controls = unique(merged_controls$platekey)
+length(l_platekeys_controls)
+# 60720 
