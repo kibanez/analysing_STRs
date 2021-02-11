984ea36768d48a4994ee92eec3d4d9ed65fb3644
kristina ibanez garikano <kibanez.ingemm@gmail.com>
2021-02-11 09:56:36 +0000

    selecting GRCH37 genomes from MAIn and PILOT programmes with HA or HSP

--- EHdn/selecting_genomes_b37_main_pilot.R
+++ EHdn/selecting_genomes_b37_main_pilot.R
@@ -9,5 +9,39 @@ R.version.string ## "R version 3.6.1 (2019-07-05)"
 library(dplyr)
 library(ggplot2)
 library(reshape)
+library(tidyverse)
 
 setwd("/Users/kibanez/Documents/STRs/ANALYSIS/EHdn/GRCh37/input/")
+
+clin_data = read.csv("~/Documents/STRs/clinical_data/clinical_data/Main_RE_V10_and_Pilot_programmes.tsv",
+                     stringsAsFactors = F, 
+                     header = T,
+                     sep = "\t")
+dim(clin_data)
+# 2101385  24
+
+# CASES selection - Main GRCh37 + Pilot
+# `normalised_specific_disease` in ataxia OR hereditary spastic paraplegia
+cases_pilot_ha = clin_data %>%
+  filter(programme %in% "RD Pilot", grepl("Hereditary ataxia", diseases_list))
+
+cases_pilot_hsp = clin_data %>%
+  filter(programme %in% "RD Pilot", grepl("Hereditary spastic paraplegia", diseases_list))
+
+cases_pilot = unique(rbind(cases_pilot_ha,
+                           cases_pilot_hsp))
+dim(cases_pilot)
+# 243  24
+
+cases_main_ha = clin_data %>%
+  filter(programme %in% "Rare Diseases", grepl("Hereditary ataxia", diseases_list), genome_build %in% "GRCh37")
+
+cases_main_hsp = clin_data %>%
+  filter(programme %in% "Rare Diseases", grepl("Hereditary spastic paraplegia", diseases_list), genome_build %in% "GRCh37")
+
+cases_main = unique(rbind(cases_main_ha,
+                          cases_main_hsp))
+dim(cases_main)
+# 405  24
+
+# CONTROL selection - Main GRCh37 + Pilot
