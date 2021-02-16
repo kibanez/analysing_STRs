4682c1e4f5492a1d35d11b08563b57706c622c8c
kristina ibanez garikano <kibanez.ingemm@gmail.com>
2021-02-16 14:22:33 +0000

    same strategy for unrel not neuro write into files

--- population_research/computing_carrier_ratio_across_13loci.R
+++ population_research/computing_carrier_ratio_across_13loci.R
@@ -52,7 +52,9 @@ dim(table_100cc_QC)
 # For each locus, compute the carrier ratio and CI 
 l_locus = unique(table_100cc_QC$locus)
 df_unrel = data.frame()
+df_unrel_notNeuro = data.frame()
 for(i in 1:length(l_locus)){
+  # Unrel
   # Compute number of expanded genomes per locus (after visual inspection)
   total_exp_after_VI_locus = table_100cc_QC %>%
     filter(locus %in% l_locus[i], Final.decision %in% "Yes", is_unrelated. %in% "Yes") %>%
@@ -61,7 +63,6 @@ for(i in 1:length(l_locus)){
     pull() %>%
     length()
   
-  # Unrel genomes
   freq_carrier = round(total_unrel / total_exp_after_VI_locus,digits = 2)
   ratio_freq_carrier = paste("1 in", as.character(freq_carrier), sep = " ")
   
@@ -72,9 +73,43 @@ for(i in 1:length(l_locus)){
   
   df_unrel = rbind(df_unrel,
                    cbind(l_locus[i], total_exp_after_VI_locus, total_unrel, ratio_freq_carrier, ci_ratio))
+
+  # Unrel NOT NEURO
+  # Compute number of expanded genomes per locus (after visual inspection)
+  total_exp_after_VI_locus_notNeuro = table_100cc_QC %>%
+    filter(locus %in% l_locus[i], Final.decision %in% "Yes", is_unrelated. %in% "Yes", is_neuro. %in% "NotNeuro") %>%
+    select(platekey) %>%
+    unique() %>%
+    pull() %>%
+    length()
+  
+  freq_carrier_notNeuro = round(total_unrel_notNeuro / total_exp_after_VI_locus_notNeuro,digits = 2)
+  ratio_freq_carrier_notNeuro = paste("1 in", as.character(freq_carrier_notNeuro), sep = " ")
+  
+  ci_max_notNeuro = round(total_unrel_notNeuro/(total_unrel_notNeuro*((total_exp_after_VI_locus_notNeuro/total_unrel_notNeuro)-1.96*sqrt((total_exp_after_VI_locus_notNeuro/total_unrel_notNeuro)*(1-total_exp_after_VI_locus_notNeuro/total_unrel_notNeuro)/total_unrel_notNeuro))), digits = 2)
+  ci_min_notNeuro = round(total_unrel_notNeuro/(total_unrel_notNeuro*((total_exp_after_VI_locus_notNeuro/total_unrel_notNeuro)+1.96*sqrt((total_exp_after_VI_locus_notNeuro/total_unrel_notNeuro)*(1-total_exp_after_VI_locus_notNeuro/total_unrel_notNeuro)/total_unrel_notNeuro))), digits = 2)
+  
+  ci_ratio_notNeuro = as.character(paste(as.character(ci_min_notNeuro), as.character(ci_max_notNeuro), sep = "-"))
+  
+  df_unrel_notNeuro = rbind(df_unrel_notNeuro,
+                            cbind(l_locus[i], total_exp_after_VI_locus_notNeuro, total_unrel_notNeuro, ratio_freq_carrier_notNeuro, ci_ratio_notNeuro))
+  
 }
 
+# Write unrel and unrel not neuro into files
+write.table(df_unrel,
+            "table_carrier_ratio_with_ci_unrel_genomes.tsv",
+            quote = F,
+            col.names = T,
+            row.names = F,
+            sep = "\t")
 
+write.table(df_unrel_notNeuro,
+            "table_carrier_ratio_with_ci_unrel_NotNeuro_genomes.tsv",
+            quote = F,
+            col.names = T,
+            row.names = F,
+            sep = "\t")
 
 # Load the table corresponding to HTT (work done by Arianna/Matteo)
 table_HTT_QC = read.csv("~/Documents/STRs/ANALYSIS/population_research/100K/carrier_freq/list_PIDs_for_HTT_pileup.tsv",
