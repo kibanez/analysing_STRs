2c73aa663027879361f0fe146aa5e085e360b51f
kristina ibanez garikano <kibanez.ingemm@gmail.com>
2021-02-25 15:09:44 +0000

    new script to analyse the demographics within the 100kGP, unrel, 125 out

---
+++ population_research/demographic_among_unrel_100kGP.R
@@ -0,0 +1,22 @@
+# Objective: study and retrieve numbers re demographic within 100kGP
+# unrelated genomes, filtering out 125bp genomes
+date ()
+Sys.info ()[c("nodename", "user")]
+commandArgs ()
+rm (list = ls ())
+R.version.string ## "R version 3.6.1 (2019-07-05)"
+
+# libraries
+library(dplyr)
+library(tidyverse)
+
+# Set working dir
+setwd("~/Documents/STRs/ANALYSIS/population_research/PAPER/")
+
+# Load clinical data
+clin_data = read.csv("~/Documents/STRs/clinical_data/clinical_data/Main_RE_V10_and_Pilot_programmes.tsv",
+                     stringsAsFactors = F, 
+                     header = T,
+                     sep = "\t")
+dim(clin_data)
+#
