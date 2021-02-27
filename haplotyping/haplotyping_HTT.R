a0ed4954b0f1e3668237deb9ce07ec18a0be8852
kristina ibanez garikano <kibanez.ingemm@gmail.com>
2021-02-27 09:49:07 +0000

    adapting to new clin data selecting unrel and also the families that have passed repoge checks

--- haplotyping/haplotyping_HTT.R
+++ haplotyping/haplotyping_HTT.R
@@ -40,21 +40,17 @@ l_exp_genomes = l_exp_genomes[-1]
 # Retrieve genome build, sex, popu for these genomes
 haplo_genomes = clin_data %>%
   filter(platekey %in% l_exp_genomes) %>%
-  select(platekey, genome_build, participant_ethnic_category, participant_phenotypic_sex, superpopu, programme, file_path)
+  select(platekey, genome_build, participant_ethnic_category, participant_phenotypic_sex, superpopu, programme)
 haplo_genomes = unique(haplo_genomes)
 dim(haplo_genomes)
-# 29  7
+# 28  6
 
 length(unique(haplo_genomes$platekey))
-# 29
-
-# Adapt or change path to the BAM file, to path to the gVCF file
-haplo_genomes$file_path = gsub("Assembly","Variations", haplo_genomes$file_path)
-haplo_genomes$file_path = gsub("bam","genome.vcf.gz", haplo_genomes$file_path)
+# 28
 
 # write them into a file
 write.table(haplo_genomes,
-            "./table_29genomes_expanded_unrelated_HTT.tsv",
+            "./table_28genomes_expanded_unrelated_HTT.tsv",
             sep = "\t",
             quote = F,
             row.names = F,
@@ -80,7 +76,7 @@ write.table(pheno_cases, "list_phenotypes_cases.tsv", quote = F, col.names = T, 
 
 # selecting genomes for CONTROL cohort
 #  genome_build %in% GRCh38 , affection_status %in% unaffected, repeat_size < 40, and population %in% EUR since the majority of genomes in the cases cohort correspond to Europeans. 
-# Unrelated, belonging to different families each of them.
+# Unrelated, batch2 (aggV2)
 # reported vs genetic checks PASS
 table_ehv3 = read.csv("~/Documents/STRs/ANALYSIS/cases_controls/batch_august/EHv322/table_STR_repeat_size_each_row_allele_EHv3.2.2_HTT_simplified.tsv",
                       stringsAsFactors = F,
@@ -98,16 +94,33 @@ list_expanded = table_ehv3 %>%
 length(list_expanded)
 # 61
 
-table_ehv3 = left_join(table_ehv3,
-                       clin_data %>% select(platekey, family_medical_review_qc_state_code),
-                       by = "platekey")
+# Only proband is enriched with genetic_vs_reported checks. Let's take the famIDs that have been undergone successfully
+# Only rare diseases. We don't do this with cancer
+l_famID_passesGvsRChecks = table_ehv3 %>%
+  filter(genetic_vs_reported_results %in% "familyPassesGvsRChecks") %>%
+  select(rare_diseases_family_id) %>%
+  unique() %>%
+  pull()
+length(l_famID_passesGvsRChecks)
+# 29925
 
 df_controls = table_ehv3 %>%
   filter(!platekey %in% list_expanded, genome_build %in% "GRCh38",
          affection_status %in% "Unaffected",superpopu %in% "EUR",
-         family_medical_review_qc_state_code %in% "Passed medical review - for interpretation")
+         rare_diseases_family_id %in% l_famID_passesGvsRChecks)
 dim(df_controls)
-# 562208  28
+# 72754  27
+
+# Select only unrelated genomes
+df_controls = df_controls %>%
+  filter(platekey %in% l_unrel)
+df_controls = unique(df_controls)
+dim(df_controls)
+# 56770 27
+
+l_controls_unrel = unique(df_controls$platekey)
+length(l_controls_unrel)
+# 18739
 
 # Random selection of 30 genomes
 set.seed(983823)
