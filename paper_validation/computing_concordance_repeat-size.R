1147ea81f8c6f3b8c3619c9cc141af021ec4bbdc
kristina ibanez garikano <kibanez.ingemm@gmail.com>
2021-04-28 10:27:01 +0200

    selecting random locus for repeated platekeys merge with dedup concordance table

--- paper_validation/computing_concordance_repeat-size.R
+++ paper_validation/computing_concordance_repeat-size.R
@@ -21,3 +21,31 @@ length(platekey_duplicated)
 # 195
 
 # There are a total of 195 repeat platekeys - for which we've got PCR lengths for more than one gene
+# We first take all dedup platekeys
+concordance_dedup = concordance %>%
+  filter(!platekey %in% platekey_duplicated) %>%
+  select(locus, validation_id, platekey, PCR, EHv312, concordance, classi, type)
+
+# We take only 1 gene/locus for each repeated platekey 
+l_genes = unique(concordance$locus)
+set.seed(532)
+
+concordance_dup = concordance %>%
+  filter(platekey %in% platekey_duplicated) %>%
+  group_by(platekey) %>%
+  mutate(random_gene = sample(l_genes, 1)) %>%
+  ungroup() %>%
+  as.data.frame()
+
+concordance_dup = concordance_dup %>%
+  select(random_gene, validation_id, platekey, PCR, EHv312, concordance, classi, type)
+colnames(concordance_dup) = colnames(concordance_dedup)
+concordance_dup = unique(concordance_dup)
+dim(concordance_dup)
+# 737  8
+
+concordance_merge = rbind(concordance_dedup,
+                          concordance_dup)
+write.table(concordance_merge,
+          "./concordance_repeats_dedup_repeated_platekeys.tsv",
+          quote = F, row.names = F, col.names = T, sep = "\t")
