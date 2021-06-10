def018da6e817a8f9c0527837350fa9a0c5366f6
kristina ibanez garikano <kibanez.ingemm@gmail.com>
2021-06-10 15:40:22 +0200

    trzing different models

--- population_research/computing_pvalue_Table2.R
+++ population_research/computing_pvalue_Table2.R
@@ -9,16 +9,22 @@ R.version.string ## "R version 3.6.3 (2020-02-29)"
 # libraries
 #install.packages("betareg")
 #install.packages("emmeans")
+#install.packages("GGally")
+#install.packages("VGAM")
+
 library(betareg); packageDescription ("betareg", fields = "Version") #3.1-4
 library(emmeans); packageDescription ("emmeans", fields = "Version") #1.6.1
+require(GGally)
+require(VGAM)
 
 # 
 test = read.csv("~/Downloads/test_anova.tsv", stringsAsFactors = F, sep = "\t")
 #Order factors by the order in data frame - otherwise, R will alphabetize them
 test$superpopu = factor(test$superpopu, levels = unique(test$superpopu))
 
-model = betareg(percentage ~ superpopu,
-                data = test)
+
+summary(m = vglm(percentage ~ superpopu, tobit(Upper = 800), data = test))
+
 
 
 
