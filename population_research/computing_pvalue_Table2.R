371a00441231a2a452a8e4c56e8f5ccc7c8c50d9
kristina ibanez garikano <kibanez.ingemm@gmail.com>
2021-06-10 14:53:05 +0200

    loading and installing beta regression libraries

--- population_research/computing_pvalue_Table2.R
+++ population_research/computing_pvalue_Table2.R
@@ -1,3 +1,25 @@
 # Objective: run anova one way-like pvalue
 # As we are working with percentages a beta regression sounds like a better strategy
+date ()
+Sys.info ()[c("nodename", "user")]
+commandArgs ()
+rm (list = ls ())
+R.version.string ## "R version 3.6.3 (2020-02-29)"
+
+# libraries
+#install.packages("betareg")
+#install.packages("emmeans")
+library(betareg); packageDescription ("betareg", fields = "Version") #3.1-4
+library(emmeans); packageDescription ("emmeans", fields = "Version") #1.6.1
+
+# 
+test = read.csv("~/Downloads/test_anova.tsv", stringsAsFactors = F, sep = "\t")
+#Order factors by the order in data frame - otherwise, R will alphabetize them
+test$superpopu = factor(test$superpopu, levels = unique(test$superpopu))
+
+model = betareg(percentage ~ superpopu,
+                data = test)
+
+
+
 
