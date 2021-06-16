2b8b43879d59575103ed7c986d86e63074393199
kristina ibanez garikano <kibanez.ingemm@gmail.com>
2021-06-16 16:03:40 +0200

    new script to plot expnaded genomes vs PC values across all genes and superpopus

---
+++ population_research/plotting_fig2.R
@@ -0,0 +1,18 @@
+# Objetive: represent number of expanded genomes beyond the premutation cut-off together with the PC values
+date ()
+Sys.info ()[c("nodename", "user")]
+commandArgs ()
+rm (list = ls ())
+R.version.string ## "R version 3.6.1 (2019-07-05)"
+
+# libraries
+library(dplyr); packageDescription ("dplyr", fields = "Version") #"0.8.3"
+library(ggplot2); packageDescription ("ggplot2", fields = "Version") #"3.2.1"
+library(reshape); packageDescription ("reshape", fields = "Version") #"0.8.8"
+library(scatterplot3d); packageDescription("scatterplot3d", fields = "Version") # 0.3-41
+library(ggpubr); packageDescription("ggpubr", fields = "Version") # 0.2.3
+
+# Set environment
+setwd("/Users/kibanez/Documents/STRs/ANALYSIS/population_research/PAPER/expanded_genomes_main_pilot/feb2021/beyond_premut/")
+
+# Load data
