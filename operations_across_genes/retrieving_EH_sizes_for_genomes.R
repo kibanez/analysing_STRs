3f6bfb3b04d1b990dde800d45771f5f535f015bd
kristina ibanez garikano <kibanez.ingemm@gmail.com>
2021-07-15 15:17:45 +0200

    load merged_table subset on 13 loci

--- operations_across_genes/retrieving_EH_sizes_for_genomes.R
+++ operations_across_genes/retrieving_EH_sizes_for_genomes.R
@@ -21,4 +21,19 @@ df_mito = read.csv("~/Downloads/table_mito_genomes.tsv",
 dim(df_mito)
 # 345 2
 
+# load merged august batch 2020
+merged_table = read.csv("~/Documents/STRs/data/research/batch_august2020/output_EHv3.2.2_vcfs/merged/merged_93446_genomes_EHv322_batch_august2020.tsv",
+                        stringsAsFactors = F,
+                        header = T,
+                        sep = "\t")
+dim(merged_table)
+# 27238  12
+
+# Can you see if they are in your data for the population paper, and pull out a the repeat-sizes for all the 13 RE loci for all genomes? 
+# A table with rows= genomes and columns =  26 (13 loci x 2 alleles)
+list_genes = c("AR", "ATN1", "ATXN1", "ATXN2", "ATXN3", "ATXN7", "C9ORF72", "CACNA1A", "DMPK", "FMR1", "FXN","HTT", "TBP")
+merged_table = merged_table %>%
+  filter(gene %in% list_genes)
+
+
 
