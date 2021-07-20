784d8ee0feba9e67345db6506dd3dc51cff7c1c3
kristina ibanez garikano <kibanez.ingemm@gmail.com>
2021-07-20 14:48:21 +0200

    keeping order of spec diseases

--- paper_validation/heatmap_figure3.R
+++ paper_validation/heatmap_figure3.R
@@ -9,6 +9,8 @@ R.version.string ## "R version 3.3.2 (2016-10-31)"
 library(dplyr); packageDescription ("dplyr", fields = "Version") #"0.8.5"
 library(tidyverse); packageDescription ("tidyverse", fields = "Version") # "1.2.1
 library(ggplot2); packageDescription ("ggplot2", fields = "Version") #"3.3.0"
+library(reshape2)
+library(tidyr)
 
 # set the working directory
 setwd("~/Documents/STRs/PAPERS/VALIDATION_PAPER/LANCET/APPEAL/")
@@ -20,3 +22,34 @@ table3 = read.csv("./template_for_figure3.tsv",
                   sep = "\t")
 dim(table3)
 # 15  14
+
+table3_reformat = pivot_longer(data = table3, 
+                                cols = -c(1),
+                                names_to = "disease", 
+                                values_to = "confirmed_repeats")
+
+table3_reformat$Spec_disease = factor(table3_reformat$Spec_disease, levels = rev(unique(table3_reformat$Spec_disease)))
+
+ggplot(data = table3_reformat, 
+       mapping = aes(x = disease,
+                     y = Spec_disease,
+                     fill = confirmed_repeats)) +
+  scale_fill_gradient(
+    name = "Cor", # changes legend title
+    low = "lightgray",
+    high = "red",
+    na.value = "lightgray",
+    limit = c(min(table3_reformat$confirmed_repeats), max(table3_reformat$confirmed_repeats)),
+    space = "Lab",
+    guide = "colourbar"
+  ) + theme_minimal() +
+  geom_tile() +
+  xlab(label = "") +
+  ylab(label = "")
+  #scale_y_reverse()
+
+
+
+
+
+
