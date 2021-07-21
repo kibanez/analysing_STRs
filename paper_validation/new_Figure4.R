d53c329c1a449532d67cdd99042a58994a5ad083
kristina ibanez garikano <kibanez.ingemm@gmail.com>
2021-07-21 11:40:17 +0200

    merging top and bottoms plots saving into a png

--- paper_validation/new_Figure4.R
+++ paper_validation/new_Figure4.R
@@ -12,6 +12,7 @@ library(ggplot2); packageDescription ("ggplot2", fields = "Version") #"3.3.0"
 library(reshape2)
 library(tidyr)
 library(scales)
+library(cowplot)
 
 # set the working directory
 setwd("~/Documents/STRs/PAPERS/VALIDATION_PAPER/LANCET/APPEAL/")
@@ -27,13 +28,13 @@ dim(df_tested)
 df_tested = df_tested %>% select(panel_id, disease, individuals_tested)
 df_tested$disease = factor(df_tested$disease, levels = unique(df_tested$disease))
 
-ggplot(df_tested, aes(x = disease, y = individuals_tested, fill = disease)) +
+top = ggplot(df_tested, aes(x = disease, y = individuals_tested, fill = disease)) +
   geom_bar(stat = "identity", position=position_dodge()) +
   #geom_text(aes(label = individuals_tested), vjust = 1.5, colour = "black") +
   #scale_fill_manual(values=rep("darkgrey", length(df_tested$disease))) +
   scale_fill_manual(values=c("grey54","grey54","grey54","grey54","grey54","grey54","grey54","grey54","grey54","grey64","grey54","grey54","grey54","grey54","grey44")) +
   guides(fill = FALSE) +
-  xlab("") + ylab("Participants tested") +
+  xlab("") + ylab("Individuals tested") +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=0)) +
   scale_x_discrete(position = "top", labels = wrap_format(5)) 
 
@@ -43,4 +44,24 @@ df_confirmed = read.csv("./table_for_figure4_confirmed.tsv",
                      header = T,
                      sep = "\t")
 dim(df_confirmed)
-# 15  3
+# 15  14
+
+melt_confirmed = melt(df_confirmed)
+melt_confirmed$disease = factor(melt_confirmed$disease, levels = unique(melt_confirmed$disease))
+
+bottom = ggplot(melt_confirmed, aes(x = disease, label = value, y = value, fill = variable)) +
+  geom_bar(stat = "identity") +
+  guides(fill = FALSE) +
+  xlab("") + ylab("") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=0)) +
+  scale_y_reverse() +
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank()) +
+  ylab("Individuals confirmed")
+
+png("new_Figure4.png",units="in", width=20, height=20, res=300)
+print(plot_grid(top, bottom, ncol = 1))
+dev.off()
+
+
