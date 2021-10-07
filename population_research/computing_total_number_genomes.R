ea7751822c9a6bdb604236561a50ed33fe89df43
kristina ibanez garikano <kibanez.ingemm@gmail.com>
2021-10-07 13:53:01 +0200

    loading clinical data

--- population_research/computing_total_number_genomes.R
+++ population_research/computing_total_number_genomes.R
@@ -19,6 +19,20 @@ l_125 = l_125$V1
 l_unrel = read.table("./l_unrelated_55603_genomes_batch2.txt", stringsAsFactors = F)
 l_unrel = l_unrel$V1
 
+# Total number of genomes, filtering out 125bp sequenced genomes
 l_unrel_not125 = unique(setdiff(l_unrel, l_125))
 length(l_unrel_not125)
 # 54437
+
+# Total number of genomes excluding Neuro
+# Update October 2021: we want to consider as Neuro, patients that have been assigned "Mito" or "ultra-rare" diseases
+
+# Load clinical data
+clin_data = read.csv("~/Documents/STRs/clinical_data/clinical_data/Main_RE_V12_and_Pilot_programmes.tsv",
+                     stringsAsFactors = F,
+                     header = T,
+                     sep = "\t")
+dim(clin_data)
+#
+
+
